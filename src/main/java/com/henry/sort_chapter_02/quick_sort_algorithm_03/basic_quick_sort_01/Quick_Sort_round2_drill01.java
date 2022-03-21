d9e53be621beb2344954fcff5f55508632dcae05
HenryInSH <2291972433@qq.com>
2022-03-21 18:57:15 +0800

    快速排序
    快速排序过程：
    1 排定切分元素；
    2 对左区间中的元素进行快速排序；
    3 对右区间中的元素进行快速排序；
    
    排定切分元素的过程：
    1 选定一个元素作为切分元素；
    2 从左边开始找比切分元素更大的元素，从右边开始找比切分元素更小的元素；
    3 都找到后，交换两个位置上的元素；
    4 如此重复，直到左右两个指针相遇；
    5 把切分元素 与 左区间中的最后一个元素进行交换 - 切分元素由此被排定

--- src/main/java/com/henry/sort_chapter_02/quick_sort_algorithm_03/basic_quick_sort_01/Quick_Sort_round2_drill01.java
+++ src/main/java/com/henry/sort_chapter_02/quick_sort_algorithm_03/basic_quick_sort_01/Quick_Sort_round2_drill01.java
@@ -4,8 +4,6 @@ import edu.princeton.cs.algs4.StdRandom;
 
 /*
 任务描述：完整地排序数组 = 把数组中的每一个元素都排定到其正确的位置
-算法描述：xxx
-
 ------
     排序的任务能够 用递归的方式解决吗？
     递归的要素：
@@ -22,6 +20,13 @@ import edu.princeton.cs.algs4.StdRandom;
     对于快速排序，这里的“其他操作”指的就是 切分操作。
     - 切分操作为元素大小添加了额外的限制，使得：当子数组有序时，整个数组也就被自然排定了。
 
+------
+快速排序算法描述：
+    1 排定一个元素，并使得 排定元素左边的元素都小于等于排定元素，排定元素右边的元素都 大于等于排定元素；
+    2 对 左侧的元素序列 + 右侧的元素序列 分别进行快速排序；
+
+切分算法的过程：
+    过程 - 手段 - 难点；
 ------
 递归调用的步骤：
     1 排定切分元素到正确的位置；
@@ -46,7 +51,20 @@ public class Quick_Sort_round2_drill01 {
         sort(a, arrangedPosition + 1, rightBar);
     }
 
-    // 排定切分元素 并 使数组满足： 左区间 排定元素 右区间 整体有序
+    // 排定切分元素 并 使数组满足： [左区间] + 排定元素 + [右区间] 整体有序
+    /*
+        过程：
+            1 选定切分元素；
+                手段：直接选择 左区间上的元素作为 切分元素；
+            2 遍历数组，把比切分元素小的元素放到切分元素左侧，把比切分元素大的元素放到切分元素右侧；
+                1 交换需要交换的元素；
+                    - 定义两个游标指针：左指针初始化指在左边界的位置上， 右指针初始化指在右边界的下一个位置上；
+                    - 左指针向右扫描，并在遇到 大于pivot元素时停下； 右指针向左扫描,并在遇到小于pivot的元素时停下；
+                    - 交换 左右指针所指向的元素；
+                    - 继续移动左右指针，直到左右指针相遇 -> 这说明左区间 + 右区间中的元素都已经满足大小要求
+                2 排定基准元素；
+                    - 把基准元素 与 左区间的最后一个元素 交换，并返回最后一个元素的位置（此时右指针指向此位置）
+     */
     private static int partition(Comparable[] a, int leftBar, int rightBar) {
         // 设置左右游标
         int leftCursor = leftBar;
@@ -57,8 +75,8 @@ public class Quick_Sort_round2_drill01 {
 
         // 用游标扫描数组中的元素 - 把小于基准元素的集合放在左边，大于基准元素的集合放在右边
         while (true) {
-            while(less(a[++leftCursor], pivot)) if(leftCursor == rightBar) break;
-            while(less(pivot, a[--rightBackwardsCursor])) if(rightBackwardsCursor == leftBar) break;
+            while(less(a[++leftCursor], pivot)) if(leftCursor == rightBar) break; // 这种情况说明：所有其他元素都小于 基准元素 - 不需要交换元素，可以直接排定基准元素
+            while(less(pivot, a[--rightBackwardsCursor])) if(rightBackwardsCursor == leftBar) break; // 所有其他元素都小于 基准元素
 
             // 结束标志：游标已经相遇
             if(rightBackwardsCursor <= leftCursor) break;
