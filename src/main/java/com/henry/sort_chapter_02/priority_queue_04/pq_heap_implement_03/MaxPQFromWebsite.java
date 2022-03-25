994f2aeb25852b5905947d78a6e1f543aee9656a
HenryInSH <2291972433@qq.com>
2022-03-25 23:28:44 +0800

    使用堆 来 实现优先队列
    堆本身也是一种 逻辑结构；
    堆这种逻辑结构的实现方式： 完全二叉树 + 元素大小约束
    
    完全二叉树 也是一种逻辑结构；
    完全二叉树的实现方式： 一个数组
    
    为了体现我们是在用堆来实现优先队列， 实例变量取名 itemHeap
    
    向优先队列中insert元素 => 向堆的最后一个位置上添加元素
    delMax from PQ => 删除堆顶元素

--- src/main/java/com/henry/sort_chapter_02/priority_queue_04/pq_heap_implement_03/MaxPQFromWebsite.java
+++ src/main/java/com/henry/sort_chapter_02/priority_queue_04/pq_heap_implement_03/MaxPQFromWebsite.java
@@ -9,7 +9,7 @@ package com.henry.sort_chapter_02.priority_queue_04.pq_heap_implement_03; /*****
  *  but the generic Key type must still be Comparable.
  *
  *  % java MaxPQFromWebsite < tinyPQ.txt 
- *  Q X P (6 left on pq)
+ *  Q X P (6 left on itemHeap)
  *
  *  We use a one-based array to simplify parent and child calculations.
  *
@@ -34,7 +34,7 @@ import java.util.NoSuchElementException;
  *  <p>
  *  This implementation uses a <em>binary heap</em>.
  *  The <em>insert</em> and <em>delete-the-maximum</em> operations take
- *  &Theta;(log <em>n</em>) amortized time, where <em>n</em> is the number
+ *  &Theta;(log <em>itemAmount</em>) amortized time, where <em>itemAmount</em> is the number
  *  of elements in the priority queue. This is an amortized bound 
  *  (and not a worst-case bound) because of array resizing operations.
  *  The <em>min</em>, <em>size</em>, and <em>is-empty</em> operations take 
@@ -49,13 +49,13 @@ import java.util.NoSuchElementException;
  *  @author Robert Sedgewick
  *  @author Kevin Wayne
  *
- *  @param <Key> the generic type of key on this priority queue
+ *  @param <Item> the generic type of key on this priority queue
  */
 
-public class MaxPQFromWebsite<Key> implements Iterable<Key> {
-    private Key[] pq;                    // store items at indices 1 to n
-    private int n;                       // number of items on priority queue
-    private Comparator<Key> comparator;  // optional comparator
+public class MaxPQFromWebsite<Item> implements Iterable<Item> {
+    private Item[] itemHeap;                    // store items at indices 1 to itemAmount
+    private int itemAmount;                       // number of items on priority queue
+    private Comparator<Item> comparator;  // optional comparator
 
     /**
      * Initializes an empty priority queue with the given initial capacity.
@@ -63,8 +63,8 @@ public class MaxPQFromWebsite<Key> implements Iterable<Key> {
      * @param  initCapacity the initial capacity of this priority queue
      */
     public MaxPQFromWebsite(int initCapacity) {
-        pq = (Key[]) new Object[initCapacity + 1];
-        n = 0;
+        itemHeap = (Item[]) new Object[initCapacity + 1];
+        itemAmount = 0;
     }
 
     /**
@@ -81,10 +81,10 @@ public class MaxPQFromWebsite<Key> implements Iterable<Key> {
      * @param  initCapacity the initial capacity of this priority queue
      * @param  comparator the order in which to compare the keys
      */
-    public MaxPQFromWebsite(int initCapacity, Comparator<Key> comparator) {
+    public MaxPQFromWebsite(int initCapacity, Comparator<Item> comparator) {
         this.comparator = comparator;
-        pq = (Key[]) new Object[initCapacity + 1];
-        n = 0;
+        itemHeap = (Item[]) new Object[initCapacity + 1];
+        itemAmount = 0;
     }
 
     /**
@@ -92,22 +92,22 @@ public class MaxPQFromWebsite<Key> implements Iterable<Key> {
      *
      * @param  comparator the order in which to compare the keys
      */
-    public MaxPQFromWebsite(Comparator<Key> comparator) {
+    public MaxPQFromWebsite(Comparator<Item> comparator) {
         this(1, comparator);
     }
 
     /**
-     * Initializes a priority queue from the array of keys.
-     * Takes time proportional to the number of keys, using sink-based heap construction.
+     * Initializes a priority queue from the array of items.
+     * Takes time proportional to the number of items, using sink-based heap construction.
      *
-     * @param  keys the array of keys
+     * @param  items the array of items
      */
-    public MaxPQFromWebsite(Key[] keys) {
-        n = keys.length;
-        pq = (Key[]) new Object[keys.length + 1];
-        for (int i = 0; i < n; i++)
-            pq[i+1] = keys[i];
-        for (int k = n/2; k >= 1; k--)
+    public MaxPQFromWebsite(Item[] items) {
+        itemAmount = items.length;
+        itemHeap = (Item[]) new Object[items.length + 1];
+        for (int i = 0; i < itemAmount; i++)
+            itemHeap[i+1] = items[i];
+        for (int k = itemAmount /2; k >= 1; k--)
             sink(k);
         assert isMaxHeap();
     }
@@ -121,7 +121,7 @@ public class MaxPQFromWebsite<Key> implements Iterable<Key> {
      *         {@code false} otherwise
      */
     public boolean isEmpty() {
-        return n == 0;
+        return itemAmount == 0;
     }
 
     /**
@@ -130,7 +130,7 @@ public class MaxPQFromWebsite<Key> implements Iterable<Key> {
      * @return the number of keys on this priority queue
      */
     public int size() {
-        return n;
+        return itemAmount;
     }
 
     /**
@@ -139,35 +139,37 @@ public class MaxPQFromWebsite<Key> implements Iterable<Key> {
      * @return a largest key on this priority queue
      * @throws NoSuchElementException if this priority queue is empty
      */
-    public Key max() {
+    public Item max() {
         if (isEmpty()) throw new NoSuchElementException("Priority queue underflow");
-        return pq[1];
+        return itemHeap[1];
     }
 
     // helper function to double the size of the heap array
     private void resize(int capacity) {
-        assert capacity > n;
-        Key[] temp = (Key[]) new Object[capacity];
-        for (int i = 1; i <= n; i++) {
-            temp[i] = pq[i];
+        assert capacity > itemAmount;
+        Item[] temp = (Item[]) new Object[capacity];
+        for (int i = 1; i <= itemAmount; i++) {
+            temp[i] = itemHeap[i];
         }
-        pq = temp;
+        itemHeap = temp;
     }
 
 
     /**
      * Adds a new key to this priority queue.
      *
-     * @param  x the new key to add to this priority queue
+     * @param  newItem the new key to add to this priority queue
      */
-    public void insert(Key x) {
+    public void insert(Item newItem) {
 
         // double size of array if necessary
-        if (n == pq.length - 1) resize(2 * pq.length);
+        if (itemAmount == itemHeap.length - 1) resize(2 * itemHeap.length);
 
-        // add x, and percolate it up to maintain heap invariant
-        pq[++n] = x;
-        swim(n);
+        // add newItem, and percolate it up to maintain heap invariant
+        itemHeap[++itemAmount] = newItem;
+
+        // 在数组末尾插入元素后，通过上浮最后一个位置的元素 来 保持数组的堆有序
+        swim(itemAmount);
         assert isMaxHeap();
     }
 
@@ -177,15 +179,21 @@ public class MaxPQFromWebsite<Key> implements Iterable<Key> {
      * @return a largest key on this priority queue
      * @throws NoSuchElementException if this priority queue is empty
      */
-    public Key delMax() {
+    public Item delMax() {
         if (isEmpty()) throw new NoSuchElementException("Priority queue underflow");
-        Key max = pq[1];
-        exch(1, n--);
+        Item maxItemInHeap = itemHeap[1];
+        // 把 堆中最大的元素(aka itemHeap[1]) 与 数组末尾的元素交换 - 这样最大元素被交换到 堆的最后一个位置
+        exch(1, itemAmount--);
+        // 下沉堆中第一个位置上的元素， 来 维持数组的堆有序
         sink(1);
-        pq[n+1] = null;     // to avoid loitering and help with garbage collection
-        if ((n > 0) && (n == (pq.length - 1) / 4)) resize(pq.length / 2);
+
+        // 删除数组中最后一个位置上的元素(它已经不属于堆) 以防止对象游离
+        itemHeap[itemAmount +1] = null;     // to avoid loitering and help with garbage collection
+
+        // 删除元素后，查看是不是需要调整 数组的容量大小
+        if ((itemAmount > 0) && (itemAmount == (itemHeap.length - 1) / 4)) resize(itemHeap.length / 2);
         assert isMaxHeap();
-        return max;
+        return maxItemInHeap;
     }
 
 
@@ -193,20 +201,20 @@ public class MaxPQFromWebsite<Key> implements Iterable<Key> {
      * Helper functions to restore the heap invariant.
      ***************************************************************************/
 
-    private void swim(int k) {
-        while (k > 1 && less(k/2, k)) {
-            exch(k, k/2);
-            k = k/2;
+    private void swim(int currentSpot) {
+        while (currentSpot > 1 && less(currentSpot/2, currentSpot)) {
+            exch(currentSpot, currentSpot/2);
+            currentSpot = currentSpot/2;
         }
     }
 
-    private void sink(int k) {
-        while (2*k <= n) {
-            int j = 2*k;
-            if (j < n && less(j, j+1)) j++;
-            if (!less(k, j)) break;
-            exch(k, j);
-            k = j;
+    private void sink(int currentSpot) {
+        while (2*currentSpot <= itemAmount) {
+            int biggerChildSpot = 2*currentSpot;
+            if (biggerChildSpot < itemAmount && less(biggerChildSpot, biggerChildSpot+1)) biggerChildSpot++;
+            if (!less(currentSpot, biggerChildSpot)) break;
+            exch(currentSpot, biggerChildSpot);
+            currentSpot = biggerChildSpot;
         }
     }
 
@@ -215,44 +223,49 @@ public class MaxPQFromWebsite<Key> implements Iterable<Key> {
      ***************************************************************************/
     private boolean less(int i, int j) {
         if (comparator == null) {
-            return ((Comparable<Key>) pq[i]).compareTo(pq[j]) < 0;
+            return ((Comparable<Item>) itemHeap[i]).compareTo(itemHeap[j]) < 0;
         }
         else {
-            return comparator.compare(pq[i], pq[j]) < 0;
+            return comparator.compare(itemHeap[i], itemHeap[j]) < 0;
         }
     }
 
     private void exch(int i, int j) {
-        Key swap = pq[i];
-        pq[i] = pq[j];
-        pq[j] = swap;
+        Item swap = itemHeap[i];
+        itemHeap[i] = itemHeap[j];
+        itemHeap[j] = swap;
     }
 
-    // is pq[1..n] a max heap?
+    // is itemHeap[1..itemAmount] a max heap?
     private boolean isMaxHeap() {
-        for (int i = 1; i <= n; i++) {
-            if (pq[i] == null) return false;
+        // 堆的性质1 - 完全二叉树 aka 数组中的元素连续且不为null
+        for (int cursor = 1; cursor <= itemAmount; cursor++) {
+            if (itemHeap[cursor] == null) return false;
         }
-        for (int i = n+1; i < pq.length; i++) {
-            if (pq[i] != null) return false;
+        // 堆的性质2 - 使用数组表示的完全二叉树 aka 数组中其他的位置上不能有元素
+        for (int i = itemAmount +1; i < itemHeap.length; i++) {
+            if (itemHeap[i] != null) return false;
         }
-        if (pq[0] != null) return false;
+        // 堆的约定 - 数组的第一个位置不存放任何元素（方便数组下标index 与 元素在二叉树中位置spot之间的转换）
+        if (itemHeap[0] != null) return false;
         return isMaxHeapOrdered(1);
     }
 
-    // is subtree of pq[1..n] rooted at k a max heap?
-    private boolean isMaxHeapOrdered(int k) {
-        if (k > n) return true;
-        int left = 2*k;
-        int right = 2*k + 1;
-        if (left  <= n && less(k, left))  return false;
-        if (right <= n && less(k, right)) return false;
-        return isMaxHeapOrdered(left) && isMaxHeapOrdered(right);
+    // is subtree of itemHeap[1..itemAmount] rooted at currentSpot a max heap?
+    private boolean isMaxHeapOrdered(int currentSpot) {
+        if (currentSpot > itemAmount) return true;
+        int leftChildSpot = 2*currentSpot;
+        int rightChildSpot = 2*currentSpot + 1;
+        if (leftChildSpot  <= itemAmount && less(currentSpot, leftChildSpot))  return false;
+        if (rightChildSpot <= itemAmount && less(currentSpot, rightChildSpot)) return false;
+
+        // 以当前节点的左右子节点作为根节点 的树，也是一个最大堆 - 堆的定义的递归性
+        return isMaxHeapOrdered(leftChildSpot) && isMaxHeapOrdered(rightChildSpot);
     }
 
 
     /***************************************************************************
-     * Iterator.
+     * Iterator. 支持迭代 - for循环
      ***************************************************************************/
 
     /**
@@ -262,28 +275,31 @@ public class MaxPQFromWebsite<Key> implements Iterable<Key> {
      *
      * @return an iterator that iterates over the keys in descending order
      */
-    public Iterator<Key> iterator() {
+    public Iterator<Item> iterator() {
         return new HeapIterator();
     }
 
-    private class HeapIterator implements Iterator<Key> {
+    private class HeapIterator implements Iterator<Item> {
 
-        // create a new pq
-        private MaxPQFromWebsite<Key> copy;
+        // 迭代器的操作可能会改变 队列中的元素，所以这里拷贝了 原始对象的一个副本
+        private MaxPQFromWebsite<Item> copy;
 
         // add all items to copy of heap
         // takes linear time since already in heap order so no keys move
         public HeapIterator() {
-            if (comparator == null) copy = new MaxPQFromWebsite<Key>(size());
-            else                    copy = new MaxPQFromWebsite<Key>(size(), comparator);
-            for (int i = 1; i <= n; i++)
-                copy.insert(pq[i]);
+            // 初始化容量
+            if (comparator == null) copy = new MaxPQFromWebsite<Item>(size());
+            else                    copy = new MaxPQFromWebsite<Item>(size(), comparator);
+            // 初始化 队列中的元素
+            for (int i = 1; i <= itemAmount; i++)
+                copy.insert(itemHeap[i]);
         }
 
         public boolean hasNext()  { return !copy.isEmpty();                     }
         public void remove()      { throw new UnsupportedOperationException();  }
 
-        public Key next() {
+        // 获取到队列中下一个位置上的元素 - 手段：删除掉当前的堆顶元素，并返回
+        public Item next() {
             if (!hasNext()) throw new NoSuchElementException();
             return copy.delMax();
         }
@@ -296,12 +312,17 @@ public class MaxPQFromWebsite<Key> implements Iterable<Key> {
      */
     public static void main(String[] args) {
         MaxPQFromWebsite<String> pq = new MaxPQFromWebsite<String>();
-        while (!StdIn.isEmpty()) {
+
+        while (!StdIn.isEmpty()) { // 判断标准输入流是否为空
+            // 读取标准输入流中的字符串
             String item = StdIn.readString();
+            // 如果当前字符串不是 - 就把它添加到 优先队列中
             if (!item.equals("-")) pq.insert(item);
+            // 如果遇到了 - 字符，就删除掉并打印 优先队列中当前的最大元素
             else if (!pq.isEmpty()) StdOut.print(pq.delMax() + " ");
         }
-        StdOut.println("(" + pq.size() + " left on pq)");
+
+        StdOut.println("(" + pq.size() + " left on itemHeap)");
     }
 
 }
