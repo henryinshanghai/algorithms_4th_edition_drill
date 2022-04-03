9963fe2bf1310aef6320fb47240d27bd8693e8ad
HenryInSH <2291972433@qq.com>
2022-04-03 17:29:34 +0800

    使用堆 来完成对数组的排序
    特征：
    1 原地排序 - 起始时，数组中所有元素都被用来创建堆
    2 每次排定数组中的当前最大值 - 然后用剩余的元素重新创建堆
    3 sink(1)的操作就可以 恢复堆有序

--- src/main/java/com/henry/sort_chapter_02/priority_queue_04/pq_sort_05/HeapSort_drill01.java
+++ src/main/java/com/henry/sort_chapter_02/priority_queue_04/pq_sort_05/HeapSort_drill01.java
@@ -27,7 +27,7 @@ public class HeapSort_drill01 {
          */
         while (itemAmount > 1) {
             exch(arrayToSort, 1, itemAmount--);
-            // todo sink(1)的操作，能够使堆有序吗？
+            // sink(1)的操作可以使得堆有序 - 因为交换节点时，交换上来的是较大的子节点
             sink(arrayToSort, 1, itemAmount);
         }
     }
@@ -35,22 +35,24 @@ public class HeapSort_drill01 {
     /**
      * 对当前堆中的指定节点执行 "下沉操作"
      * 说明：这里之所以需要 arrayToSort, n作为参数，是因为没有把它维护成一个实例变量
-     *
+     * 特征：下沉的次数不确定，这里使用一个while()循环
      * @param arrayToSort
      * @param currentSpotInHeap
      * @param lastSpotInHeap
      */
     private static void sink(Comparable[] arrayToSort, int currentSpotInHeap, int lastSpotInHeap) { // 当前节点的值小于子节点中较大者
         while (2 * currentSpotInHeap + 1 <= lastSpotInHeap) { // 2*currentSpotInHeap+1 <= lastSpotInHeap
+            // 1 找到当前节点的较大子节点
             int biggerChildSpot = 2 * currentSpotInHeap;
             if (less(arrayToSort, biggerChildSpot, biggerChildSpot + 1)) biggerChildSpot = biggerChildSpot + 1;
 
+            // 4 在执行交换之前，如果当前节点 在堆中已经被排定 - 跳出循环
             if (!less(arrayToSort, currentSpotInHeap, biggerChildSpot)) break;
 
-            // 交换 堆中两个位置上的元素
+            // 2 交换 堆中两个位置上的元素
             exch(arrayToSort, currentSpotInHeap, biggerChildSpot);
 
-            // 更新 当前的位置
+            // 3 更新 当前的位置
             currentSpotInHeap = biggerChildSpot;
         }
     }
