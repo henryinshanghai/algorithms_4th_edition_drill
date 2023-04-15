cb36d10f7c79698a15a230b6ae8df983c7aeeff1
HenryInSH <2291972433@qq.com>
2023-04-16 00:08:09 +0800

    --- 插入排序：#1 获取未排序区间的当前元素; #2 把#1中元素插入到有序区间中（相邻比较 + less时交换） ---

---
+++ src/main/java/com/henry/sort_chapter_02/primary_algorithm_01/insert_sort_02/InsertionSortDrill.java
@@ -0,0 +1,41 @@
+package com.henry.sort_chapter_02.primary_algorithm_01.insert_sort_02;
+
+import edu.princeton.cs.algs4.In;
+
+public class InsertionSortDrill {
+
+    public static void sort(Comparable[] a) {
+        int itemAmount = a.length;
+
+        for (int cursorOfItemToInsert = 1; cursorOfItemToInsert < itemAmount; cursorOfItemToInsert++) {
+
+            // 检测布尔表达式的值。如果为 true，循环体被执行。如果为false，循环终止
+            for (int backwardsCursor = cursorOfItemToInsert;
+                 backwardsCursor > 0 && less(a[backwardsCursor], a[backwardsCursor - 1]) ; backwardsCursor--) {
+                exch(a, backwardsCursor, backwardsCursor - 1);
+            }
+        }
+    }
+
+    public static boolean less(Comparable v, Comparable w) {
+        return v.compareTo(w) < 0;
+    }
+
+    public static void exch(Comparable[] a, int i, int j) {
+        Comparable temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+
+    public static void show(Comparable[] a) {
+        for (int cursor = 0; cursor < a.length; cursor++) {
+            System.out.print(a[cursor] + " ");
+        }
+    }
+    public static void main(String[] args) {
+        String[] a = In.readStrings();
+
+        sort(a);
+        show(a);
+    }
+}
