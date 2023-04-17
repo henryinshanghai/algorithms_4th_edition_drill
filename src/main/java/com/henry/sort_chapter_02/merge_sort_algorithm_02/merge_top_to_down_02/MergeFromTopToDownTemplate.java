ba7b646441e31d0326b47564d477f152f3f028fd
HenryInSH <2291972433@qq.com>
2023-04-17 08:00:53 +0800

    --- 归并排序（自顶向下）：直接对左半区间&右半区间进行归并排序 ---

--- src/main/java/com/henry/sort_chapter_02/merge_sort_algorithm_02/merge_top_to_down_02/MergeFromTopToDownTemplate.java
+++ src/main/java/com/henry/sort_chapter_02/merge_sort_algorithm_02/merge_top_to_down_02/MergeFromTopToDownTemplate.java
@@ -1,53 +1,38 @@
 package com.henry.sort_chapter_02.merge_sort_algorithm_02.merge_top_to_down_02;
 
-import edu.princeton.cs.algs4.StdIn;
-
 /*
-    任务描述：完整地排序数组 = 把数组中的每一个元素都排定到其正确的位置
-    算法描述：xxx
+任务描述：得到“完整排序的数组” = 把数组中的每一个元素都“排定到其正确的位置”
 
-------
-    排序的任务能够 用递归的方式解决吗？
-    递归的要素：
-        1 原始任务 能够分解成为 规模更小的同类型任务；
-        2 解决 规模更小的任务（的结果）能够帮助解决原始任务。
+递归可行性分析：~
 
-    对排序任务来说, 什么是规模更小的任务？
-        排序数组的一部分
-
-    排序数组的一部分，能够帮助 “完全排序整个数组”吗？
-        答：不尽然，因为即使数组的局部被排序了 但元素本身离它的正确位置还会有一些距离
-        但局部排序的结果(小问题的答案) 有可能帮助 完全排序整个数组(原始问题的答案)， 但是必然需要其他操作的支持 - 比如手中的牌是 JQKA 2345
-        对于归并排序，这里的“其他操作” ———— 归并操作。
-
-------
+术语定义：
+    有序的子数组：子数组中的相邻元素连续升序或降序排列；
     归并操作： 能够 把 两个有序的数组，归并得到一个更大的有序数组。
-    应用： 支持 归并排序。
-    方法作用： 把a[leftBar, middle] 与 a[middle+1, rightBar]两个有序的子数组进行归并，并把结果存回到a[leftBar, rightBar]中
-    基于以上方法作用所设计的方法签名：
-        merge(Comparable[] a, int leftBar, int middle, int rightBar)
-    特征：
-        这里的辅助数组，为了减少方法的参数，可以设计成为成员变量,然后在适当的方法中进行初始化即可
 
-------
-    public方法签名： public的方法签名 - sort(Comparable[] a) 传入一个数组进行排序
-    内层方法签名：
-        如何在方法签名层面，体现出 规模更小的排序呢？
-        答：在方法签名中添加 排序区间 - 因为在做原地排序
-        初步设计： sort(Comparable[] a, int leftBar, int rightBar)
+算法描述：
 
-------
-String[] a = {"S", "O", "R", "T", "E", "X", "A", "M", "P", "L", "E"};
+排序算法步骤：
+    #1 对左半区间排序；
+    #2 对右半区间排序；
+    #3 执行左半区间与右半区间的归并操作，得到“完全排序的数组”
 
-// here is <=
-// "int cursor = 0; cursor < a.length; cursor++" is wrong, 这里应该使用的是左右区间
+归并算法步骤：
+    #1 拷贝原始数组的指定区间到 辅助数组中；
+    #2 为辅助数组的左右区间，添加游标指针；
+    #3 使用左右区间中的较小元素 来 填充回原始数组。
+
+方法签名设计：~
+
+执行过程的特征：先在左半区间逐步归并（左-右-完整区间 区间逐渐扩大）出一个完整的有序子数组，
+再在右半区间逐步归并（左-右-完整区间 区间逐渐扩大）出一个完整的有序子数组；
+最终，对有序的左右子数组，一次性归并得到“完全排序的数组”
  */
-public class Merge_topToDown {
+public class MergeFromTopToDownTemplate {
     // 成员变量 - 好处：可以在当前类的所有方法中使用它
     private static Comparable[] aux;
 
     public static void sort(Comparable[] a) {
-        // 初始化辅助数组的大小
+        // 🐖 初始化辅助数组的大小
         aux = new Comparable[a.length];
 
         // 对数组的指定区间进行排序 - 这里是全部区间
@@ -56,7 +41,7 @@ public class Merge_topToDown {
 
     // 排序数组的指定区间 a[leftBar, rightBar] 闭区间
     private static void sort(Comparable[] a, int leftBar, int rightBar) {
-        // 递归终结的条件：区间变窄为0
+        // 递归终结的条件：区间变窄为0 aka 数组已经有序
         if(leftBar >= rightBar) return;
 
         // 计算当前区间的中间位置
@@ -99,7 +84,7 @@ public class Merge_topToDown {
         return v.compareTo(w) < 0;
     }
 
-    private static void printItems(Comparable[] a) {
+    private static void show(Comparable[] a) {
         int N = a.length;
         for (int i = 0; i < N; i++) {
             System.out.print(a[i] + " ");
@@ -109,10 +94,10 @@ public class Merge_topToDown {
     }
 
     public static void main(String[] args) {
-//        String[] a = {"M", "E", "R", "G", "E", "S", "O", "R", "T", "E", "X", "A", "M", "P", "L", "E"};
-        String[] a = StdIn.readAllStrings();
+        String[] a = {"M", "E", "R", "G", "E", "S", "O", "R", "T", "E", "X", "A", "M", "P", "L", "E"};
+//        String[] a = StdIn.readAllStrings();
         sort(a);
 
-        printItems(a);
+        show(a);
     }
 }
