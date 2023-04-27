166aaf81687425657dd4fbaa1573865b3ac4a998
HenryInSH <2291972433@qq.com>
2023-04-27 20:11:57 +0800

    --- merge sort(bottom up): #1 partition; #2 iterate to insert sort the sequences; #3 downhill and repeat. ---

--- src/main/java/com/henry/sort_chapter_02/primary_algorithm_01/shell_sort_03/ShellSortDrill.java
+++ src/main/java/com/henry/sort_chapter_02/primary_algorithm_01/shell_sort_03/ShellSortDrill.java
@@ -1,52 +1,60 @@
 package com.henry.sort_chapter_02.primary_algorithm_01.shell_sort_03;
 
+// #1 partition the array into subGroup via 'itemAmountOfSubGroup';
+// #2 iterate each item after the item(startPointOfDisorder), then using 'insert sort' to sort each item sequence
+// #3 downhill the subGroup till 1.
 public class ShellSortDrill {
 
     public static void sort(Comparable[] a) {
-        int itemAmount = a.length;
 
-        // 计算一个较大的N值
+        int itemAmount = a.length;
         int itemAmountOfSubGroup = 1;
+
         while (itemAmountOfSubGroup < itemAmount / 3) {
             itemAmountOfSubGroup = itemAmountOfSubGroup * 3 + 1;
         }
 
-        // 获取“完全排序的数组”
+        int count = 0;
         while (itemAmountOfSubGroup >= 1) {
-            // 获取“分隔有序的元素序列”
             int startPointOfDisorder = itemAmountOfSubGroup;
-            for (int cursorOfItemToInsert = startPointOfDisorder; cursorOfItemToInsert < itemAmount; cursorOfItemToInsert++) {
-                // 把当前待插入元素 插入到序列中
-                for (int backwardsCursor = cursorOfItemToInsert;
-                     backwardsCursor >= startPointOfDisorder && less(a[backwardsCursor], a[backwardsCursor - itemAmountOfSubGroup]);
-                     backwardsCursor-=itemAmountOfSubGroup) {
+
+            for (int currentItemToInsert = startPointOfDisorder; currentItemToInsert < itemAmount; currentItemToInsert++) {
+                for (int backwardsCursor = currentItemToInsert; backwardsCursor >= itemAmountOfSubGroup && less(a, backwardsCursor, backwardsCursor - itemAmountOfSubGroup) ; backwardsCursor-=itemAmountOfSubGroup) {
                     exch(a, backwardsCursor, backwardsCursor - itemAmountOfSubGroup);
                 }
             }
-
+            System.out.println("第" + (++count) + "次循环");
+            System.out.println("当前 itemAmountOfSubGroup的值为： " + itemAmountOfSubGroup);
+            System.out.println("当前数组元素为：");
+            show(a);
+            System.out.println("--------------");
             itemAmountOfSubGroup = itemAmountOfSubGroup / 3;
         }
     }
 
-    public static boolean less(Comparable v, Comparable w) {
-        return v.compareTo(w) < 0;
-    }
-
-    public static void exch(Comparable[] a, int i, int j) {
+    private static void exch(Comparable[] a, int i, int j) {
         Comparable temp = a[i];
         a[i] = a[j];
         a[j] = temp;
     }
 
-    public static void show(Comparable[] a) {
-        for (int i = 0; i < a.length; i++) {
-            System.out.print(a[i] + " ");
+    private static boolean less(Comparable[] a, int i, int j) {
+        return a[i].compareTo(a[j]) < 0;
+    }
+
+    private static void show(Comparable[] a) {
+        for (int cursor = 0; cursor < a.length; cursor++) {
+            System.out.print(a[cursor] + " ");
         }
+
+        System.out.println();
     }
 
     public static void main(String[] args) {
         String[] a = {"S", "H", "E", "L", "L", "S", "O", "R", "T", "E", "X", "A", "M", "P", "L", "E"};
+
         sort(a);
+
         show(a);
     }
 }
