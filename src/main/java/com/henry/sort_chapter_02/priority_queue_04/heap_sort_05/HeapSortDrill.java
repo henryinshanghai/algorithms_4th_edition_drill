5217ff7dbf53b01b681046c51b0802b47cb5eea3
HenryInSH <2291972433@qq.com>
2023-04-30 22:32:42 +0800

    --- 堆排序：#1 先把原始数组变成一个堆（sink from non-leaf node）; #2 再排定最大元素（via exch()）; #3 重建堆（sink 1st node），排定当前最大元素。---

--- src/main/java/com/henry/sort_chapter_02/priority_queue_04/heap_sort_05/HeapSortDrill.java
+++ src/main/java/com/henry/sort_chapter_02/priority_queue_04/heap_sort_05/HeapSortDrill.java
@@ -2,36 +2,33 @@ package com.henry.sort_chapter_02.priority_queue_04.heap_sort_05;
 
 import edu.princeton.cs.algs4.StdIn;
 
-// first, make the original array a heap, then heap sort it via arrange the biggest item and re-make the heap.
 public class HeapSortDrill {
 
-    public static void sort(Comparable[] a) {
-        // build a heap out of the original array
+    private static void sort(Comparable[] a) {
         int itemAmount = a.length;
 
-        for (int currentNodeSpot = itemAmount / 2; currentNodeSpot > 1; currentNodeSpot--) {
+        // #1 build a heap out of the array - via sink(currentNodeSpot)
+        for (int currentNodeSpot = itemAmount / 2; currentNodeSpot >= 1; currentNodeSpot--) { // 这里是 >=
             sink(a, currentNodeSpot, itemAmount);
         }
 
-        // heap-sort the array based on the heap
-        int cursorToLastNode = itemAmount;
-        while (cursorToLastNode > 1) {
-            // 排定最大元素
-            exch(a, 1, cursorToLastNode--);
-
-            // 重建堆
-            sink(a, 1, cursorToLastNode);
+        // #2 sort the items in the array - arrange the biggest item, and re-construct the heap.
+        int cursorToLastNodeSpot = itemAmount;
+        while (cursorToLastNodeSpot > 1) {
+            exch(a, 1, cursorToLastNodeSpot--);
+            sink(a, 1, cursorToLastNodeSpot);
         }
     }
 
     private static void sink(Comparable[] originalArray, int currentNodeSpot, int lastNodeSpot) {
         while (currentNodeSpot * 2 < lastNodeSpot) {
             int biggerChildSpot = currentNodeSpot * 2;
-            if (less(originalArray, biggerChildSpot, biggerChildSpot+1)) biggerChildSpot++;
+            if (less(originalArray, biggerChildSpot, biggerChildSpot + 1)) biggerChildSpot++;
 
-            if (!less(originalArray, currentNodeSpot, biggerChildSpot)) break;
+            if(!less(originalArray, currentNodeSpot, biggerChildSpot)) break;
 
             exch(originalArray, currentNodeSpot, biggerChildSpot);
+
             currentNodeSpot = biggerChildSpot;
         }
     }
@@ -46,17 +43,17 @@ public class HeapSortDrill {
         return originalArray[spotI - 1].compareTo(originalArray[spotJ - 1]) < 0;
     }
 
-    public static void show(Comparable[] a) {
+    private static void show(Comparable[] a) {
         for (int i = 0; i < a.length; i++) {
             System.out.print(a[i] + " ");
         }
     }
 
     public static void main(String[] args) {
-        String[] words = StdIn.readAllStrings();
+        String[] a = StdIn.readAllStrings();
 
-        HeapSortDrill.sort(words);
+        HeapSortDrill.sort(a);
 
-        show(words);
+        show(a);
     }
 }
