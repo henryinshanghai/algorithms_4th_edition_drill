528b397106cbb11e99dd0a3c9a813ee18c36c8f7
HenryInSH <2291972433@qq.com>
2023-08-20 18:19:36 +0800

    --- 集合数据类型Stack: 使用链表作为底层数据结构；   特性：由于在链表中获取下一个节点只能依靠于当前节点，所以迭代操作时需要一个currentNode的实例变量。 ---

--- src/main/java/com/henry/basic_chapter_01/collection_types/stack/via_linked_node/Stack.java
+++ src/main/java/com/henry/basic_chapter_01/collection_types/stack/via_linked_node/Stack.java
@@ -1,35 +1,21 @@
-package com.henry.basic_chapter_01.collection_types.stack_via_linked_node;
+package com.henry.basic_chapter_01.collection_types.stack.via_linked_node;
 
 import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
 
 import java.util.Iterator;
 
-// 以 链表作为底层数据结构 来 实现 ”泛型“、”可迭代“的下压栈数据结构
+// 目标：实现 动态容量、能够处理泛型参数、支持迭代元素操作的栈数据结构
+// 数据类型的预期特征：{#1 支持泛型类型; #2 栈的容量是能够动态变化的; #3 支持对集合中元素的迭代操作}
+// 实现手段：使用链表作为底层的数据结构
 // 执行手段：redirect input from <tobe.txt>
-public class StackViaNodeTemplate<Item> implements Iterable<Item> {
-
-    public static void main(String[] args) {
-        StackViaNodeTemplate<String> myStackViaNodeTemplate = new StackViaNodeTemplate<>();
-
-        while (!StdIn.isEmpty()) {
-            String item = StdIn.readString();
-
-            if (!item.equals("-")) {
-                myStackViaNodeTemplate.push(item);
-            } else if (!myStackViaNodeTemplate.isEmpty()) {
-                StdOut.print(myStackViaNodeTemplate.pop() + " ");
-            }
-        }
-
-        StdOut.println("(" + myStackViaNodeTemplate.size() + " left on stack)");
-    }
+public class Stack<Item> implements Iterable<Item> {
 
     // 底层使用的数据结构 - 链表
     private Node topNode;
     private int itemAmount;
 
-
+    // 用于表示链表/链表中节点的数据类型 - 这里出于方便目的，把它定义成为内部类
     private class Node {
         private Item item;
         Node nextNode;
@@ -43,6 +29,7 @@ public class StackViaNodeTemplate<Item> implements Iterable<Item> {
         return itemAmount;
     }
 
+    // 入栈操作 - 向链表中添加一个新的节点，并将之作为新的起始节点
     public void push(Item item) {
         // 记录当前链表中的起始节点
         Node previousTopNode = topNode;
@@ -55,6 +42,7 @@ public class StackViaNodeTemplate<Item> implements Iterable<Item> {
         itemAmount++;
     }
 
+    // 出栈操作 - 移除链表中的第一个节点，并返回节点中的元素
     public Item pop() {
         Item item = topNode.item;
 
@@ -73,6 +61,7 @@ public class StackViaNodeTemplate<Item> implements Iterable<Item> {
     private class MyStackIterator implements Iterator<Item> {
 
         // 为了使 迭代的操作 不会干扰 链表上的原始指针，这里添加一个 原始链表的副本 来 作为实例变量
+        // 原因：与数组不同，在链表中获取下一个节点，只能通过“当前节点”的下一个指针
         private Node currentNode = topNode;
 
         @Override
@@ -86,5 +75,21 @@ public class StackViaNodeTemplate<Item> implements Iterable<Item> {
             currentNode = currentNode.nextNode;
             return item;
         }
+    }
+
+    public static void main(String[] args) {
+        Stack<String> myStackViaNodeTemplate = new Stack<>();
+
+        while (!StdIn.isEmpty()) {
+            String item = StdIn.readString();
+
+            if (!item.equals("-")) {
+                myStackViaNodeTemplate.push(item);
+            } else if (!myStackViaNodeTemplate.isEmpty()) {
+                StdOut.print(myStackViaNodeTemplate.pop() + " ");
+            }
+        }
+
+        StdOut.println("(" + myStackViaNodeTemplate.size() + " left on stack)");
     }
 }
