efdcb77d2004d355792420858813aedfb4a74b25
HenryInSH <2291972433@qq.com>
2023-08-20 11:33:35 +0800

    --- 方法中的局部变量：要先初始化，才能使用 否则会有编译报错 ---

--- src/main/java/com/henry/basic_chapter_01/java_basic/requireInitVariableDemo.java
+++ src/main/java/com/henry/basic_chapter_01/java_basic/requireInitVariableDemo.java
@@ -2,7 +2,7 @@ package com.henry.basic_chapter_01.java_basic;
 
 // 验证：如果方法的局部变量没有被初始化，则：使用此变量时，Java编译器会给出编译错误；
 // 手段：如果删除 第8行中 = 0，则：在第10行 编译器报错 Variable 'number' might not have been initialized
-public class requestInitVariableDemo {
+public class requireInitVariableDemo {
 
     public void calculate() {
         int number = 0;
