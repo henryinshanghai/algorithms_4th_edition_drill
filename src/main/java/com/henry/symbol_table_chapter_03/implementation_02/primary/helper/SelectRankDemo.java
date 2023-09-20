5daff557f514a84d50fafde3346b282f1b807301
HenryInSH <2291972433@qq.com>
2023-09-20 08:48:56 +0800

    --- 有序数组中rank()方法的实现：#1 循环手段 - 找到当前区间的中间位置 与 传入的key做比较，并根据比较结果返回对应的位置 或者 更新区间； #2 递归手段 - 同理做出比较，并根据比较结果 来 返回对应的位置 或者 递归调用更小的区间。 ---

--- src/main/java/com/henry/symbol_table_chapter_03/implementation_02/primary/helper/SelectRankDemo.java
+++ src/main/java/com/henry/symbol_table_chapter_03/implementation_02/primary/helper/SelectRankDemo.java
@@ -1,6 +1,7 @@
 package com.henry.symbol_table_chapter_03.implementation_02.primary.helper;
 
 public class SelectRankDemo<Key extends Comparable<Key>> {
+    // 底层数据结构：一个有序的keys数组
     private Key[] keys;
 
     public SelectRankDemo(Key[] passedArray) {
@@ -12,19 +13,23 @@ public class SelectRankDemo<Key extends Comparable<Key>> {
         }
     }
 
-    public Key select(int ranking) {
-        return keys[ranking];
+    // 获取到/选择出 数组中指定排名的key
+    public Key selectOutKeyWhoseRankIs(int passedRanking) {
+        return keys[passedRanking];
     }
 
     // 排名算法的基础是二分排序 依赖于有序的数组
     // 迭代方式的实现
-    public int rank(Key passedKey) {
+    public int rankOfViaLoop(Key passedKey) {
+        // 闭区间
         int leftBar = 0;
         int rightBar = keys.length - 1;
 
         while (leftBar <= rightBar) {
+            // 区间的中间位置
             int middle = leftBar + (rightBar - leftBar) / 2;
 
+            // 根据比较结果 来 决定 passedKey的排名
             int compareResult = keys[middle].compareTo(passedKey);
             if (compareResult < 0) leftBar = middle + 1;
             else if(compareResult > 0) rightBar = middle - 1;
@@ -34,15 +39,16 @@ public class SelectRankDemo<Key extends Comparable<Key>> {
         return leftBar; // 当符号表中不存在传入的键时，返回的是：小于它的键的数量
     }
 
-    // 递归方式的实现 - 计算key在 [leftBar, rightBar]区间中的排名
-    public int rank(Key key, int leftBar, int rightBar) {
+    // 递归方式的实现
+    // 把任务泛化： 计算key在数组中的排名 -> 计算key在 数组的[leftBar, rightBar]区间中的排名
+    public int rankOfViaRecursion(Key key, int leftBar, int rightBar) {
         if (leftBar > rightBar) return leftBar; // 当符号表中不存在传入的键时，返回的是：小于它的键的数量
 
         int middle = leftBar + (rightBar - leftBar) / 2;
         int result = key.compareTo(keys[middle]);
 
-        if (result > 0) return rank(key, middle + 1, rightBar);
-        else if (result < 0) return rank(key, leftBar, middle - 1);
+        if (result > 0) return rankOfViaRecursion(key, middle + 1, rightBar);
+        else if (result < 0) return rankOfViaRecursion(key, leftBar, middle - 1);
         else return middle;
     }
 
@@ -51,11 +57,11 @@ public class SelectRankDemo<Key extends Comparable<Key>> {
         // 排名第N的元素 在数组中的排名一定是N
         // ranking = rank(select(ranking))
         for (int currentRank = 0; currentRank < keys.length; currentRank++)
-            if (currentRank != rank(select(currentRank))) return false;
+            if (currentRank != rankOfViaLoop(selectOutKeyWhoseRankIs(currentRank))) return false;
 
         // key = select(rank(key))
         for (int currentSpot = 0; currentSpot < keys.length; currentSpot++)
-            if (keys[currentSpot].compareTo(select(rank(keys[currentSpot]))) != 0) return false;
+            if (keys[currentSpot].compareTo(selectOutKeyWhoseRankIs(rankOfViaLoop(keys[currentSpot]))) != 0) return false;
 
         return true;
     }
