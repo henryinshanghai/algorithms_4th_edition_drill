a162cbb56c65939e79d1b20caa7b94112cefd1a2
HenryInSH <2291972433@qq.com>
2023-11-12 22:48:51 +0800

    --- 低位优先的字符串排序 拾遗：#1 使用当前字符作为groupNo; #2 键->索引数组的初始大小为 biggestGroupNo+1； #3 真正需要使用到的元素范围为 [0, biggestGroupNo]  ---

--- src/main/java/com/henry/string_05/string_sorting_01/LSD_02/LSDLite.java
+++ src/main/java/com/henry/string_05/string_sorting_01/LSD_02/LSDLite.java
@@ -1,21 +1,21 @@
 package com.henry.string_05.string_sorting_01.LSD_02;
 
-// 原理：TBD
-// 基础原理：TBD
+// 原理：键索引计数法的稳定性 + 归纳法； 组间排序 & 组内排序
+// 基础原理：使用字符作为键时，字符在字符表中的顺序是确定的。
 public class LSDLite {
 
     // 使用低位优先的方式，对定长字符串的集合进行排序
     public static void sort(String[] plateArr, int plateLength) {
         // 通过前W个字符，把a[]排序
         int plateAmount = plateArr.length;
-        int biggestGroupNoPlus1 = 256;
+        int biggestGroupNoPlus1 = 256; // 最大组号为255
 
         String[] aux = new String[plateAmount];
 
         for (int backwardsCursor = plateLength - 1; backwardsCursor >= 0; backwardsCursor--) {
             /* #1  统计各个组号出现的频率,得到 groupNoPlus1ToGroupSizeArr[] */
             // 使用第 backwardsCursor 个字符，来 作为 “键索引计数法排序中的groupNo/键”
-            int[] groupNoToItsStartIndexArr = new int[biggestGroupNoPlus1 + 1];
+            int[] groupNoToItsStartIndexArr = new int[biggestGroupNoPlus1 + 1]; // 键->索引的数组初始大小为 最大组号/key + 1 + 1
 
             for (int currentSlot = 0; currentSlot < plateAmount; currentSlot++) {
                 // 获取到待排序元素的groupNo - 使用：当前字符 来 作为groupNo
