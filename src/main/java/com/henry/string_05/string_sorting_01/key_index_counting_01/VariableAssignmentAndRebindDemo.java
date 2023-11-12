c1499d85818218a8d7a7a3b0753fd3ecab375016
HenryInSH <2291972433@qq.com>
2023-11-12 17:36:39 +0800

    --- Java基础：#1 变量之间的绑定，只是为内存地址添加了一个新的别名; #2 为变量绑定新的值，只是使变量指向了新的内存地址。---

--- src/main/java/com/henry/string_05/string_sorting_01/key_index_counting_01/VariableAssignmentAndRebindDemo.java
+++ src/main/java/com/henry/string_05/string_sorting_01/key_index_counting_01/VariableAssignmentAndRebindDemo.java
@@ -7,25 +7,27 @@ public class VariableAssignmentAndRebindDemo {
 
         String[] names = {"henry", "jiaming", "wanxiong", "xinrui"};
 
-        // 变量之间的绑定
+        // 变量之间的绑定 - 变量是一个数组元素
         String alias = names[0];
-        System.out.println(alias);
-        System.out.println(names[0]);
+        System.out.println(alias); // henry 表示alias指向henry这个值
+        System.out.println(names[0]); // henry 表示 names[0] 指向henry这个值
 
         // 为变量绑定新的值
         alias = "kelly";
-        System.out.println(alias);
-        System.out.println(names[0]);
+        System.out.println(alias); // kelly 表示alias指向kelly这个值
+        System.out.println(names[0]); // henry 表示 names[0]仍旧指向henry这个值
 
+        // 直接对数组元素执行++操作
         int[] nums = {1, 2, 3, 4, 5};
         for (int i = 0; i < nums.length; i++) {
             nums[i]++;
         }
         for (int num : nums) {
             System.out.print(num + " ");
-        }
+        } // 2 3 4 5 6
         System.out.println();
 
+        // 把数组元素绑定到一个新的变量上后，对新变量执行++操作
         int[] vals = {1, 2, 3, 4, 5};
         for (int i = 0; i < vals.length; i++) {
             int val = vals[i];
@@ -33,7 +35,7 @@ public class VariableAssignmentAndRebindDemo {
         }
         for (int val : vals) {
             System.out.print(val + " ");
-        }
+        } // 1 2 3 4 5 数组的元素没有发生任何变化
         System.out.println();
     }
 }
