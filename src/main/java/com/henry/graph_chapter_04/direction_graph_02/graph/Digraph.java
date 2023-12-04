952e6d84690ddd6ee28860c6244ff0937a041324
HenryInSH <2291972433@qq.com>
2023-12-04 19:10:51 +0800

    --- 结论：可以使用邻接表 来 实现“有向图”这种逻辑结构。 ---

--- src/main/java/com/henry/graph_chapter_04/direction_graph_02/graph/Digraph.java
+++ src/main/java/com/henry/graph_chapter_04/direction_graph_02/graph/Digraph.java
@@ -3,13 +3,16 @@ package com.henry.graph_chapter_04.direction_graph_02.graph;
 import edu.princeton.cs.algs4.Bag;
 import edu.princeton.cs.algs4.In;
 
+// 结论：可以使用邻接表 来 实现有向图这种逻辑结构；
+// 用法：在构造方法中，读取 结点数量、边的数量、构成每条边的两个结点，然后使用 addEdge()来构造出完整的图
+// 特征：相比于无向图，新增了一个 reverseEdgeDirection()的API，用于 得到原始有向图的反向图
 public class Digraph {
     private final int vertexAmount;
     private int edgeAmount;
     private Bag<Integer>[] vertexToAdjacentVertexes;
 
     public Digraph(In in) {
-        this(in.readInt()); // 这个语句必须是构造方法中的第一个语句
+        this(in.readInt()); // 🐖 this()语句（对其他构造方法调用的快捷方式） 必须作为 构造方法中的第一个语句
 
         int edgeAmount = in.readInt(); // 局部变量用来存储图中总的边的数量
 //        this.edgeAmount = in.readInt(); // 成员变量用来记录当前构建的图中的边的数量
@@ -51,6 +54,7 @@ public class Digraph {
         return vertexToAdjacentVertexes[vertexV];
     }
 
+    // 反转有向图中边的方向，得到其反向图
     public Digraph reverseEdgeDirection() {
         Digraph edgeReversedGraph = new Digraph(vertexAmount);
         // 🐖 DFS中标准的结点遍历方式 - 按照自然数的顺序 来 遍历 有向图中的结点
