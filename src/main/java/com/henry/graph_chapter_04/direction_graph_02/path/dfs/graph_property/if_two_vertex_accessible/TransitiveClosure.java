513db38e7a7f81360a604138635fcb25a9362cd5
HenryInSH <2291972433@qq.com>
2023-12-04 19:37:45 +0800

    --- 有向图任意两个顶点之间的有向可达性  结论：对于有向图中顶点之间的连通性问题，可以使用方形的结点矩阵 来 为每一个结点，建立其可以达到的结点列表； 手段：使用DFS算法，就能够 为指定的结点，搜索&收集到其 可以达到的所有顶点；---

--- src/main/java/com/henry/graph_chapter_04/direction_graph_02/path/dfs/graph_property/if_two_vertex_accessible/TransitiveClosure.java
+++ src/main/java/com/henry/graph_chapter_04/direction_graph_02/path/dfs/graph_property/if_two_vertex_accessible/TransitiveClosure.java
@@ -1,9 +1,11 @@
-package com.henry.graph_chapter_04.direction_graph_02.path.dfs.graph_property;
+package com.henry.graph_chapter_04.direction_graph_02.path.dfs.graph_property.if_two_vertex_accessible;
 
 import com.henry.graph_chapter_04.direction_graph_02.graph.Digraph;
-import com.henry.graph_chapter_04.direction_graph_02.if_accessible.DirectedDFS;
+import com.henry.graph_chapter_04.direction_graph_02.if_accessible_from_startVertex.DirectedDFS;
 
-// 有向图中，顶点对的可达性
+// 结论：对于有向图中顶点之间的连通性问题，可以使用方形的结点矩阵 来 为每一个结点，建立其可以达到的结点列表；
+// 手段：使用DFS算法，就能够 为指定的结点，搜索&收集到其 可以达到的所有顶点；
+// 用法：把顶点对中的其中一个顶点作为startVertex，另外一个顶点作为 passedVertex，就能够 使用DFS对象的API 来 判断两个结点是否连通
 public class TransitiveClosure {
     private DirectedDFS[] vertexToItsAccessibleVertexes;
 
@@ -15,6 +17,7 @@ public class TransitiveClosure {
         }
     }
 
+    // 原始问题：判断图中的 v->w是否存在
     boolean reachable(int vertexV, int vertexW) {
         int startVertex = vertexV;
         DirectedDFS markedDigraph = vertexToItsAccessibleVertexes[startVertex];
