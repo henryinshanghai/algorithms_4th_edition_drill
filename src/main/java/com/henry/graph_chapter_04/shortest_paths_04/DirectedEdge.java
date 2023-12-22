33563665c53e4902f1c9ac72293bafd0ede420e1
HenryInSH <2291972433@qq.com>
2023-12-22 16:10:24 +0800

    --- 最短路径树所依赖的数据结构：#1 有向边（departVertex -> terminalVertex）； #2 加权有向图（vertexToItsAssociatedEdges） ---

---
+++ src/main/java/com/henry/graph_chapter_04/shortest_paths_04/DirectedEdge.java
@@ -0,0 +1,99 @@
+package com.henry.graph_chapter_04.shortest_paths_04;
+
+/******************************************************************************
+ *  Compilation:  javac DirectedEdge.java
+ *  Execution:    java DirectedEdge
+ *  Dependencies: StdOut.java
+ *
+ *  Immutable weighted directed edge.
+ *
+ ******************************************************************************/
+
+import edu.princeton.cs.algs4.StdOut;
+
+/**
+ * The {@code DirectedEdge} class represents a weighted edge in an
+ * {@link EdgeWeightedDigraph}. Each edge consists of two integers
+ * (naming the two vertices) and a real-value weight. The data type
+ * provides methods for accessing the two endpoints of the directed edge and
+ * the weight.
+ * <p>
+ * For additional documentation,
+ * see <a href="https://algs4.cs.princeton.edu/44sp">Section 4.4</a> of
+ * <i>Algorithms, 4th Edition</i> by Robert Sedgewick and Kevin Wayne.
+ *
+ * @author Robert Sedgewick
+ * @author Kevin Wayne
+ */
+
+public class DirectedEdge {
+    private final int departVertex;
+    private final int terminalVertex;
+    private final double weight;
+
+    /**
+     * Initializes a directed edge from vertex {@code v} to vertex {@code w} with
+     * the given {@code weight}.
+     *
+     * @param departVertex   the tail vertex
+     * @param terminalVertex the head vertex
+     * @param weight         the weight of the directed edge
+     * @throws IllegalArgumentException if either {@code v} or {@code w}
+     *                                  is a negative integer
+     * @throws IllegalArgumentException if {@code weight} is {@code NaN}
+     */
+    public DirectedEdge(int departVertex, int terminalVertex, double weight) {
+        if (departVertex < 0) throw new IllegalArgumentException("Vertex names must be non-negative integers");
+        if (terminalVertex < 0) throw new IllegalArgumentException("Vertex names must be non-negative integers");
+        if (Double.isNaN(weight)) throw new IllegalArgumentException("Weight is NaN");
+        this.departVertex = departVertex;
+        this.terminalVertex = terminalVertex;
+        this.weight = weight;
+    }
+
+    /**
+     * Returns the tail vertex of the directed edge.
+     *
+     * @return the tail vertex of the directed edge
+     */
+    public int departVertex() {
+        return departVertex;
+    }
+
+    /**
+     * Returns the head vertex of the directed edge.
+     *
+     * @return the head vertex of the directed edge
+     */
+    public int terminalVertex() {
+        return terminalVertex;
+    }
+
+    /**
+     * Returns the weight of the directed edge.
+     *
+     * @return the weight of the directed edge
+     */
+    public double weight() {
+        return weight;
+    }
+
+    /**
+     * Returns a string representation of the directed edge.
+     *
+     * @return a string representation of the directed edge
+     */
+    public String toString() {
+        return departVertex + "->" + terminalVertex + " " + String.format("%5.2f", weight);
+    }
+
+    /**
+     * Unit tests the {@code DirectedEdge} data type.
+     *
+     * @param args the command-line arguments
+     */
+    public static void main(String[] args) {
+        DirectedEdge e = new DirectedEdge(12, 34, 5.67);
+        StdOut.println(e);
+    }
+}
