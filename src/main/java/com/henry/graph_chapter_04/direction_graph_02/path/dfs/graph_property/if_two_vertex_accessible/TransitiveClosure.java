39da8f2ef72b96ae98b6a623028ce8c1bf733171
HenryInSH <2291972433@qq.com>
2024-01-06 15:07:24 +0800

    --- 判断有向图中给定的两个结点是否相互连通：先取其中任意一个结点作为startVertex，获取到“以此startVertex作为起点在图中DFS得到的连通子图”。然后使用 连通子图的API 来 判断“是否能够到达另一个结点” ---

--- src/main/java/com/henry/graph_chapter_04/direction_graph_02/path/dfs/graph_property/if_two_vertex_accessible/TransitiveClosure.java
+++ src/main/java/com/henry/graph_chapter_04/direction_graph_02/path/dfs/graph_property/if_two_vertex_accessible/TransitiveClosure.java
@@ -3,6 +3,7 @@ package com.henry.graph_chapter_04.direction_graph_02.path.dfs.graph_property.if
 import com.henry.graph_chapter_04.direction_graph_02.graph.Digraph;
 import com.henry.graph_chapter_04.direction_graph_02.if_accessible_from_startVertex.DirectedDFS;
 
+// 验证：可以使用/通过 为每个图中的每个结点都建立一个 与其连通的结点集合的方式 来 回答“图中指定的两个顶点是否相连通”的问题；
 // 结论：对于有向图中顶点之间的连通性问题，可以使用方形的结点矩阵 来 为每一个结点，建立其可以达到的结点列表；
 // 手段：使用DFS算法，就能够 为指定的结点，搜索&收集到其 可以达到的所有顶点；
 // 用法：把顶点对中的其中一个顶点作为startVertex，另外一个顶点作为 passedVertex，就能够 使用DFS对象的API 来 判断两个结点是否连通
