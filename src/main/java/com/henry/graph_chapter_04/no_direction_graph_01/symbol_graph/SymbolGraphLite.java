a40c37f1b6b17a4cbb1cb324c64b20dafbfdb7f9
HenryInSH <2291972433@qq.com>
2024-01-06 21:24:49 +0800

    --- 实现符号图(结点中的值是一个字符串)的手段：可以使用 “简单图(vertexIndexGraph) + 符号表(vertexStrToVertexIndexST)”的方式 来 实现这个数据结构。 ---

--- src/main/java/com/henry/graph_chapter_04/no_direction_graph_01/symbol_graph/SymbolGraphLite.java
+++ src/main/java/com/henry/graph_chapter_04/no_direction_graph_01/symbol_graph/SymbolGraphLite.java
@@ -2,21 +2,22 @@ package com.henry.graph_chapter_04.no_direction_graph_01.symbol_graph;
 
 import edu.princeton.cs.algs4.*;
 
+// 验证：可以使用 符号表 与 简单图(图结点的值是整数) 来 实现符号图（图结点的值是字符串）
 public class SymbolGraphLite {
     /* 实现 符号图 所需要的底层数据结构 */
     // 符号表 用于 记录节点值 -> 索引值 的映射
-    private ST<String, Integer> keyStrToVertexIndexMap;
+    private ST<String, Integer> vertexStrToVertexIndexST;
     // 字符串数组 用于 记录索引值 -> 节点值 的关系    作用与st刚好相反
-    private String[] vertexIndexToKeyStrArr;
+    private String[] vertexIndexToVertexStrArr;
     // 从文件中构建出的图 用于索引图中的顶点
-    private Graph vertexIndexConnectionGraph;
+    private Graph vertexIndexGraph;
 
     /* constructor */
     // 参数列表： 文件流对象、文件分隔符字符串
     public SymbolGraphLite(String fileName, String delimiter) {
         /* 初始化成员变量 */
         // 初始化符号表 👇
-        keyStrToVertexIndexMap = new ST<>();
+        vertexStrToVertexIndexST = new ST<>();
         In fileStream = new In(fileName);
         // 逐行读取文件...
         while (fileStream.hasNextLine()) {
@@ -28,35 +29,35 @@ public class SymbolGraphLite {
             for (int currentSpot = 0; currentSpot < keyStrArr.length; currentSpot++) {
                 String currentKeyStr = keyStrArr[currentSpot];
 
-                if (!keyStrToVertexIndexMap.contains(currentKeyStr)) { // 避免重复添加
-                    keyStrToVertexIndexMap.put(currentKeyStr, keyStrToVertexIndexMap.size());
+                if (!vertexStrToVertexIndexST.contains(currentKeyStr)) { // 避免重复添加
+                    vertexStrToVertexIndexST.put(currentKeyStr, vertexStrToVertexIndexST.size());
                 }
             }
         }
 
         // 初始化字符串数组indexToKeyStringArr中的元素 👇
-        vertexIndexToKeyStrArr = new String[keyStrToVertexIndexMap.size()];
+        vertexIndexToVertexStrArr = new String[vertexStrToVertexIndexST.size()];
 
-        for (String keyStr : keyStrToVertexIndexMap.keys()) {
+        for (String keyStr : vertexStrToVertexIndexST.keys()) {
             // keys[<索引值>] = <节点的值>
-            Integer indexOfKey = keyStrToVertexIndexMap.get(keyStr);
-            vertexIndexToKeyStrArr[indexOfKey] = keyStr;
+            Integer indexOfKey = vertexStrToVertexIndexST.get(keyStr);
+            vertexIndexToVertexStrArr[indexOfKey] = keyStr;
         }
 
         // 初始化图G 👇
         // #1 初始化图中的节点数量
-        vertexIndexConnectionGraph = new Graph(keyStrToVertexIndexMap.size());
+        vertexIndexGraph = new Graph(vertexStrToVertexIndexST.size());
         fileStream = new In(fileName);
 
         while (fileStream.hasNextLine()) {
             String[] keyStrArr = fileStream.readLine().split(delimiter);
             String firstKeyStr = keyStrArr[0];
-            int indexOfFirstKeyStr = keyStrToVertexIndexMap.get(firstKeyStr);
+            int indexOfFirstKeyStr = vertexStrToVertexIndexST.get(firstKeyStr);
 
             // 对于当前行其余的keyStr...
             for (int currentSpot = 1; currentSpot < keyStrArr.length; currentSpot++) {
                 // #2 构造 index -> index的边，并添加到图中
-                vertexIndexConnectionGraph.addEdge(indexOfFirstKeyStr, keyStrToVertexIndexMap.get(keyStrArr[currentSpot]));
+                vertexIndexGraph.addEdge(indexOfFirstKeyStr, vertexStrToVertexIndexST.get(keyStrArr[currentSpot]));
             }
         }
     }
@@ -64,22 +65,22 @@ public class SymbolGraphLite {
     /* public APIs */
     // 图中 是否存在 某个节点的值 为指定的字符串
     public boolean containsVertexWithName(String passedName) {
-        return keyStrToVertexIndexMap.contains(passedName);
+        return vertexStrToVertexIndexST.contains(passedName);
     }
 
     // 指定的字符串在图中的索引位置是多少？
     public int indexOfVertexWithName(String name) {
-        return keyStrToVertexIndexMap.get(name);
+        return vertexStrToVertexIndexST.get(name);
     }
 
     // 指定索引位置上节点的值是什么？
     public String nameOfVertexWithIndex(int passedIndex) {
-        return vertexIndexToKeyStrArr[passedIndex];
+        return vertexIndexToVertexStrArr[passedIndex];
     }
 
     // 实现符号图的 底层的一般图 的字符串表示
     public Graph underlyingGraph() {
-        return vertexIndexConnectionGraph;
+        return vertexIndexGraph;
     }
 
     public static void main(String[] args) {
@@ -87,6 +88,7 @@ public class SymbolGraphLite {
         String delimiter = args[1];
         SymbolGraphLite symbolGraph = new SymbolGraphLite(filename, delimiter);
 
+        // 这里向用例 暴露了其底层实现 - 这不是很好的设计~
         Graph underlyingGraph = symbolGraph.underlyingGraph();
 
         // 获取 符号图 的各种性质
