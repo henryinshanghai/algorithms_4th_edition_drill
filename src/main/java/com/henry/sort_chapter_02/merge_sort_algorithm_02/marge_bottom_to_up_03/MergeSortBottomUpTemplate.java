96e36e2815e3335ac375948dafb3e55fa3696f64
HenryInSH <2291972433@qq.com>
2024-01-06 23:50:52 +0800

    --- 通用排序算法之自底向上的归并排序：#1 初始化unitSize=1,使用它来顺序地“成对归并”数组中的所有元素；#2 倍增unitSize，再次顺序地“成对归并”数组中的所有元素 - 重复#2，直到unitSize>=itemAmount/2。（🐖 正确计算归并currentPair时的左边界、中间位置、右边界） ---

--- src/main/java/com/henry/sort_chapter_02/merge_sort_algorithm_02/marge_bottom_to_up_03/MergeSortBottomUpTemplate.java
+++ src/main/java/com/henry/sort_chapter_02/merge_sort_algorithm_02/marge_bottom_to_up_03/MergeSortBottomUpTemplate.java
@@ -30,10 +30,10 @@ package com.henry.sort_chapter_02.merge_sort_algorithm_02.marge_bottom_to_up_03;
         #3 更新（倍增）N，重复#1、#2，直到（子组的元素数量 >= 原始数组的元素数量）得到“完全排序的数组”
 
     有意义的变量名：
-        #1 按照特定的size对原始数组进行分割 - itemAmountOfSubGroup
+        #1 按照特定大小的unit对原始数组进行分割 - itemAmountOfSubGroup/blockSize/unitSize
         #2 当前参与归并的两个子数组所组成的对 - currentPair
-        #2 参与归并的区间左边界 - leftBarCursor
-        #3 参与归并的区间右边界 - rightBarCursor
+        #2 参与归并的区间左边界 - leftBarOfPair
+        #3 参与归并的区间右边界 - rightBarOfPair
         #4 原始数组的元素数量 - itemAmount
         #5 归并区间的中间位置 - middle
  */
@@ -41,57 +41,59 @@ public class MergeSortBottomUpTemplate {
     private static Comparable[] aux;
 
     // 使用自底向上的方式 来 实现对数组完全排序
-    public static void sort(Comparable[] a) {
-        int itemAmount = a.length;
+    public static void sort(Comparable[] originalArr) {
+        int itemAmount = originalArr.length;
         aux = new Comparable[itemAmount];
 
         // #3 更新（倍增）N，重复#1、#2，直到（子组的元素数量 >= 原始数组的元素数量）得到“完全排序的数组”
-        for (int blockSize = 1; blockSize < itemAmount; blockSize = blockSize * 2) {
-            // #2 按照当前 blockSize 分组后，对得到的子数组 从左到右 执行两两归并；
-            // 手段：先对当前Pair执行归并操作，再更新指针，对新的Pair执行归并操作。直到 原始数组中的最后一个Pair
-            for (int leftBarOfCurrentPair = 0; leftBarOfCurrentPair < itemAmount - blockSize; leftBarOfCurrentPair += (blockSize * 2)) {
+        for (int unitSize = 1; unitSize < itemAmount; unitSize = unitSize * 2) { // #1 初始化unitSize为1，在所有pair都归并完成后，倍增unitSize
+            // #2 按照当前 unitSize 分割后，对得到的子数组 从左到右 执行两两归并；
+            // 手段：先对当前Pair执行归并操作，再更新指针的位置，对新的Pair执行归并操作。直到 原始数组中的最后一个Pair
+            for (int leftBarOfCurrentPair = 0; leftBarOfCurrentPair < itemAmount - unitSize; leftBarOfCurrentPair += (unitSize * 2)) {
                 // 随着currentPair被不断更新，rightBarCursor可能会超出原始数组的边界。因此这里使用min()
-                mergeBlocksInPair(a, leftBarOfCurrentPair, leftBarOfCurrentPair + blockSize - 1,
-                        Math.min((leftBarOfCurrentPair + blockSize * 2) - 1, itemAmount - 1));
+                mergeUnitsInPair(originalArr, leftBarOfCurrentPair, leftBarOfCurrentPair + unitSize - 1,
+                        Math.min((leftBarOfCurrentPair + unitSize * 2) - 1, itemAmount - 1));
             }
         }
     }
 
-    // 归并指定闭区间中的元素
+    // 归并数组中指定闭区间中的元素
     // 特征：a[leftBar, middle] 与 a[middle+1, rightBar] - 均为闭区间
-    private static void mergeBlocksInPair(Comparable[] a, int leftBarOfPair, int middleOfPair, int rightBarOfPair) {
-        // 1
-        for (int cursor = leftBarOfPair; cursor <= rightBarOfPair; cursor++) {
-            aux[cursor] = a[cursor];
+    private static void mergeUnitsInPair(Comparable[] originalArr, int leftBarOfPair, int middleOfPair, int rightBarOfPair) {
+        // #1 把原始数组中“指定区间”中的元素，拷贝到辅助数组aux中
+        for (int currentSpotCursor = leftBarOfPair; currentSpotCursor <= rightBarOfPair; currentSpotCursor++) {
+            aux[currentSpotCursor] = originalArr[currentSpotCursor];
         }
 
-        // 2
+        // #2 准备左右游标指针 - 用于比较元素，得到“正确的元素”
         int leftHalfCursor = leftBarOfPair;
         int rightHalfCursor = middleOfPair + 1;
 
-        // 3
-        for (int cursor = leftBarOfPair; cursor <= rightBarOfPair; cursor++) {
-            if (leftHalfCursor > middleOfPair) a[cursor] = aux[rightHalfCursor++];
-            else if (rightHalfCursor > rightBarOfPair) a[cursor] = aux[leftHalfCursor++];
-            else if (less(aux[leftHalfCursor], aux[rightHalfCursor])) a[cursor] = aux[leftHalfCursor++];
-            else a[cursor] = aux[rightHalfCursor++];
+        // #3 对于原始数组中的“当前待排定的位置”...
+        for (int currentSpotToArrange = leftBarOfPair; currentSpotToArrange <= rightBarOfPair; currentSpotToArrange++) {
+            // 比较辅助数组中，左右指针所指向的元素。然后把“较小的元素” 绑定到 原始数组“待排定的位置”上
+            if (leftHalfCursor > middleOfPair) originalArr[currentSpotToArrange] = aux[rightHalfCursor++];
+            else if (rightHalfCursor > rightBarOfPair) originalArr[currentSpotToArrange] = aux[leftHalfCursor++];
+            else if (less(aux[leftHalfCursor], aux[rightHalfCursor])) originalArr[currentSpotToArrange] = aux[leftHalfCursor++];
+            else originalArr[currentSpotToArrange] = aux[rightHalfCursor++];
         }
     }
 
-    private static boolean less(Comparable v, Comparable w) {
-        return v.compareTo(w) < 0;
+    private static boolean less(Comparable itemV, Comparable itemW) {
+        return itemV.compareTo(itemW) < 0;
     }
 
     public static void printItems(Comparable[] a) {
-        int N = a.length;
+        int itemAmount = a.length;
 
-        for (int cursor = 0; cursor < N; cursor++) {
-            System.out.print(a[cursor] + " ");
+        for (int currentSpotCursor = 0; currentSpotCursor < itemAmount; currentSpotCursor++) {
+            System.out.print(a[currentSpotCursor] + " ");
         }
 
         System.out.println();
     }
 
+    // 单元测试 - 测试sort()方法是否实现了预期的功能
     public static void main(String[] args) {
         String[] a = new String[]{"M", "E", "R", "G", "E", "S", "O", "R", "T", "E", "X", "A", "M", "P", "L", "E"};
         sort(a);
