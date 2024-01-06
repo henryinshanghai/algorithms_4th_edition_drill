71544bfe553706bb315ac70087fd470898e0d1bb
HenryInSH <2291972433@qq.com>
2024-01-06 15:20:04 +0800

    --- 判断一幅无向图是不是一个连通图：指定一个startVertex，从它开始在图中执行DFS来标记结点。如果 最终标记的结点数量 = 原始图中的总结点数量，则：原始的无向图是连通图。否则不是 ---

--- src/main/java/com/henry/graph_chapter_04/no_direction_graph_01/if_connect/DepthFirstSearch.java
+++ src/main/java/com/henry/graph_chapter_04/no_direction_graph_01/if_connect/DepthFirstSearch.java
@@ -4,6 +4,7 @@ import com.henry.graph_chapter_04.no_direction_graph_01.graph.Graph;
 import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdOut;
 
+// 验证：可以使用在图中从指定起点开始进行DFS（标记结点）的方式 来 判断给定的一幅图 是不是连通图
 // 任务：遍历图中的每一个顶点、边，以实现某些更具体的作用
 // 命令行参数：E:\development_project\algorithms_4th_edition_drill\src\main\java\com\henry\graph_chapter_04\no_direction_graph_01\dfs\tinyG 0
 public class DepthFirstSearch {
@@ -19,13 +20,13 @@ public class DepthFirstSearch {
         // 初始化 boolean数组 起始默认为false
         vertexToIsMarkedArr = new boolean[graph.V()];
         // 调用dfs 在遍历的同时：标记顶点、检查顶点
-        dfs(graph, startVertex);
+        markVertexAndCountsViaDFS(graph, startVertex);
     }
 
     // 如何验证 通过这种方式能够遍历图中所有的顶点与边？
     // 深度优先搜索 只能够找到 图G中所有 与顶点s相连通的节点集合
     // 算法步骤：一边遍历，一边标记
-    private void dfs(Graph graph, int currentVertex) {
+    private void markVertexAndCountsViaDFS(Graph graph, int currentVertex) {
         // 标记当前顶点“已经被访问过”
         vertexToIsMarkedArr[currentVertex] = true;
         // 记录“被标记的节点”的数量
@@ -33,7 +34,7 @@ public class DepthFirstSearch {
         // 对于 当前节点v的每一个相邻节点w
         for (int currentAdjacentVertex : graph.adj(currentVertex)) {
             // 如果节点w还没有被标记过，就对它进行深度搜索
-            if(isNotMarked(currentAdjacentVertex)) dfs(graph, currentAdjacentVertex);
+            if(isNotMarked(currentAdjacentVertex)) markVertexAndCountsViaDFS(graph, currentAdjacentVertex);
         }
     }
 
@@ -60,19 +61,19 @@ public class DepthFirstSearch {
         int startVertex = Integer.parseInt(args[1]);
 
         // 1 创建 DFS对象
-        DepthFirstSearch dfs = new DepthFirstSearch(graph, startVertex);
+        DepthFirstSearch markedGraph = new DepthFirstSearch(graph, startVertex);
 
         // 2 调用API来获取图的性质
         for (int currentVertex = 0; currentVertex < graph.V(); currentVertex++) {
             // 打印出图中，所有可以到达（aka 与顶点连通）的顶点
-            if (dfs.doesConnectedWithStartVertex(currentVertex)) {
+            if (markedGraph.doesConnectedWithStartVertex(currentVertex)) {
                 StdOut.print(currentVertex + " ");
             }
         }
         StdOut.println();
 
         // 如果 与起点连通的顶点的数量 不等于 图中所有顶点的数量
-        if (dfs.vertexAmountConnectToStartVertex() != graph.V()) {
+        if (markedGraph.vertexAmountConnectToStartVertex() != graph.V()) {
             StdOut.print("NOT ");
         }
         StdOut.print("connected");
