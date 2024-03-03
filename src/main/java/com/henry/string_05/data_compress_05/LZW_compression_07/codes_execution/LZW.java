d3351689aff3b9165d65d7eaf0559c28788c326b
HenryInSH <2291972433@qq.com>
2024-03-03 21:13:56 +0800

    --- LZW展开算法步骤“向解码表中添加条目”的特殊情况：输入中的当前码值 与 解码表中“待填充/添加”的条目的码值 相同（这种情况下，没办法像其他情况，简单地从“下一个码值对应的字符串”中获取到“首字符”，并进一步构造条目的“多字符键”）。 处理手段：使用“当前码值解码出的字符串” 与 其第一个字符 来 构造出 “下一个码值所对应的字符串”。 ---

--- src/main/java/com/henry/string_05/data_compress_05/LZW_compression_07/codes_execution/LZW.java
+++ src/main/java/com/henry/string_05/data_compress_05/LZW_compression_07/codes_execution/LZW.java
@@ -121,43 +121,45 @@ public class LZW {
      */
     public static void expand() {
         // #0 初始化一个“所有可能编码大小”的符号表
-        String[] codeValueToDecodedStr = new String[encodedValueOptions];
-        int currentCodeValue; // next available codeword value
+        String[] codeValueToItsCharacterSeq = new String[encodedValueOptions];
+        int currentCodeValueInDecodedTable; // next available codeword value
 
         // #1 初始化“符号表”（解码表）中的“单字符键”的条目 - 手段：使用“字符的int表示”来作为“码值”，使用字符本身来作为“字符串”
-        for (currentCodeValue = 0; currentCodeValue < characterOptions; currentCodeValue++)
-            codeValueToDecodedStr[currentCodeValue] = "" + (char) currentCodeValue;
-        codeValueToDecodedStr[currentCodeValue++] = "";                        // (unused) lookahead for EOF
+        for (currentCodeValueInDecodedTable = 0; currentCodeValueInDecodedTable < characterOptions; currentCodeValueInDecodedTable++)
+            codeValueToItsCharacterSeq[currentCodeValueInDecodedTable] = "" + (char) currentCodeValueInDecodedTable;
+
+        codeValueToItsCharacterSeq[currentCodeValueInDecodedTable++] = "";                        // (unused) lookahead for EOF
 
         // #2-① 读取“输入中的当前码值”
-        int codeValue = BinaryStdIn.readInt(bitWidthLength);
+        int currentCodeValueOfInput = BinaryStdIn.readInt(bitWidthLength);
         // 如果码值 在“最大的可选字符选项”的刻度上，说明 展开的信息是一个空字符串，则：直接返回，不再解码了
-        if (codeValue == characterOptions) return;           // expanded message is empty string
+        if (currentCodeValueOfInput == characterOptions) return;           // expanded message is empty string
         // #2-② 解码出 “当前码值”所对应的字符串
-        String currentDecodedStr = codeValueToDecodedStr[codeValue];
+        String currentDecodedStr = codeValueToItsCharacterSeq[currentCodeValueOfInput];
 
         while (true) {
-            // #2-③ 把“解码当前码值得到的字符串”写入到标准输出中
+            // #2-③ 把当前码值“解码得到的字符串”写入到标准输出中
             BinaryStdOut.write(currentDecodedStr);
 
             // #3-① 读取“输入中的下一个码值”
-            codeValue = BinaryStdIn.readInt(bitWidthLength);
+            currentCodeValueOfInput = BinaryStdIn.readInt(bitWidthLength);
             // 如果 码值等于“最大的可选字符选项”，说明???，则：xx结束，跳出循环
-            if (codeValue == characterOptions) break;
+            if (currentCodeValueOfInput == characterOptions) break;
             // #3-② 解码出 “输入中的下一个码值”所对应的字符串
-            String nextDecodedStr = codeValueToDecodedStr[codeValue];
+            String nextDecodedStr = codeValueToItsCharacterSeq[currentCodeValueOfInput];
 
             // #4 向“解码表”中添加条目👇
-            // #4-① 先处理特殊情况：前瞻过程中得到的字符 与 当前子字符串的开头字符 相同👇
-            if (currentCodeValue == codeValue)
+            // #4-① 先处理特殊情况：前瞻过程中得到的字符 与 当前子字符串的开头字符(??) 相同，则..
+            if (currentCodeValueInDecodedTable == currentCodeValueOfInput)
+                // 按照规则，构造出 “下一个码值”所对应的字符序列👇
                 // “输入中的下一个码值”所对应的字符串 就等于 “输入中的当前码值”所对应的字符串 + “当前码值所对应的字符串的首字符”
                 nextDecodedStr = currentDecodedStr + currentDecodedStr.charAt(0);
-            // #4-② 如果当前码值 还是“有效码值”，则：构造符号表条目的“码值” 与 “字符串”，将它们关联起来
-            if (currentCodeValue < encodedValueOptions)
+            // #4-② 如果当前码值 还是在“有效码值”的范围内，则：构造符号表条目的“码值” 与 “字符串”，将它们关联起来
+            if (currentCodeValueInDecodedTable < encodedValueOptions)
                 // 构造码值 - 手段：把“自然数序列中的当前码值”+1；  构造“字符串” - 手段：“输入中的当前编码所对应的字符串” + “输入中的下一个编码所对应的字符串”的首字符
-                codeValueToDecodedStr[currentCodeValue++] = currentDecodedStr + nextDecodedStr.charAt(0);
+                codeValueToItsCharacterSeq[currentCodeValueInDecodedTable++] = currentDecodedStr + nextDecodedStr.charAt(0);
 
-            // #5 更新 “当前字符串”变量 为 “下一个字符串” 来 为下一个循环做准备
+            // #5 更新 “当前字符串”变量 为 “输入中的下一个字符串” 来 为下一个循环做准备{1 打印字符序列； 2 添加解码表条目}
             currentDecodedStr = nextDecodedStr;
         }
         BinaryStdOut.close();
