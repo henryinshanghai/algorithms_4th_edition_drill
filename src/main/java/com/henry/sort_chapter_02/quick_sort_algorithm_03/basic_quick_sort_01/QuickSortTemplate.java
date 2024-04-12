dcfc9aaf414672ea7ce59e8066402b96e9b8ed24
HenryInSH <2291972433@qq.com>
2024-04-12 09:04:06 +0800

    --- 通用排序算法之高级排序算法（快速排序）：refactor 04/12; 先分拣非基准元素，再排定基准元素 ---

--- src/main/java/com/henry/sort_chapter_02/quick_sort_algorithm_03/basic_quick_sort_01/QuickSortTemplate.java
+++ src/main/java/com/henry/sort_chapter_02/quick_sort_algorithm_03/basic_quick_sort_01/QuickSortTemplate.java
@@ -82,6 +82,16 @@ public class QuickSortTemplate {
 
     // 在数组的指定区间（闭区间）中，排定一个切分元素，并返回排定的位置
     private static int arrangePivotViaPartition(Comparable[] arrayToSort, int leftBar, int rightBar) {
+        // #1 借助一组指针（小于区边界指针、大于区边界指针） 来 把元素放置到正确的区域中
+        int greaterZoneBoundary = putItemIntoRightZone(arrayToSort, leftBar, rightBar);
+
+        // #2 排定基准元素 aka 把基准元素放到它正确的位置上     手段: 把基准元素 与 greaterZoneBoundary所指向的元素 交换位置
+        exch(arrayToSort, leftBar, greaterZoneBoundary);
+
+        return greaterZoneBoundary;
+    }
+
+    private static int putItemIntoRightZone(Comparable[] arrayToSort, int leftBar, int rightBar) {
         // #1 准备左右边界的指针
         int lessZoneBoundary = leftBar;
         int greaterZoneBoundary = rightBar + 1;
@@ -91,28 +101,26 @@ public class QuickSortTemplate {
 
         // #3 把剩余的其他元素（除了基准元素）分拣到左半区间、右半区间中  特征：死循环 + 执行体break
         while (true) {
-            // #1 让左指针停在 从左往右第一个比基准元素更大的元素上
+            // #3-① 让左指针停在 从左往右第一个比基准元素更大的元素上
             // 🐖 如果所有元素都比基准元素小的话，则：左指针会一直向右移动，直到等于rightBar 这种情况下，需要停止移动指针
             while (less(arrayToSort[++lessZoneBoundary], pivotItem)) if (lessZoneBoundary == rightBar) break;
 
-            // #2 让右指针停在 从右往左第一个比基准元素小的元素上
+            // #3-② 让右指针停在 从右往左第一个比基准元素小的元素上
             // 如果基准元素大于其他的任何元素，则：循环条件直接不成立，右指针不会向左移动
             while (less(pivotItem, arrayToSort[--greaterZoneBoundary])) if (greaterZoneBoundary == leftBar) break;
 
-            // #3 判断元素的分拣工作是否已经完成
+            // #3-③ 判断元素的分拣工作是否已经完成
             // 当左指针与右指针相遇时，说明元素分拣已经完成 - 小于基准元素的元素都在左侧区间、大于基准元素的元素都在右侧区间
             if (lessZoneBoundary >= greaterZoneBoundary) {
                 break;
             }
 
-            // #4 对元素进行分拣 手段：交换 两个位置上的元素
+            // #3-④ 对元素进行分拣 手段：交换 两个位置上的元素
             // 特征：如果左右指针刚好遇到了两个 与pivot相同的元素，那么 这两个元素也会被交换 - 这其实是多余的操作
             exch(arrayToSort, lessZoneBoundary, greaterZoneBoundary);
         }
 
-        // #4 排定基准元素 aka 把基准元素放到它正确的位置上     手段: 把基准元素 与 greaterZoneBoundary所指向的元素 交换位置
-        exch(arrayToSort, leftBar, greaterZoneBoundary);
-
+        // 分拣完成后，返回“大于区的边界指针” - 它就是 基准元素的排定位置
         return greaterZoneBoundary;
     }
 
