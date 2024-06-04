07f4ff7687b9c06e10ada7143cd93814aa20c582
HenryInSH <2291972433@qq.com>
2024-06-04 23:37:39 +0800

    --- 键索引计数操作的应用#2: 高位优先的字符串排序(MSD). ---

--- src/main/java/com/henry/string_05/string_sorting_01/MSD_03/MSDLite.java
+++ src/main/java/com/henry/string_05/string_sorting_01/MSD_03/MSDLite.java
@@ -1,7 +1,8 @@
 package com.henry.string_05.string_sorting_01.MSD_03;
 
-// 算法：从左往右，对特定区间中的 所有字符串的 当前字符，进行键索引计数。
+// MSD算法：对于特定区间内的字符串序列，从左往右地，以当前字符作为索引、以字符串本身作为键 进行 键索引计数排序的操作；
 // 原理：把原始任务（把所有的字符串按照字母表顺序 完全排序） 分解成为 #1 对首字符执行键索引计数操作（得到有序的首字符）; #2 对各个子组进行同样的操作。
+// 特征：每次执行 键索引计数排序时，都需要设置正确的 字符串序列范围；
 // 递归方法：将 指定闭区间中的所有字符串，从 指定字符开始 完全排序；
 // 手段：使用 字符串中的字符 来 作为”键索引计数法“中的”索引“，使用 字符串本身 作为”键“
 // 可以使用递归的特征：更小规模问题的答案 能够帮助解决 原始问题。
@@ -32,7 +33,30 @@ public class MSDLite {
             return;
         }
 
-        // 🐖 每次对sort()的调用，都会产生一个新的 indexToItsStartSpotInResultSequence[]数组
+        // Ⅰ 以当前位置上的字符作为索引 来 对字符串序列执行 键索引计数操作 - 得到 组间有序、组内元素相对顺序同原始序列的结果序列
+        // 🐖 每次 以“指定的index”对“指定区间”中的字符串序列 来 执行键索引计数的操作，都会产生一个新的 indexToItsStartSpotInResultSequence[]数组
+        int[] indexToItsStartSpotInResultSequence = performKeyIndexOperation(originalWordArr, wordLeftBar, wordRightBar, currentStartCharacterCursor);
+
+        // Ⅱ 对于”使用首字符进行键索引计数操作后“所得到的 多个 索引不同的子集合/子组，对各个子组中的字符串序列 以下一个位置的字符作为索引 来 执行键索引计数的操作
+        // 特征：结果序列中，index存在有多个，且不确定具体是哪些index（字符）；
+        // 手段：对所有可能的字符index 进行遍历，找到那些个 能够产生有效子集合的index
+        // 🐖 原始数组根据”首字符“产生了几个分组/子集合，这里就会对应的 有多少次循环（需要排序的次数）
+        for (int currentIndex = 0; currentIndex < biggestIndexPlus1; currentIndex++) {
+            // ① 对于当前index，计算 其所对应的 字符串区间👇
+            // 区间的左边界&&右边界
+            int wordLeftBarForCurrentIndex = wordLeftBar + indexToItsStartSpotInResultSequence[currentIndex];
+            int wordRightBarForCurrentIndex = wordLeftBar + indexToItsStartSpotInResultSequence[currentIndex + 1] - 1;
+
+            // ② 使用当前index 来 对其对应的字符串序列 执行键索引计数操作 - 组间有序、组内元素相对顺序不变
+            // 🐖 如果index对应的区间[leftBar, rightBar] 不是一个有效区间，说明 index不存在对应的子集合，则：sort()会直接return
+            sortWordRangesFromGivenCharacter(originalWordArr, wordLeftBarForCurrentIndex, wordRightBarForCurrentIndex, currentStartCharacterCursor + 1);
+        }
+    }
+
+    // 键索引计数操作：pick the item in original sequence, and arrange it into correct spot.
+    // originalWordArr, 原始的字符串序列    wordLeftBar, 待操作的字符串序列区间的左边界
+    // wordRightBar, 待操作字符串序列区间的右边界     currentStartCharacterCursor, 作为索引的字符的位置
+    private static int[] performKeyIndexOperation(String[] originalWordArr, int wordLeftBar, int wordRightBar, int currentStartCharacterCursor) {
         // Ⅰ 准备 indexToItsStartSpotInResultSequence[] - #1 index = 字符的数字表示 + 1; 用于避免出现值为-1的index  #2 多预留出一个位置，用于 累加得到 startSpot
         int[] indexToItsStartSpotInResultSequence = new int[biggestIndexPlus1 + 2];
         for (int currentWordCursor = wordLeftBar; currentWordCursor <= wordRightBar; currentWordCursor++) {
@@ -48,7 +72,7 @@ public class MSDLite {
             indexToItsStartSpotInResultSequence[currentIndex + 1] += indexToItsStartSpotInResultSequence[currentIndex];
         }
 
-        // Ⅲ 从[a[wordLeftBar], wordRightBar]]区间中的所有字符串中，构造出 第currentCharacterCursor个字符有序的 aux[]
+        // Ⅲ 从[a[wordLeftBar], a[wordRightBar]]区间中的所有字符串中，构造出 第currentCharacterCursor个字符有序的 aux[]
         for (int currentWordCursor = wordLeftBar; currentWordCursor <= wordRightBar; currentWordCursor++) {
             // 获取当前单词在当前位置上的字符的数字表示
             int characterInInt = charAt(originalWordArr[currentWordCursor], currentStartCharacterCursor);
@@ -68,20 +92,7 @@ public class MSDLite {
         for (int currentWordCursor = wordLeftBar; currentWordCursor <= wordRightBar; currentWordCursor++) {
             originalWordArr[currentWordCursor] = aux[currentWordCursor - wordLeftBar];
         }
-
-        // 对于”使用首字符进行键索引计数操作后“所得到的 多个 索引不同的子集合，获取到 index在当前结果序列中的子集合区间
-        // 特征：结果序列中，index存在有多个，且不确定具体是哪些index（字符）；
-        // 手段：对所有可能的字符进行遍历，找到那些个 能够产生有效子集合的index
-        // 🐖 原始数组根据”首字符“产生几个分组/子集合，这里就会对应的 有多少次循环（需要排序的次数）
-        for (int currentIndex = 0; currentIndex < biggestIndexPlus1; currentIndex++) {
-            // 当前index 在”结果序列“中，所对应的 有效子集合/区间 leftBar = X[index], rightBar = X[index + 1] - 1.
-            // 🐖 如果index对应的区间[leftBar, rightBar] 不是一个有效区间，说明 index不存在对应的子集合，则：sort()会直接return
-            int wordLeftBarForCurrentIndex = wordLeftBar + indexToItsStartSpotInResultSequence[currentIndex];
-            int wordRightBarForCurrentIndex = wordLeftBar + indexToItsStartSpotInResultSequence[currentIndex + 1] - 1;
-
-            // 从下一个字符开始 完全有序 / 子集合中的”首字符“：currentCharacterCursor + 1
-            sortWordRangesFromGivenCharacter(originalWordArr, wordLeftBarForCurrentIndex, wordRightBarForCurrentIndex, currentStartCharacterCursor + 1);
-        }
+        return indexToItsStartSpotInResultSequence;
     }
 
     // 对 [a[leftBar], a[rightBar]]闭区间中的元素，从 startPointToCompare个字符开始 完全排序
