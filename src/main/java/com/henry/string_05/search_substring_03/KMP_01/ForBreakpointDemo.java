df2030384f7a8ab74cb8c089b4078adc1a37b689
HenryInSH <2291972433@qq.com>
2024-07-27 17:12:18 +0800

    --- 辅助操作：for语句上添加断点时，执行步进的步骤 - 初始化语句 -> 布尔表达式 -> 循环体 -> 变量更新语句 -> 布尔表达式... ---

---
+++ src/main/java/com/henry/string_05/search_substring_03/KMP_01/ForBreakpointDemo.java
@@ -0,0 +1,10 @@
+package com.henry.string_05.search_substring_03.KMP_01;
+
+// 验证：当把断点添加到for语句上时，执行步进的顺序是 - 初始化语句 -> 布尔表达式 -> for语句体 -> 更新语句 -> 布尔表达式...
+public class ForBreakpointDemo {
+    public static void main(String[] args) {
+        for (int i = 0; i < 4; i++) {
+            System.out.println(i);
+        }
+    }
+}
