96368b16ab1ea2561460895980a9eb6fc0326207
HenryInSH <2291972433@qq.com>
2024-12-15 20:19:55 +0800

    --- 买卖股票的最佳时机ii：#1 最后一天会获得最大的利润；#2 对于某一天的利润存在有两种状态{① 持有股票状态； ② 不持有股票状态；#3 当前持有股票的可能性 {① 前一天已经持有股票，今天未交易；② 前一天未持有股票，今天买入股票} & 当前未持有股票的可能性 {① 前一天未持有股票，今天未交易； ② 前一天持有股票，今天卖出股票} // 递推方程} ---

--- src/main/java/com/henry/leetcode_traning_camp/week_04/day05/trade_the_stock/Solution_best_timing_to_trade_the_stock.java
+++ src/main/java/com/henry/leetcode_traning_camp/week_04/day05/trade_the_stock/Solution_best_timing_to_trade_the_stock.java
@@ -1,45 +1,49 @@
 // 概念：持有股票、不持有股票
-// 原理：最后一天是不持有股票的状态时，才会获得最大的利润。
-// 手段：从所有可能的情况中，选取较大值 作为当前元素的值
+// 假设：每一天 都有 这一天的最大利润（最优子结构） - 这样最后一天的利润 才会是 最大利润；
+// 原理：任何情况下，卖出股票 总会得到 相比于 持有股票 更大的利润。
+// 手段：从所有可能的情况中，选取较大值 作为当前元素的值；
 class Solution_best_timing_to_trade_the_stock {
     public static void main(String[] args) {
-        int[] priceSequence = {7, 1, 5, 3, 6, 4};
-        int maxProfit = maxProfitFrom(priceSequence);
+        int[] consecutivePriceSequence = {7, 1, 5, 3, 6, 4};
+        int maxProfit = maxProfitFrom(consecutivePriceSequence);
 
         System.out.println("单笔交易所能得到的最大利润是： " + maxProfit);
     }
 
-    // 实现1：二维数组存储
-    // 可以将每天持有与否的情况分别用 dp[i][0] 和 dp[i][1] 来进行存储
-    // 时间复杂度：O(n)，空间复杂度：O(n)
+    // 实现1：二维数组dp[i][j] 来 存储 第i天(交易完成后,手中状态为j时)所能够得到的最大利润
+    // i表示 当前是第几天;
+    // 0表示 不持有股票的状态; 1表示 持有股票的状态;
     public static int maxProfitFrom(int[] priceSequence) {
-        int sequenceDays = priceSequence.length;
-        int[][] dp = new int[sequenceDays][2];     // 创建二维数组存储状态
+        // 创建二维数组 来 存储状态
+        int daysAmountInSequence = priceSequence.length;
+        int[][] currentDayToItsMaxProfitWhenStatusIs = new int[daysAmountInSequence][2];
 
         // 初始状态
-        dp[0][0] = 0; // [x][0] 表示”持有“股票
-        dp[0][1] = -priceSequence[0]; // [x][1]表示”不持有“股票
+        currentDayToItsMaxProfitWhenStatusIs[0][0] = 0; // 如果第0天不持有股票，说明 要么是 买入股票后又卖出，要么是 没有进行股票交易，则：收益为0
+        currentDayToItsMaxProfitWhenStatusIs[0][1] = -priceSequence[0]; // 如果第0天持有股票([1])，说明 当天买入了股票，则：收益为 -股票的价格
 
-        for (int currentDayNo = 1; currentDayNo < sequenceDays; ++currentDayNo) {
-            // 第 currentDayNo 天，"不持有"股票
-            dp[currentDayNo][0] = Math.max(dp[currentDayNo - 1][0], // #1 前一天就”不持有“股票
-                    dp[currentDayNo - 1][1] + priceSequence[currentDayNo]); // #2 前一天持有股票，但今天”卖出“股票
-            // 第 currentDayNo 天，”持有“股票
-            dp[currentDayNo][1] = Math.max(dp[currentDayNo - 1][1], // #1 前一天就”持有“股票
-                    dp[currentDayNo - 1][0] - priceSequence[currentDayNo]); // #2 前一天”不持有“股票，但今天”买入“股票
+        // 对于股票价格序列中的每一天...
+        for (int currentDayNo = 1; currentDayNo < daysAmountInSequence; ++currentDayNo) {
+            // #1 计算这一天 "不持有"股票的状态下的最大利润
+            currentDayToItsMaxProfitWhenStatusIs[currentDayNo][0] = Math.max(currentDayToItsMaxProfitWhenStatusIs[currentDayNo - 1][0], // option1： 前一天就”不持有“股票
+                    currentDayToItsMaxProfitWhenStatusIs[currentDayNo - 1][1] + priceSequence[currentDayNo]); // option2： 前一天持有股票，但今天”卖出“股票
+            // #2 同时计算这一天 “持有股票”状态下的最大利润
+            currentDayToItsMaxProfitWhenStatusIs[currentDayNo][1] = Math.max(currentDayToItsMaxProfitWhenStatusIs[currentDayNo - 1][1], // option1 前一天就”持有“股票
+                    currentDayToItsMaxProfitWhenStatusIs[currentDayNo - 1][0] - priceSequence[currentDayNo]); // option2 前一天”不持有“股票，但今天”买入“股票
         }
 
-        print(dp);
+        // 打印计算得到的dp[][]数组
+        print(currentDayToItsMaxProfitWhenStatusIs);
 
         // #1 卖出股票收益高于持有股票收益，因此取[0]
-        // #2 越是后面的元素，元素值就会更高 因此这里是 dp[sequenceDay]
-        return dp[sequenceDays - 1][0];
+        // #2 越是后面的元素，元素值就会更高 因此这里是 currentDayToItsMaxProfitWhenStatusIs[sequenceDay]
+        return currentDayToItsMaxProfitWhenStatusIs[daysAmountInSequence - 1][0];
     }
 
-    private static void print(int[][] dp) {
-        for (int[] currentRow : dp) {
-            for (int currentItem : currentRow) {
-                System.out.print(currentItem + " ");
+    private static void print(int[][] currentDayToItsMaxProfit) {
+        for (int[] currentDaysMaxProfits : currentDayToItsMaxProfit) {
+            for (int maxProfitUnderCurrentStatus : currentDaysMaxProfits) {
+                System.out.print(maxProfitUnderCurrentStatus + " ");
             }
 
             System.out.println();
