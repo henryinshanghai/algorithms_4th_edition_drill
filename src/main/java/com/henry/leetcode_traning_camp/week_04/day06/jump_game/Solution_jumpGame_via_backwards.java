a91e8ef7accb48c044fc72cb86245910d1536f6d
HenryInSH <2291972433@qq.com>
2024-12-20 09:45:10 +0800

    --- 跳跃游戏：是否可以到达终点? 逆向指针 + 更新“当前需要到达的位置” + 第一个“当前所需要到达的位置”是否是位置0； ---

---
+++ src/main/java/com/henry/leetcode_traning_camp/week_04/day06/jump_game/Solution_jumpGame_via_backwards.java
@@ -0,0 +1,33 @@
+package com.henry.leetcode_traning_camp.week_04.day06.jump_game;
+
+// 验证：对于 是否能够跳到终点的问题，可以 使用逆向指针遍历所有终点前的位置 + 更新当前所需到达位置的方式 来 判断
+// 概念：终点位置、当前所需要到达的位置、当前位置
+// 原理：按此方式回溯，如果 能够跳转到终点，则 第一个“当前需要到达的位置” 一定会是位置0
+public class Solution_jumpGame_via_backwards {
+    public static void main(String[] args) {
+//        int[] currentSpotToItsMaxJumpDistance = {2,3,1,1,4};
+        int[] currentSpotToItsMaxJumpDistance = {3, 2, 1, 0, 4};
+        boolean canOrNot = ableToReachEnd(currentSpotToItsMaxJumpDistance);
+        System.out.println("按照当前数组能够跳到数组末尾？" + canOrNot);
+    }
+
+    private static boolean ableToReachEnd(int[] currentSpotToItsMaxJumpDistance) {
+        // 〇 准备一个变量 用于记录当前需要到达的位置
+        int spotNeedToReach = currentSpotToItsMaxJumpDistance.length - 1;
+
+        // Ⅰ 从后往前遍历数组   更新“当前需要到达的位置”
+        // 对于 目标位置 之前的每一个位置...
+        for (int backwardsCursor = currentSpotToItsMaxJumpDistance.length - 2; backwardsCursor >= 0; backwardsCursor--) {
+            int maxJumpDistanceOnCurrentSpot = currentSpotToItsMaxJumpDistance[backwardsCursor];
+            // 如果 当前位置 + 当前位置上所能跳出的最大距离 大于等于 目标位置，说明 能够 由当前位置 到达 目标位置，则：
+            if (backwardsCursor + maxJumpDistanceOnCurrentSpot >= spotNeedToReach) {
+                // 使用 当前位置 来 更新目标位置
+                spotNeedToReach = backwardsCursor;
+            }
+        }
+
+        // Ⅱ 判断最终“需要到达的位置” 是不是 落到了起点位置
+        // 原理：我们唯一确定的是 - 起点是可达的
+        return spotNeedToReach == 0;
+    }
+}
