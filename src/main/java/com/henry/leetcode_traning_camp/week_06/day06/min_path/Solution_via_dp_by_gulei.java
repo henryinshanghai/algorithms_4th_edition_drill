a173b1152207d920f5223094ccce1537e2d95bdd
HenryInSH <2291972433@qq.com>
2025-02-28 23:11:12 +0800

    --- 动态规划算法之 矩形棋盘上的最小路径和：最优子结构 - min_path(current_grid) = min_path(previous_grid) + current_grid_value。 ---

---
+++ src/main/java/com/henry/leetcode_traning_camp/week_06/day06/min_path/Solution_via_dp_by_gulei.java
@@ -0,0 +1,70 @@
+package com.henry.leetcode_traning_camp.week_06.day06.min_path;
+
+// 验证：对于 从矩形棋盘上计算出 按照特定规则从左上角到右下角的最小路径的问题，可以使用
+// 从所有可能的选项中选择最小路径的方式 来 得到 “到达当前方格的最小路径”
+// 最优子结构：到达当前方格的最小路径 = 到达“上一个方格”的最小路径 + 当前方格的value
+// 🐖 这种方式 先拷贝元素到dp[][]数组中
+public class Solution_via_dp_by_gulei {
+    public static void main(String[] args) {
+        int[][] numberBoard = {
+                {1, 3, 1},
+                {1, 5, 1},
+                {4, 2, 1}
+        };
+
+        int pathSum = getMinimumPathSumOf(numberBoard);
+
+        System.out.println("矩阵中从左上角到右下角的最小路径的加和值等于： " + pathSum);
+    }
+
+    private static int getMinimumPathSumOf(int[][] numberBoard) {
+        // 边界条件
+        if (numberBoard == null || numberBoard.length == 0) {
+            return 0;
+        }
+
+        // 准备合乎题意的dp[][]数组
+        int[][] currentGridToMinPathSumTowardsIt = new int[numberBoard.length][numberBoard[0].length];
+
+        // #0 先把 二维数组中的元素 拷贝到 dp[][]数组中，然后在此基础上 来 计算dp[][]数组正确的元素值
+        fullCopy(numberBoard, currentGridToMinPathSumTowardsIt);
+
+        // #1 初始化dp[][]数组第一列的元素
+        for (int currentRow = 1; currentRow < currentGridToMinPathSumTowardsIt.length; currentRow++) {
+            currentGridToMinPathSumTowardsIt[currentRow][0]
+                    += currentGridToMinPathSumTowardsIt[currentRow - 1][0];
+        }
+
+        // #2 初始化dp[][]数组第一行的元素
+        for (int currentColumn = 1; currentColumn < currentGridToMinPathSumTowardsIt[0].length; currentColumn++) {
+            currentGridToMinPathSumTowardsIt[0][currentColumn] +=
+                    currentGridToMinPathSumTowardsIt[0][currentColumn - 1];
+        }
+
+
+        // #3 计算并填充 dp[][]数组中的剩余元素
+        for (int currentRow = 1; currentRow < currentGridToMinPathSumTowardsIt.length; currentRow++) {
+            for (int currentColumn = 1; currentColumn < currentGridToMinPathSumTowardsIt[currentRow].length; currentColumn++) {
+
+                currentGridToMinPathSumTowardsIt[currentRow][currentColumn] += // 当前元素
+                        Math.min(currentGridToMinPathSumTowardsIt[currentRow - 1][currentColumn], // option1: 上一行同列的元素
+                                currentGridToMinPathSumTowardsIt[currentRow][currentColumn - 1]); // option2: 同一行前一列的元素
+
+            }
+        }
+
+        // 最后，返回 最后一个方格 => 到达此方格的最小路径sum
+        // 🐖 数组的索引从0开始，因此 最后一个元素是 arr[length - 1]
+        return currentGridToMinPathSumTowardsIt[currentGridToMinPathSumTowardsIt.length - 1][currentGridToMinPathSumTowardsIt[0].length - 1];
+    }
+
+    private static void fullCopy(int[][] numberBoard, int[][] currentCoordinationToItsMinPathSum) {
+        for (int currentRow = 0; currentRow < numberBoard.length; currentRow++) {
+            for (int currentColumn = 0; currentColumn < numberBoard[0].length; currentColumn++) {
+                currentCoordinationToItsMinPathSum[currentRow][currentColumn] = numberBoard[currentRow][currentColumn];
+            }
+        }
+    }
+}
+
+// reference: https://www.youtube.com/watch?v=ItjZdu6jEMs
