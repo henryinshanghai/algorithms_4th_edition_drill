751f87c20f0740e950ff9a5e7c2a3463fab7c3f4
HenryInSH <2291972433@qq.com>
2025-03-04 20:26:06 +0800

    --- 跳跃游戏之跳跃到最后一个位置所需要的最少跳跃数量：原理 - #1 对于每一次跳跃，都有该次跳跃所能够到达的最远距离； #2 如果 当前位置 到达 当前跳跃所能到达的最远位置，说明 需要进行下一次跳跃了。 ---

--- src/main/java/com/henry/leetcode_traning_camp/week_04/day06/jump_game2/Solution_jumpGame_ii_via_greedy.java
+++ src/main/java/com/henry/leetcode_traning_camp/week_04/day06/jump_game2/Solution_jumpGame_ii_via_greedy.java
@@ -1,8 +1,10 @@
 package com.henry.leetcode_traning_camp.week_04.day06.jump_game2;
 
 // 对于 求跳跃楼梯所需要的最小跳跃次数 这样的问题，可以使用 向前推进的动态指针 的方式 来 得到结果
-// 概念：#1 跳跃作为基本动作(一次跳跃可以 跳xx单位的距离)、
-// #2 当前所能达到的最远位置 currentMaxReachSpot、#3 下一次跳跃所能到达的最远位置 maxReachSpotViaNextJump
+// 概念：#0 第X次跳跃 所能达到的最远位置；
+// #1 跳跃作为基本动作(一次跳跃可以 跳xx单位的距离)、
+// #2 当前所能达到的最远位置 currentMaxReachSpot、
+// #3 下一次跳跃所能到达的最远位置 maxReachSpotViaNextJump
 // 原理：#1 从起点开始跳跃，每次跳跃都有其能够到达的最远位置；
 // #2 当游标指针 移动到此最远位置后，就需要开始下一次跳跃。
 // 🐖 使用添加的打印语句 来 帮助理解算法的执行过程
@@ -16,33 +18,30 @@ public class Solution_jumpGame_ii_via_greedy {
     }
 
     private static int minJumpAmountToReachEnd(int[] currentSpotToItsMaxJumpDistance) {
-        // 这种需要维护多个变量的手段总是不容易理解 因为变量太多
-        int currentMaxReachSpot = 0; // 当前所能够到达的最远位置
-        int maxReachSpotViaNextJump = 0; // 通过下一次跳跃所能到达的最远位置
+        int currentJumpMaxReachSpot = 0; // 当前跳跃所能够到达的最远位置
+        int nextJumpMaxReachSpot = 0; // 通过下一次跳跃所能到达的最远位置
         int requiredMinJumpSteps = 0; // 到达终点位置所需要的跳跃数量
 
-        // 对于 每一个当前位置...
         // 🐖 作为一个动态的过程，以下两个if发生的时机并不相同
-        for (int currentSpot = 0; currentSpot < currentSpotToItsMaxJumpDistance.length - 1; currentSpot++) {
-            /* #1 获取到 由当前位置 所能跳出的最远距离 */
-            int maxJumpDistanceOfCurrentSpot = currentSpotToItsMaxJumpDistance[currentSpot];
+        for (int currentCursorSpot = 0; currentCursorSpot < currentSpotToItsMaxJumpDistance.length - 1; currentCursorSpot++) {
+            /* #1 获取到 由当前位置 经过一步跳跃所能跳出的最远距离 */
+            int maxJumpDistanceOfCurrentSpot = currentSpotToItsMaxJumpDistance[currentCursorSpot];
 
-            /* #2 更新 ”下一次跳跃所能到达的最大位置“ */
+            /* #2 计算 从当前跳跃所能到达的各个位置上开始，再经过一次跳跃(下一次跳跃)所能到达的最大位置👇 */
             // 如果 (当前位置 + 由此位置所能跳出的最远距离) 大于 当前记录的"下一次跳跃所能到达的最大位置",
-            // 说明 在所有“当前跳跃所能够跳到的位置“中，我们找到了一个 ”更好的位置“
-            // 🐖 我们不管 是怎么跳到当前位置上的...
-            if (currentSpot + maxJumpDistanceOfCurrentSpot > maxReachSpotViaNextJump) {
+            // 说明 在所有“当前跳跃所能够跳到的位置“中，我们找到了一个 ”更好的起跳位置“（因为可以跳得更远）
+            if (currentCursorSpot + maxJumpDistanceOfCurrentSpot > nextJumpMaxReachSpot) {
                 // 则：使用 它 来 更新 “下一次跳跃所能到达的最大位置”
-                maxReachSpotViaNextJump = currentSpot + maxJumpDistanceOfCurrentSpot;
+                nextJumpMaxReachSpot = currentCursorSpot + maxJumpDistanceOfCurrentSpot;
             }
 
             /* #3 更新 ”当前所能到达的最大位置“ */
-            // 如果 随着当前位置的推进，当前位置 已经等于 “当前所能到达的最远位置”，说明 需要开始新的跳跃了，则：
-            if (currentSpot == currentMaxReachSpot) {
+            // 如果 随着当前位置的推进，当前游标位置 已经到达 “当前跳跃所能到达的最远位置”，说明 需要开始新的跳跃了，则：
+            if (currentCursorSpot == currentJumpMaxReachSpot) {
                 // 把 跳跃的步数+1 来 执行跳跃
                 requiredMinJumpSteps++;
-                // 使用 “下一次跳跃所能到达的最远位置” 来 更新 "当前所能到达的最远位置"
-                currentMaxReachSpot = maxReachSpotViaNextJump;
+                // 使用 当前“下一次跳跃所能到达的最远位置” 来 更新 “当前跳跃所能到达的最远位置” - 为下一轮跳跃做准备
+                currentJumpMaxReachSpot = nextJumpMaxReachSpot;
             }
         }
 
