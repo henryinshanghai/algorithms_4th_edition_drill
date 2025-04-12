303e512cdf7a7de8e04844112773159ce90ff4e1
HenryInSH <2291972433@qq.com>
2025-04-13 00:30:34 +0800

    --- 单词接龙之转换得到目标单词的最短转化序列本身：#1 需要完整地生成图本身（一个map对象）；#2 需要从图中，使用DFS+回溯的手段 来 得到所有的path，并收集到一个集合中。 ---

--- src/main/java/com/henry/leetcode_traning_camp/week_04/day03/solitaire_02/Solution_findLadders_via_bfs_and_dfs_happygirllzt.java
+++ src/main/java/com/henry/leetcode_traning_camp/week_04/day03/solitaire_02/Solution_findLadders_via_bfs_and_dfs_happygirllzt.java
@@ -3,103 +3,118 @@ package com.henry.leetcode_traning_camp.week_04.day03.solitaire_02;
 import java.util.*;
 
 // 验证：可以使用BFS + DFS的手段 来 得到由“起始单词”经由“有效的单词变体 中间结果”得到“目标单词”的 所有具体转换序列
-// 原理：BFS 用于 构建图中 word -> itsValidWordVariants 的映射关系；
-// DFS+回溯 用于 图中存在的、所有可能的“到目标单词的转换序列”。
+// 原理：BFS（递归实现） 用于 构建图中 word -> itsValidWordVariants 的映射关系；
+// DFS（递归实现）+回溯 用于 图中所存在的“到目标单词的转换序列”。
+// 🐖 DFS/BFS的基本作用是：找到图中与指定顶点相连通的所有顶点
 public class Solution_findLadders_via_bfs_and_dfs_happygirllzt {
     public static void main(String[] args) {
         String beginWord = "hit";
         String endWord = "cog";
-        List<String> validWordVariantList =
+        List<String> availableWordVariantList =
                 new ArrayList<>(
                         Arrays.asList("hot", "dot", "dog", "lot", "log", "cog")
                 );
 
-        List<List<String>> allValidTransformSequenceList = findLadders(beginWord, endWord, validWordVariantList);
-        System.out.println("最终的结果为：" + allValidTransformSequenceList);
+        List<List<String>> allShortestTransformSequences = findLadders(beginWord, endWord, availableWordVariantList);
+        System.out.println("最终的结果为：" + allShortestTransformSequences);
     }
 
     private static List<List<String>> findLadders(String beginWord,
                                                   String endWord,
                                                   List<String> availableValidWordVariantList) {
-        // 准备一个 xxx - 用于存放所有找到的 可行的最短转换路径
-        List<List<String>> allValidTransformSequenceList = new ArrayList<>();
+        // 准备一个 元素为list的list对象 - 用于存放所有找到的 “最短转换路径”
+        List<List<String>> allShortestTransformSequenceList = new ArrayList<>();
 
         // 把wordList转化成为一个set对象 用于更快速地判断 是否存在重复元素
-        Set<String> validWordVariantSet = new HashSet<>(availableValidWordVariantList);
+        Set<String> availableWordVariantSet = new HashSet<>(availableValidWordVariantList);
 
         // 鲁棒性代码 - endWord一定要出现在 可选的单词变体集合中，否则 不存在任何的转换序列，则：
-        if (!validWordVariantSet.contains(endWord)) {
+        if (!availableWordVariantSet.contains(endWord)) {
             // 直接return空列表
-            return allValidTransformSequenceList;
+            return allShortestTransformSequenceList;
         }
 
-        /* #1 使用bfs 来 得到图中所存在的 word -> itsTransformedWordVariants 映射 */
-        Map<String, List<String>> wordVariantToItsTransformedWordVariants = new HashMap<>();
+        /* #1 使用bfs 来 构建出图/(word -> itsTransformedWordVariants 一对多映射关系) */
+        Map<String, List<String>> wordToItsTransformedVariantsMap = new HashMap<>();
+        // 准备一个集合(用于存储所有的“起始单词”), 然后把当前的beginWord放进去
         Set<String> startWordSet = new HashSet<>();
         startWordSet.add(beginWord);
 
+        // 构建出 对应的无权无向图
         generateTheMapViaBFS(startWordSet,
-            endWord,
-            wordVariantToItsTransformedWordVariants,
-            validWordVariantSet);
+                endWord,
+                wordToItsTransformedVariantsMap, // map参数的作用：在方法执行的过程中被构建
+                availableWordVariantSet);
 
-        /* #2 查看下map是否 “按照预期被创建” */
-        printThe(wordVariantToItsTransformedWordVariants);
+        /* #2 查看下map对象|图 是否 “按照预期被创建” */
+        printThe(wordToItsTransformedVariantsMap);
 
         /* #3 使用DFS+回溯 来 从图中得到 所有可能的“到目标单词的转换序列” */
-        // 准备一个path     把beginWord放进去
+        // 准备一个path（用于 收集“到目标单词的转换序列”中的所有单词）然后把beginWord放进去
         List<String> pathTowardsEndWord = new ArrayList<>();
         pathTowardsEndWord.add(beginWord);
 
         // 使用dfs来生成结果
         generatePathListViaDFS(beginWord,
-            endWord,
-            pathTowardsEndWord,
-            allValidTransformSequenceList,
-            wordVariantToItsTransformedWordVariants);
+                endWord,
+                wordToItsTransformedVariantsMap,
+                pathTowardsEndWord,
+                allShortestTransformSequenceList
+        );
 
-        return allValidTransformSequenceList;
+        return allShortestTransformSequenceList;
 
     }
 
-    private static void printThe(Map<String, List<String>> wordVariantToItsTransformedWordVariants) {
-        if (wordVariantToItsTransformedWordVariants == null) return;
+    // 打印出所有 word -> its transformed variants的映射关系
+    private static void printThe(Map<String, List<String>> wordToItsTransformedWordVariants) {
+        if (wordToItsTransformedWordVariants == null) return;
 
-        for (Map.Entry<String, List<String>> currentWordVariantToItsTransformedWordVariants : wordVariantToItsTransformedWordVariants.entrySet()) {
-            String currentWordVariant = currentWordVariantToItsTransformedWordVariants.getKey();
-            List<String> itsTransformedWordVariants = currentWordVariantToItsTransformedWordVariants.getValue();
-            System.out.print("Key - " + currentWordVariant + " | ");
+        for (Map.Entry<String, List<String>> currentWordToItsTransformedWordVariants : wordToItsTransformedWordVariants.entrySet()) {
+            String currentWord = currentWordToItsTransformedWordVariants.getKey();
+            List<String> itsTransformedWordVariants = currentWordToItsTransformedWordVariants.getValue();
+
+            System.out.print("Key - " + currentWord + " | ");
             System.out.println("Value - " + itsTransformedWordVariants);
         }
     }
 
+    /**
+     * 在构建好的图中，找到 所需要的路径 pathTowardsEndWord，并把它添加到 pathList中
+     */
     private static void generatePathListViaDFS(String beginWord,
                                                String endWord,
+                                               Map<String, List<String>> wordsToItsTransformedVariantsMap,
                                                List<String> pathTowardsEndWord,
-                                               List<List<String>> allValidTransformSequenceList,
-                                               Map<String, List<String>> wordsToItsValidVariantsMap) {
-        // 如果起始单词 与 目标单词 相同，说明 不需要进行任何转换 就得到了目标单词，则：
+                                               List<List<String>> allShortestPathList) {
+        // 如果起始单词 与 目标单词 相同，说明
+        // #1 要么 不需要进行任何转换 就得到了目标单词 OR
+        // #2 要么 转换序列终于转换到了 目标单词，则：
         if (beginWord.equals(endWord)) {
-            // 把 当前到endWord的path 添加到 结果列表中
-            allValidTransformSequenceList.add(new ArrayList(pathTowardsEndWord)); // 因为list会被回溯，所以我们使用的是一个新的副本
+            // 把 当前到endWord的路径（而不是目标单词本身） 添加到 结果列表中
+            allShortestPathList.add(new ArrayList(pathTowardsEndWord)); // 因为path本身会被回溯而变化，所以我们使用的是一个新的副本
             // 即刻返回 以 停止当前级方法的继续调用，返回到上一级
             return;
         }
 
-        // 如果“当前起始单词”不存在任何“有效的单词变体”，说明 由当前起始单词经“有效单词变体” 无法转变成为“目标单词”，则：
-        if (wordsToItsValidVariantsMap.get(beginWord) == null) {
+        // 如果“当前起始单词” 不存在 任何“有效的单词变体”，说明 由当前起始单词经“有效单词变体” 无法转变成为“目标单词”，则：
+        if (wordsToItsTransformedVariantsMap.get(beginWord) == null) {
             // 直接return 以 停止当前级方法的调用
             return;
         }
 
         // 如果“当前起始单词” 存在有 一堆的“有效的单词变体”，说明 由当前起始单词 经“有效单词变体” 有可能转变得到“目标单词”，则：
         // 遍历每一个“有效的单词变体”
-        for (String currentValidWordVariant : wordsToItsValidVariantsMap.get(beginWord)) {
+        for (String currentTransformedWordVariant : wordsToItsTransformedVariantsMap.get(beginWord)) {
             // 把 当前“有效的单词变体” 添加到 “由起始单词转变到目标单词的路径”中
-            pathTowardsEndWord.add(currentValidWordVariant);
+            pathTowardsEndWord.add(currentTransformedWordVariant);
 
             // 调用dfs 以 得到完整的“到目标单词的路径”
-            generatePathListViaDFS(currentValidWordVariant, endWord, pathTowardsEndWord, allValidTransformSequenceList, wordsToItsValidVariantsMap);
+            generatePathListViaDFS(currentTransformedWordVariant, // 子问题：此参数发生了变化
+                    endWord,
+                    wordsToItsTransformedVariantsMap,
+                    pathTowardsEndWord, // path参数也发生了变化
+                    allShortestPathList);
 
             // 回溯当前选择 以便选择下一个“有效的单词变体” 来 构造“到目标单词的路径”
             pathTowardsEndWord.remove(pathTowardsEndWord.size() - 1);
@@ -114,70 +129,78 @@ public class Solution_findLadders_via_bfs_and_dfs_happygirllzt {
      */
     private static void generateTheMapViaBFS(Set<String> startWordSet,
                                              String endWord,
-                                             Map<String, List<String>> wordToItsTransformedValidVariants,
-                                             Set<String> availableValidWordVariantSet) {
-        /* Ⅰ 为正确地扩展出图map做一些准备工作 */
-        // 如果startSet的size为0，说明 图已经没有下一层顶点了（aka 图已经创建完成），则：直接return
-        if (startWordSet.size() == 0) return;
+                                             Map<String, List<String>> wordToItsTransformedVariantsMap,
+                                             Set<String> availableWordVariantSet) {
+        /* 〇 为正确地扩展出图map做一些准备工作 */
+        // 如果startSet的size为0，说明 图已经没有下一层顶点了（aka 图已经创建完成），则：
+        if (startWordSet.size() == 0) {
+            // 直接return 跳出方法
+            return;
+        }
 
-        // 准备一个tempSet  用于存储序列中下一个位置可能的单词（aka 下一层顶点集合）
+        // 准备一个tempSet  用于存储序列中下一个位置可能的单词 | 图中下一层的所有结点（aka 下一层顶点集合）
         Set<String> startWordSetOnNextLevel = new HashSet<>();
 
-        // 准备一个布尔值  判断转换序列是不是已经走到了结尾
+        // 准备一个布尔值  用于判断转换序列是不是已经到达了结尾 | 图是否已经构建完成
         boolean isTransformFinished = false;
 
-        // 从 可选的“有效单词变体集合”中，移除 当前的“起始单词集合”
-        availableValidWordVariantSet.removeAll(startWordSet);
+        // Ⅰ 先从 “可用的单词变体集合”中，移除 当前的“起始单词集合” 来 避免图中出现环??
+        availableWordVariantSet.removeAll(startWordSet);
 
         /* Ⅱ 准备一个循环 在当前level上对图进行扩展 */
-        // 遍历startSet中的每一个单词，找到其合法的转换结果，并添加到下一层中（并构建图map）/或者直接返回
+        // 遍历startWordSet中的每一个单词，找到其 所有可用的转换结果 #1 添加到下一层中；#2 并构建图map；/或者直接返回
         for (String currentStartWordStr : startWordSet) {
-            // 列举当前单词可能转换到的有效字符串
+            // 列举当前单词可能转换到的 所有可用的转换结果
             char[] currentStartWordCharacterArr = currentStartWordStr.toCharArray();
+
+            // 对于“当前起始单词”中的每一个位置...
             for (int currentSpotToReplace = 0; currentSpotToReplace < currentStartWordCharacterArr.length; currentSpotToReplace++) {
+                // 获取到 当前位置上的字符
                 char originalCharacter = currentStartWordCharacterArr[currentSpotToReplace];
 
+                // 尝试使用[a-z]范围内的所有字符 对其逐一替换，以得到“所有可能的单词变体”
                 for (char replacedCharacterOption = 'a'; replacedCharacterOption <= 'z'; replacedCharacterOption++) {
+                    // 替换字符
                     currentStartWordCharacterArr[currentSpotToReplace] = replacedCharacterOption;
-                    String replacedCharacterWord = new String(currentStartWordCharacterArr);
+                    // 得到一个“替换字符后的单词变体”
+                    String replacedCharacterWordVariant = new String(currentStartWordCharacterArr);
 
-                    // 如果“替换字符后的字符串”存在于“有效的单词变体集合”中，说明 找到了一个 序列中的“转换中间结果”，则：
-                    if (availableValidWordVariantSet.contains(replacedCharacterWord)) {
-                        /* #1 判断“替换字符后的字符串” 是不是 就是 “目标字符串” */
-                        // #1-① 如果是，说明 转换序列已经结束，则：
-                        if (endWord.equals(replacedCharacterWord)) {
-                            // 把 flag变量 设置为true
+                    // 如果“替换字符后的字符串”存在于“有效的单词变体集合”中，说明 找到了一个 结果序列中的“转换中间结果”，则：
+                    // 🐖 这就是为什么我们需要 先移除startWordSet，否则 我们无法据此判断是不是找到了一个“转换中间结果”
+                    if (availableWordVariantSet.contains(replacedCharacterWordVariant)) {
+                        /* #1 把当前“转换中间结果” 添加到 下一层的“起始单词”集合中 */
+                        // 如果当前“替换字符后的单词变体” 就是 “目标单词”/“结束单词”，说明 已经得到了完整的转换序列，不需要继续转换，则：
+                        if (endWord.equals(replacedCharacterWordVariant)) {
+                            // 把 标记转换是否结束的变量 设置为true
                             isTransformFinished = true;
-                        } else {
-                            // #1-② 如果不是，说明转换序列还没有结束，则：
-                            // 把 这个“转换中间结果” 添加到一个 set对象中
-                            startWordSetOnNextLevel.add(replacedCharacterWord);
+                        } else { // 否则，说明转换序列还没有结束，则：
+                            // 把 这个“转换中间结果” 添加到 表示“图中下一层结点”的set对象中 来 为扩展图的下一层做准备
+                            startWordSetOnNextLevel.add(replacedCharacterWordVariant);
                         }
 
-                        /* #2 把 “当前startWord” -> 其所能转换到的所有“有效中间结果” 的映射 添加到 xxx 中 */
-                        // #2-① 新增条目
-                        if (wordToItsTransformedValidVariants.get(currentStartWordStr) == null) {
-                            wordToItsTransformedValidVariants.put(currentStartWordStr, new ArrayList<>());
-                        }
-                        // #2-② 向条目中添加item
-                        wordToItsTransformedValidVariants.get(currentStartWordStr).add(replacedCharacterWord);
+                        /* #2 把 “当前startWord” -> 当前“转换中间结果” 的映射 添加到 用于存储转换映射关系的map对象 中 */
+                        // #2-① 如果缺失条目，则先新增条目
+                        // 🐖 computeIfAbsent() 用来 替换 “if(null) {}”的语句块
+                        wordToItsTransformedVariantsMap.computeIfAbsent(currentStartWordStr, key -> new ArrayList<>());
+                        // #2-② 再向映射关系的该条目中，添加其所能转换到item
+                        wordToItsTransformedVariantsMap.get(currentStartWordStr).add(replacedCharacterWordVariant);
                     }
                 }
 
-                // 恢复当前位置上的字符，以便在其他位置继续尝试通过转换 来 得到中间结果或最终结果
+                // 恢复当前位置上的字符，以便在其他位置继续尝试通过转换 来 得到其他的中间结果或最终结果
                 currentStartWordCharacterArr[currentSpotToReplace] = originalCharacter;
             }
         }
 
-        /* Ⅲ 判断图是否已经扩展完成，如果没有，使用bfs继续进行扩展 */
-        // 如果经过上述循环后，finish仍旧为false，说明 仍旧没有找到endWord，则：
+        /* Ⅲ 判断图/Map是否已经扩展完成，如果没有，则：使用bfs继续进行下一层的扩展 */
+        // 经过上述循环后，如果finish仍旧为false，说明 仍旧没有找到endWord，则：
         if (!isTransformFinished) {
-            // 继续做bfs 来 进一步建立图（查找到endWord时，图就建立完成了）
-            // 🐖 此时 startWordSet 的值就是 startWordSetOnNextLevel了 - 也就是图中 下一个level的所有结点
-            generateTheMapViaBFS(startWordSetOnNextLevel, endWord, wordToItsTransformedValidVariants, availableValidWordVariantSet);
+            // 继续构建图 / map（当查找到endWord时，图就建立完成了）
+            generateTheMapViaBFS(startWordSetOnNextLevel, // 🐖 此时 startWordSet 的值就是 startWordSetOnNextLevel了 - 也就是图中 下一个level的所有结点
+                    endWord,
+                    wordToItsTransformedVariantsMap,
+                    availableWordVariantSet);
         }
-
         // 如果finish为true，则：方法结束，图已经创建完成
     }
-} // this method failed, I don't know why. but let's skip to next method first
-// super stupid：原因是因为传入的beginWord 与 endWord参数传成了空字符串
+}
--- src/main/java/com/henry/leetcode_traning_camp/week_04/day03/solitaire_02/Solution_findLadders_via_liweiwei.java
+++ src/main/java/com/henry/leetcode_traning_camp/week_04/day03/solitaire_02/Solution_findLadders_via_liweiwei.java
@@ -6,36 +6,39 @@ public class Solution_findLadders_via_liweiwei {
     public static void main(String[] args) {
         String beginWord = "hit";
         String endWord = "cog";
-        List<String> wordList =
+        List<String> availableWordVariants =
                 new ArrayList<>(
-                        Arrays.asList("hot","dot","dog","lot","log","cog")
+                        Arrays.asList("hot", "dot", "dog", "lot", "log", "cog")
                 );
 
-        List<List<String>> res = findLadders(beginWord, endWord, wordList);
-        System.out.println("最终的结果为：" + res);
+        List<List<String>> allShortestPathList = findLadders(beginWord, endWord, availableWordVariants);
+        System.out.println("最终的结果为：" + allShortestPathList);
     }
 
-    private static List<List<String>> findLadders(String beginWord, String endWord, List<String> wordList) {
+    private static List<List<String>> findLadders(String beginWord,
+                                                  String endWord,
+                                                  List<String> availableWordVariants) {
         // 先将 wordList 放到哈希表里，便于判断某个单词是否在 wordList 里
-        Set<String> wordSet = new HashSet<>(wordList);
-        List<List<String>> res = new ArrayList<>();
-        if (wordSet.size() == 0 || !wordSet.contains(endWord)) {
-            return res;
+        Set<String> availableWordSet = new HashSet<>(availableWordVariants);
+        List<List<String>> allShortestPathList = new ArrayList<>();
+
+        if (availableWordSet.size() == 0 || !availableWordSet.contains(endWord)) {
+            return allShortestPathList;
         }
 
         // 第 1 步：使用广度优先遍历得到后继结点列表 successors
-        // key：字符串，value：广度优先遍历过程中 key 的后继结点列表
+        // key：字符串; value：广度优先遍历过程中 key 的后继结点列表
         Map<String, Set<String>> successors = new HashMap<>();
-        boolean found = bfs(beginWord, endWord, wordSet, successors);
-        if (!found) {
-            return res;
+        boolean foundEndWord = buildTheMapViaBFS(beginWord, endWord, availableWordSet, successors);
+        if (!foundEndWord) {
+            return allShortestPathList;
         }
 
         // 第 2 步：基于后继结点列表 successors ，使用回溯算法得到所有最短路径列表
         Deque<String> path = new ArrayDeque<>();
         path.addLast(beginWord);
-        dfs(beginWord, endWord, successors, path, res);
-        return res;
+        dfs(beginWord, endWord, successors, path, allShortestPathList);
+        return allShortestPathList;
     }
 
     private static void dfs(String beginWord, String endWord, Map<String, Set<String>> successors, Deque<String> path, List<List<String>> res) {
@@ -56,60 +59,74 @@ public class Solution_findLadders_via_liweiwei {
         }
     }
 
-    private static boolean bfs(String beginWord, String endWord, Set<String> wordSet, Map<String, Set<String>> successors) {
-        Queue<String> queue = new LinkedList<>();
-        queue.offer(beginWord);
+    private static boolean buildTheMapViaBFS(String beginWord,
+                                             String endWord,
+                                             Set<String> availableWordSet,
+                                             Map<String, Set<String>> wordToItsSuccessorsMap) {
+        Queue<String> beginWordsOnCurrentLevel = new LinkedList<>();
+        beginWordsOnCurrentLevel.offer(beginWord);
 
         // 标准写法，记录方法问过的单词
-        Set<String> visited = new HashSet<>();
-        visited.add(beginWord);
+        Set<String> visitedVertexSet = new HashSet<>();
+        visitedVertexSet.add(beginWord);
 
-        boolean found = false;
-        int wordLen = beginWord.length();
-        // 当前层访问过的结点，当前层全部遍历完成以后，再添加到总的 visited 集合里
-        Set<String> nextLevelVisited = new HashSet<>();
-        while (!queue.isEmpty()) {
-            int currentSize = queue.size();
-            for (int i = 0; i < currentSize; i++) {
-                String currentWord = queue.poll();
-                char[] charArray = currentWord.toCharArray();
-                for (int j = 0; j < wordLen; j++) {
-                    char originChar = charArray[j];
-                    for (char k = 'a'; k <= 'z'; k++) {
-                        if (charArray[j] == k) {
+        boolean doesEndWordFound = false;
+        int beginWordLength = beginWord.length();
+
+        // 当前层访问过的结点，当前层全部遍历完成以后，再添加到总的 visitedVertexSet 集合里
+        Set<String> nextLevelVisited = new HashSet<>(); // 为什么要设置这个变量,具体什么作用?? 不好理解... 😳
+
+        while (!beginWordsOnCurrentLevel.isEmpty()) {
+            int wordAmountOnCurrentLevel = beginWordsOnCurrentLevel.size();
+
+            for (int currentWordCursor = 0; currentWordCursor < wordAmountOnCurrentLevel; currentWordCursor++) {
+                String currentBeginWord = beginWordsOnCurrentLevel.poll();
+                char[] currentBeginWordArr = currentBeginWord.toCharArray();
+
+                for (int currentCharacterCursor = 0; currentCharacterCursor < beginWordLength; currentCharacterCursor++) {
+                    char originCharacter = currentBeginWordArr[currentCharacterCursor];
+
+                    for (char optionalChar = 'a'; optionalChar <= 'z'; optionalChar++) {
+                        if (currentBeginWordArr[currentCharacterCursor] == optionalChar) {
                             continue;
                         }
-                        charArray[j] = k;
-                        String nextWord = new String(charArray);
-                        if (wordSet.contains(nextWord)) {
-                            if (!visited.contains(nextWord)) {
-                                if (nextWord.equals(endWord)) {
-                                    found = true;
+                        // 使用 可选的字符 来 替换原始字符
+                        currentBeginWordArr[currentCharacterCursor] = optionalChar;
+                        // 得到替换后的字符串
+                        String transformedWord = new String(currentBeginWordArr);
+                        // 如果 转换后的字符串 存在于 可用的字符变体集合中，说明 得到了一个 转换的中间单词
+                        if (availableWordSet.contains(transformedWord)) {
+                            // 如果 该中间单词还没有 被访问过，说明 ???，则：
+                            if (!visitedVertexSet.contains(transformedWord)) {
+                                // 判断 该中间单词 是不是 与目标单词相等，如果是，说明 找到了目标单词，则：
+                                if (transformedWord.equals(endWord)) {
+                                    // 更新flag变量的值
+                                    doesEndWordFound = true;
                                 }
 
                                 // 避免下层元素重复加入队列，这里感谢 https://leetcode-cn.com/u/zhao-da-ming/ 优化了这个逻辑
-                                if (!nextLevelVisited.contains(nextWord)) {
-                                    queue.offer(nextWord);
-                                    nextLevelVisited.add(nextWord);
+                                if (!nextLevelVisited.contains(transformedWord)) {
+                                    beginWordsOnCurrentLevel.offer(transformedWord);
+                                    nextLevelVisited.add(transformedWord);
                                 }
 
                                 // 维护 successors 的定义
-                                successors.computeIfAbsent(currentWord, a -> new HashSet<>());
-                                successors.get(currentWord).add(nextWord);
+                                wordToItsSuccessorsMap.computeIfAbsent(currentBeginWord, a -> new HashSet<>());
+                                wordToItsSuccessorsMap.get(currentBeginWord).add(transformedWord);
                             }
                         }
                     }
-                    charArray[j] = originChar;
+                    currentBeginWordArr[currentCharacterCursor] = originCharacter;
                 }
             }
 
-            if (found) {
+            if (doesEndWordFound) {
                 break;
             }
-            visited.addAll(nextLevelVisited);
+            visitedVertexSet.addAll(nextLevelVisited);
             nextLevelVisited.clear();
         }
-        return found;
+        return doesEndWordFound;
     }
 } // 感觉上weiwei的解法还是有点复杂 关键没有配套视频说明
 // 做法：熟悉一下图的数据结构表示————邻接表。以及创建图的BFS & 遍历图的DFS
