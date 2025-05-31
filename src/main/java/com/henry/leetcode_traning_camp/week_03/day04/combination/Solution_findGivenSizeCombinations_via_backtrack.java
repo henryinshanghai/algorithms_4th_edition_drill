f46d260c43dcdce59be0b6d926ed15a9f7f4d363
HenryInSH <2291972433@qq.com>
2025-05-31 18:14:48 +0800

    --- 回溯算法应用之 得到所有可能的组合结果：#1 先添加当前元素；#2 再从剩余的可选集合中添加剩余元素；#3 最后回退“当前选择” 来 继续得到其他的可能结果。 ---

--- src/main/java/com/henry/leetcode_traning_camp/week_03/day04/combination/Solution_findGivenSizeCombinations_via_backtrack.java
+++ src/main/java/com/henry/leetcode_traning_camp/week_03/day04/combination/Solution_findGivenSizeCombinations_via_backtrack.java
@@ -8,13 +8,14 @@ import java.util.List;
 // 验证：对于“所有可能构成的组合”的问题，可以使用  回溯算法 来 在选择当前元素后，再撤销“对当前元素的选择”。最终得到完整的决策树 aka 所有可能的组合
 // 🐖 #1 回溯算法对应的问题模型是一个 “多叉树”，因此会需要 在循环中使用递归(也就是多次调用递归)；
 // #2 既然回溯本身由递归实现，那么就必然满足：子问题的解 能够帮助解决原始问题[这个是递归的性质]。因此回溯所能解决的问题 也会有自己的“子问题”
+// 子问题是如何帮助解决原始问题的呢？答：对于拼凑类型的问题，子问题的解 就是原始问题的解的一部分！
 public class Solution_findGivenSizeCombinations_via_backtrack {
     public static void main(String[] args) {
         int availableMaxItem = 4; // 原始集合的大小
         int combinationItemAmount = 2; // 满足条件的组合的大小
 
         // 获取到 所有的组合的列表
-        List<List<Integer>> combinationList = getCombinationFrom(availableMaxItem, combinationItemAmount);
+        List<List<Integer>> combinationList = getCombinationListFrom(availableMaxItem, combinationItemAmount);
         // 逐个打印 所有组合
         print(combinationList);
     }
@@ -44,14 +45,14 @@ public class Solution_findGivenSizeCombinations_via_backtrack {
     // 准备一个列表   用于作为当前构造出的合法组合
     private static Deque<Integer> currentGeneratedCombination = new LinkedList<>();
 
-    private static List<List<Integer>> getCombinationFrom(int availableMaxItem, int expectedCombinationItemAmount) {
+    private static List<List<Integer>> getCombinationListFrom(int availableMaxItem, int expectedCombinationsItemAmount) {
         // #0 健壮性代码 对传入的参数进行判断
-        if (expectedCombinationItemAmount <= 0 || expectedCombinationItemAmount > availableMaxItem) {
+        if (expectedCombinationsItemAmount <= 0 || expectedCombinationsItemAmount > availableMaxItem) {
             return combinationList;
         }
 
         // #1 利用回溯算法 来 得到“所有可能的组合的集合”
-        constructGivenSizeCombinationFrom(availableMaxItem, expectedCombinationItemAmount, 1);
+        constructGivenSizeCombinationFrom(availableMaxItem, expectedCombinationsItemAmount, 1);
 
         // #2 返回 该组合的集合
         return combinationList;
@@ -75,10 +76,10 @@ public class Solution_findGivenSizeCombinations_via_backtrack {
         for (int currentItem = dynamicStartAnchorItem; currentItem <= availableMaxItem; currentItem++) {
             // #2-1 向“当前组合”中添加“当前元素” {1} | {2} | {3} | {4}
             currentGeneratedCombination.addLast(currentItem);
-            // #2-2 递归地 从剩余的(N-1)个元素中，挑选出剩下的(K-1)个元素  aka 补齐集合中的剩余元素 👇
-            // 子问题：在包含有(N-1)个自然数的序列中，挑选出(K-1)个元素 构成“大小为(K-1)的组合”
+            // #2-2 递归地 从剩余的(N-1)个元素中，挑选出剩余的所需元素  以此来 补齐组合所需的其他元素 👇
+            // 子问题：在包含有(N-1)个自然数的序列中，挑选出(K-1)个元素 构成“大小为(K-1)的组合” 最终使得组合的元素数量 == 预期的组合尺寸
             // 子问题的解 就是原始问题的解的一部分
-            constructGivenSizeCombinationFrom(availableMaxItem, expectedCombinationItemAmount, currentItem + 1); // 注意这里的代码时i + 1,而不是anchorItem + 1
+            constructGivenSizeCombinationFrom(availableMaxItem, expectedCombinationItemAmount, currentItem + 1); // 注意这里的代码是i + 1,而不是anchorItem + 1
             // #2-3 回溯掉”当前的选择“，以便 算法能够”重新选择“后继的元素
             currentGeneratedCombination.removeLast();
         }
--- src/main/java/com/henry/leetcode_traning_camp/week_03/day04/combination/Solution_findGivenSizeCombinations_via_backtrack_cutBranch.java
+++ src/main/java/com/henry/leetcode_traning_camp/week_03/day04/combination/Solution_findGivenSizeCombinations_via_backtrack_cutBranch.java
@@ -6,24 +6,25 @@ import java.util.LinkedList;
 import java.util.List;
 
 // 验证：回溯算法的过程 会产生很多没有用的分支，对于这样的分支，可以通过代码来把它们剪枝，从而提高代码运行效率；
-// 剪枝主要针对for循环的执行次数 - 我们想要执行“有效的循环”。对于组合问题，有效的循环的条件是：剩余可用的元素数量 + 1 >= 构造组合还需要的元素数量
+// 剪枝主要针对for循环的执行次数 - 我们想要执行“有效的循环”。
+// 对于组合问题，有效的循环的条件是：剩余可用的元素数量 + 1 >= 构造组合还需要的元素数量
 // 🐖 +1是因为currentItem是从1开始的
 public class Solution_findGivenSizeCombinations_via_backtrack_cutBranch {
     public static void main(String[] args) {
         int availableMaxItem = 4;
         int expectedCombinationSize = 2;
 
-        List<List<Integer>> allLegitCombinations = combine(availableMaxItem, expectedCombinationSize);
+        List<List<Integer>> allLegitCombinations = getCombinationListFrom(availableMaxItem, expectedCombinationSize);
 
         System.out.println(allLegitCombinations);
     }
 
-    private static List<List<Integer>> combine(int availableMaxItem, int expectedCombinationSize) {
+    private static List<List<Integer>> getCombinationListFrom(int availableMaxItem, int expectedCombinationSize) {
         if (expectedCombinationSize <= 0 || expectedCombinationSize > availableMaxItem) {
             return legitCombinationList;
         }
 
-        dfs(availableMaxItem, expectedCombinationSize, 1);
+        constructExpectedCombinationIncrementally(availableMaxItem, expectedCombinationSize, 1);
 
         return legitCombinationList;
     }
@@ -31,20 +32,23 @@ public class Solution_findGivenSizeCombinations_via_backtrack_cutBranch {
     private static Deque<Integer> currentGeneratedCombination = new LinkedList<>();
     private static List<List<Integer>> legitCombinationList = new ArrayList<>();
 
-    private static void dfs(int availableMaxItem, int expectedCombinationSize, int startAnchorItem) {
+    private static void constructExpectedCombinationIncrementally(int availableMaxItem, int expectedCombinationSize, int startAnchorItem) {
         if (currentGeneratedCombination.size() == expectedCombinationSize) {
             List<Integer> legitCombination = new ArrayList<>(currentGeneratedCombination);
             legitCombinationList.add(legitCombination);
             return;
         }
 
-        // 有效的遍历条件：剩余可用的元素数量 >= 组合还需要的元素数量
+        // 有效的遍历条件：剩余可用的元素数量(availableMaxItem - requiredItemAmount) >= 组合还需要的元素数量
         int requiredItemAmount = expectedCombinationSize - currentGeneratedCombination.size();
         // 剩余可用的元素数量 = availableMaxItem - currentItem;
         // 示例：最开始时 availableMaxItem=4, currentItem=1, requiredItemAmount=4。所以需要 在不等式右边+1
         for (int currentItem = startAnchorItem; currentItem <= availableMaxItem - requiredItemAmount + 1; currentItem++) {
+            // #1 向组合中添加一个元素
             currentGeneratedCombination.addLast(currentItem);
-            dfs(availableMaxItem, expectedCombinationSize, currentItem + 1);
+            // #2 向组合中添加 剩余的元素（currentItem+1的作用是 避免已经被使用的元素 再度被使用）
+            constructExpectedCombinationIncrementally(availableMaxItem, expectedCombinationSize, currentItem + 1);
+            // #3 从组合中回退掉#1中所添加的元素
             currentGeneratedCombination.removeLast();
         }
     }
