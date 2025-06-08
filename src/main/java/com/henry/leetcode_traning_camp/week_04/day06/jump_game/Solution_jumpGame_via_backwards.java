f540aa159cce669cbef69b436752532446bae84c
HenryInSH <2291972433@qq.com>
2025-06-08 11:20:31 +0800

    --- 逆向思维应用之 能否到达序列终点位置：如果存在到达终点位置的跳法，那么 第一个“需要到达的位置”/“目标位置”一定是 起始位置。 ---

--- src/main/java/com/henry/leetcode_traning_camp/week_04/day06/jump_game/Solution_jumpGame_via_backwards.java
+++ src/main/java/com/henry/leetcode_traning_camp/week_04/day06/jump_game/Solution_jumpGame_via_backwards.java
@@ -5,29 +5,29 @@ package com.henry.leetcode_traning_camp.week_04.day06.jump_game;
 // 原理：按此方式回溯，如果 能够跳转到终点，则 第一个“当前需要到达的位置” 一定会是位置0
 public class Solution_jumpGame_via_backwards {
     public static void main(String[] args) {
-//        int[] currentSpotToItsMaxJumpDistance = {2,3,1,1,4};
-        int[] currentSpotToItsMaxJumpDistance = {3, 2, 1, 0, 4};
-        boolean canOrNot = ableToReachEnd(currentSpotToItsMaxJumpDistance);
-        System.out.println("按照当前数组能够跳到数组末尾？" + canOrNot);
+        int[] currentSpotToItsMaxJumpDistance = {2,3,1,1,4};
+//        int[] currentSpotToItsMaxJumpDistance = {3, 2, 1, 0, 4};
+        boolean yesOrNo = IsAbleToReachEnd(currentSpotToItsMaxJumpDistance);
+        System.out.println("按照当前数组能否跳到数组末尾？" + yesOrNo);
     }
 
-    private static boolean ableToReachEnd(int[] currentSpotToItsMaxJumpDistance) {
+    private static boolean IsAbleToReachEnd(int[] currentSpotToItsMaxJumpDistance) {
         // 〇 准备一个变量 用于记录当前需要到达的位置
         int spotNeedToReach = currentSpotToItsMaxJumpDistance.length - 1;
 
         // Ⅰ 从后往前遍历数组   更新“当前需要到达的位置”
-        // 对于 目标位置 之前的每一个位置...
+        // 对于 目标位置 之前的每一个位置，判断 由它能否跳跃到达到 “需要到达的位置”/“目标位置”
         for (int backwardsCursor = currentSpotToItsMaxJumpDistance.length - 2; backwardsCursor >= 0; backwardsCursor--) {
             int maxJumpDistanceOnCurrentSpot = currentSpotToItsMaxJumpDistance[backwardsCursor];
             // 如果 当前位置 + 当前位置上所能跳出的最大距离 大于等于 目标位置，说明 能够 由当前位置 到达 目标位置，则：
             if (backwardsCursor + maxJumpDistanceOnCurrentSpot >= spotNeedToReach) {
-                // 使用 当前位置 来 更新目标位置
+                // 使用 当前位置 来 更新“需要到达的位置”/“目标位置”
                 spotNeedToReach = backwardsCursor;
             }
         }
 
-        // Ⅱ 判断最终“需要到达的位置” 是不是 落到了起点位置
-        // 原理：我们唯一确定的是 - 起点是可达的
+        // Ⅱ 判断最终“反向需要到达的位置” 是不是 落到了“起点位置”
+        // 原理：我们唯一能够确定的是 - 起点是无条件可达的
         return spotNeedToReach == 0;
     }
 }
