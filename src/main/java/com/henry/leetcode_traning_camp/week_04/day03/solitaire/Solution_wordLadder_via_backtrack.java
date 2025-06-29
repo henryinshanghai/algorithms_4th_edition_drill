1a3d4772829290012e4462c51a1639cc56e41a70
HenryInSH <2291972433@qq.com>
2025-06-30 00:51:21 +0800

    --- BFS应用之求出最短转换路径的长度：相关概念 validMiddleWord：有效的中间单词；替换字母后的字符串：replacedResult. ---

--- src/main/java/com/henry/leetcode_traning_camp/week_04/day03/solitaire/Solution_wordLadder_via_backtrack.java
+++ src/main/java/com/henry/leetcode_traning_camp/week_04/day03/solitaire/Solution_wordLadder_via_backtrack.java
@@ -10,63 +10,65 @@ public class Solution_wordLadder_via_backtrack {
         String beginWord = "hit";
         String endWord = "cog";
 
-        List<String> availableWordVariants = new ArrayList<>(
+        List<String> validMiddleWordList = new ArrayList<>(
                 Arrays.asList("hot", "dot", "dog", "lot", "log", "cog")
         );
 
-        int shortestTransformSequenceLength = getShortestLadderLength(beginWord, endWord, availableWordVariants);
+        int shortestTransformSequenceLength = getShortestLadderLength(beginWord, endWord, validMiddleWordList);
         System.out.println("从 \"" + beginWord + "\" 到 \"" + endWord + "\" 的最短转换序列的长度为： " + shortestTransformSequenceLength);
     }
 
-    private static int getShortestLadderLength(String beginWord, String endWord, List<String> availableWordVariantList) {
-        Set<String> availableWordVariantSet = new HashSet<>(availableWordVariantList);
+    private static int getShortestLadderLength(String beginWord,
+                                               String endWord,
+                                               List<String> validMiddleWordList) {
+        Set<String> validMiddleWordSet = new HashSet<>(validMiddleWordList);
 
-        Queue<String> currentLevelWordVariantsQueue = new LinkedList<>();
-        currentLevelWordVariantsQueue.add(beginWord);
+        Queue<String> validReplacedResultQueue = new LinkedList<>();
+        validReplacedResultQueue.add(beginWord);
 
         int currentLevelSerialNumber = 0;
 
         // 每轮循环 处理图中的一层结点
-        while (!currentLevelWordVariantsQueue.isEmpty()) {
+        while (!validReplacedResultQueue.isEmpty()) {
             // #1 获取 图中 当前层上的节点数量
-            int variantWordAmountOnCurrentLevel = currentLevelWordVariantsQueue.size();
+            int middleWordAmountOnCurrentLevel = validReplacedResultQueue.size();
 
             // #2 基于 当前层中的单词 + 可用的单词变体 来 扩展出下一层的单词（OR 返回结果序列的总长度）
-            for (int currentVariantWordCursor = 0; currentVariantWordCursor < variantWordAmountOnCurrentLevel; currentVariantWordCursor++) {
+            for (int currentLevelCursor = 0; currentLevelCursor < middleWordAmountOnCurrentLevel; currentLevelCursor++) {
                 // 对于“当前层中的当前单词”...
-                String currentValidVariantWord = currentLevelWordVariantsQueue.poll();
+                String currentMiddleWord = validReplacedResultQueue.poll();
 
                 /* 特殊场景：找到了目标单词 */
                 // 如果 当前单词 与 目标单词 相等，说明 找到了 “转化到目标单词的最短序列”，则：
-                if (currentValidVariantWord.equals(endWord))
+                if (currentMiddleWord.equals(endWord))
                     // 把 当前转换序列的长度 + 1（目标单词） 来 作为 转换序列的长度 返回
                     return currentLevelSerialNumber + 1;
 
                 /* 得到 所有由“当前单词” 所能转换得到的 单词变体，并以此来 #1 扩展队列； #2 更新可用单词变体集合 */
-                char[] currentValidVariantCharacterArr = currentValidVariantWord.toCharArray();
+                char[] currentMiddleWordCharacterArr = currentMiddleWord.toCharArray();
                 // 对于每一个当前位置...
-                for (int currentSpotToReplace = 0; currentSpotToReplace < currentValidVariantCharacterArr.length; currentSpotToReplace++) {
+                for (int currentSpotToReplace = 0; currentSpotToReplace < currentMiddleWordCharacterArr.length; currentSpotToReplace++) {
                     // 获取到 当前位置上的字符
-                    char characterOnCurrentSpot = currentValidVariantCharacterArr[currentSpotToReplace];
+                    char characterOnCurrentSpot = currentMiddleWordCharacterArr[currentSpotToReplace];
 
                     // 使用26个字母 来 逐个尝试替换当前位置上的字符...
-                    for (char replacedCharacterOption = 'a'; replacedCharacterOption <= 'z'; replacedCharacterOption++) {
+                    for (char replacedLetter = 'a'; replacedLetter <= 'z'; replacedLetter++) {
                         // 先替换掉 当前位置上的字符
-                        currentValidVariantCharacterArr[currentSpotToReplace] = replacedCharacterOption;
+                        currentMiddleWordCharacterArr[currentSpotToReplace] = replacedLetter;
                         // 而后得到 “替换字符后的单词变体”
-                        String replacedWordVariantStr = new String(currentValidVariantCharacterArr);
+                        String currentReplacedResult = new String(currentMiddleWordCharacterArr);
 
                         // 如果 该“替换字符后的单词变体” 存在于 有效单词变体集合 中，说明 找到了转换的 “下一个有效中间结果”，则：
-                        if (availableWordVariantSet.contains(replacedWordVariantStr)) {
+                        if (validMiddleWordSet.contains(currentReplacedResult)) {
                             // #1 把 “该有效中间结果” 添加到 “单词队列”中
-                            currentLevelWordVariantsQueue.add(replacedWordVariantStr);
+                            validReplacedResultQueue.add(currentReplacedResult);
                             // #2 从 “有效的单词变体集合” 中，移除 “该有效中间结果” - 以防止序列中出现环
-                            availableWordVariantSet.remove(replacedWordVariantStr);
+                            validMiddleWordSet.remove(currentReplacedResult);
                         }
                     }
 
                     // 恢复 当前位置上的字符，继续产生其他单词变体
-                    currentValidVariantCharacterArr[currentSpotToReplace] = characterOnCurrentSpot;
+                    currentMiddleWordCharacterArr[currentSpotToReplace] = characterOnCurrentSpot;
                 }
             }
 
