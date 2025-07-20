10555f25f0d44f64fb599c0a52a97084f7ade45e
HenryInSH <2291972433@qq.com>
2025-07-20 11:39:53 +0800

    --- 移除旧的文件 & 更新文件格式 ---

--- src/main/java/com/henry/leetcode_traning_camp/week_06/preview/longest_common_substr/Solution_via_dp_by_tushor.java
+++ src/main/java/com/henry/leetcode_traning_camp/week_06/preview/longest_common_substr/Solution_via_dp_by_tushor.java
@@ -15,14 +15,17 @@ public class Solution_via_dp_by_tushor {
         // 准备一个临时的二维数组  用于作为动态规划所需要的计算空间
         int currentComboToTheirLCSLength[][] = new int[str1CharArr.length + 1][str2CharArr.length + 1];
 
-
         /* Ⅰ 准备两层的嵌套循环    在循环中，填充二维数组中的每一个位置 */
+        // 从dp[1][1]开始来计算dp[][]元素   🐖 这里的游标指针 并不是 字符串中的字符指针
         for (int currentRow = 1; currentRow < currentComboToTheirLCSLength.length; currentRow++) {
             for (int currentColumn = 1; currentColumn < currentComboToTheirLCSLength[currentRow].length; currentColumn++) {
                 // EXPR：递推公式
                 // 1 如果两个指针指向的字符相同，说明：这个字符可以用于构成最长公共子串
-                char currentStr1Character = str1CharArr[currentRow - 1];
-                char currentStr2Character = str2CharArr[currentColumn - 1];
+                int currentStr1Cursor = currentRow - 1;
+                int currentStr2Cursor = currentColumn - 1;
+
+                char currentStr1Character = str1CharArr[currentStr1Cursor];
+                char currentStr2Character = str2CharArr[currentStr2Cursor];
 
                 if (currentStr1Character == currentStr2Character) {
                     // 则：当前位置的元素值 = 左上角位置的元素值 + 1
@@ -32,7 +35,8 @@ public class Solution_via_dp_by_tushor {
                     // 2 如果两个指针指向的字符不相同，说明当前字符不能用来构成最长公共子串
                     // 则：当前位置的元素值(公共子序列的长度) = Max(上一行（同一列）的元素值, 同一行(上一列)的元素值)
                     currentComboToTheirLCSLength[currentRow][currentColumn]
-                            = Math.max(currentComboToTheirLCSLength[currentRow][currentColumn - 1], currentComboToTheirLCSLength[currentRow - 1][currentColumn]);
+                            = Math.max(currentComboToTheirLCSLength[currentRow][currentColumn - 1],
+                                currentComboToTheirLCSLength[currentRow - 1][currentColumn]);
                 }
             }
         }
