14ca93266cf83640cad6a70f97247edd0f2ea985
HenryInSH <2291972433@qq.com>
2025-09-30 23:46:17 +0800

    --- BM算法（用于找到文本字符串中的模式字符串）：① 从后往前地 逐个比较 文本字符 与 模式字符；② 对两者不相同的情况，需要分类讨论；③ 如果文本字符 不存在于 模式字符串中，说明 以此位置为止 不可能产生匹配，则：把 文本指针 移动到该字符的下一个位置；④ 如果文本指针 存在于 模式字符串中，说明 我们可以 先把模式字符串 向右平移到 字符对齐的位置 然后再进行逐个字符的匹配，则：把文本指针 向后移动 对应的距离； ---

--- src/main/java/com/henry/string_05/search_substring_03/Boyer_Moore_02/execution/BoyerMoore.java
+++ src/main/java/com/henry/string_05/search_substring_03/Boyer_Moore_02/execution/BoyerMoore.java
@@ -45,29 +45,33 @@ import edu.princeton.cs.algs4.StdOut;
  * see <a href="https://algs4.cs.princeton.edu/53substring">Section 5.3</a> of
  * <i>Algorithms, 4th Edition</i> by Robert Sedgewick and Kevin Wayne.
  */
-// 验证：可以使用BoyerMoore算法(设置从前往后的文本指针与从后往前的模式指针，当字符不匹配时，把文本指针跳转到 下一个可能匹配的位置) 来 在文本字符串中查找 与模式字符串相匹配的子字符串
-// 字符匹配: pat_character = txt_character; 字符串匹配：每一个文本字符 都与模式字符匹配；
+// 验证：可以使用 BoyerMoore算法 来 在 文本字符串 中 查找 与模式字符串相匹配 的子字符串
+// BM算法步骤：
+// #1 设置 从前往后的 文本指针 与 从后往前的模式指针；
+// #2 当 字符不匹配 时，把 文本指针 跳转到 下一个可能匹配的位置.
+// 字符匹配: pat_character = txt_character; 字符串匹配：每一个文本字符 都 与模式字符 相匹配；
 // 概念：可能发生匹配的位置(current_txt_character)；
-// 特征：txt_cursor并不是直接指向 txt_character的，而是 txt_cursor + backward_pat_cursor
+// 特征：txt_cursor并不是 直接指向 txt_character的，而是指向 txt_cursor + backward_pat_cursor
 public class BoyerMoore {
-    private final int characterOptionAmount;     // the radix
-    private int[] characterToItsLastOccurrenceSpotInPatStr;     // characterToItsRightmostSpotInPatStr the bad-character skip array
+    private final int characterOptionAmount;     // 字符选项的数量
+    private int[] characterToItsLastOccurrenceSpotInPatStr;     // 字符 -> 该字符最后一次在模式字符串中出现的位置
 
-    private char[] patternCharacterArr;  // store the pattern as a character array
-    private String patternStr;      // or as a string
+    private char[] patternCharacterArr;  // 把 模式 存储为一个 字符数组
+    private String patternStr;      // 或者 存储为一个 字符串
 
     /**
-     * Preprocesses the pattern string.
-     * 预处理模式字符串,得到模式字符串对应的 characterToItsLastOccurrenceSpotInPatStr[]
-     * @param patternStr the pattern string
+     * 预处理 模式字符串,得到 模式字符串所对应的 characterToItsLastOccurrenceSpotInPatStr[]
+     *
+     * @param patternStr 模式字符串
      */
     public BoyerMoore(String patternStr) {
         this.patternStr = patternStr;
         this.characterOptionAmount = 256;
 
-        // 记录下 字符->它在模式字符串中最后一次出现的位置（使用数组） aka 跳跃表??
+        // 初始化 字符 -> 它在模式字符串中最后一次出现的位置（使用数组）为-1 aka 跳跃表??
         initLastOccurrenceSpotForCharacters();
 
+        // 使用 模式字符串 来 更新这个lastOccurrenceSpot[]数组
         renewLastOccurrenceSpotOfCharacterIn(patternStr);
     }
 
@@ -83,14 +87,15 @@ public class BoyerMoore {
             char currentPatCharacter = patternStr.charAt(currentCharacterSpot);
             characterToItsLastOccurrenceSpotInPatStr[currentPatCharacter] = currentCharacterSpot;
         }
-        // 那些“没有在模式字符串中出现”的字符，它所对应的 lastOccurrenceSpotInPatStr的值为-1
+        // 那些“没有在模式字符串中出现”的字符，它所对应的 lastOccurrenceSpotInPatStr的值 会是 -1
+        // 那些 在模式字符串中 多次出现的字符，它所对应的 lastOccurrenceSpotInPatStr的值 会是 它最后一次出现的位置
     }
 
     /**
-     * Preprocesses the pattern string.
-     * 预处理模式字符串
-     * @param patternCharacterArr the pattern string
-     * @param characterOptionAmount       the alphabet size
+     * 预处理模式字符串     重载方法
+     *
+     * @param patternCharacterArr   模式字符串
+     * @param characterOptionAmount 字母表大小
      */
     public BoyerMoore(char[] patternCharacterArr, int characterOptionAmount) {
         this.characterOptionAmount = characterOptionAmount;
@@ -99,35 +104,39 @@ public class BoyerMoore {
         for (int currentCharacterSpot = 0; currentCharacterSpot < patternCharacterArr.length; currentCharacterSpot++)
             this.patternCharacterArr[currentCharacterSpot] = patternCharacterArr[currentCharacterSpot];
 
-        // position of rightmost occurrence of c in the pattern
+        // 得到 指定字符 -> 该字符在模式字符串中 出现的最右位置
         characterToItsLastOccurrenceSpotInPatStr = new int[characterOptionAmount];
+        // 初始化元素值
         for (int currentCharacterOption = 0; currentCharacterOption < characterOptionAmount; currentCharacterOption++)
             characterToItsLastOccurrenceSpotInPatStr[currentCharacterOption] = -1;
-
+        // 更新元素值
         for (int currentCharacterSpot = 0; currentCharacterSpot < patternCharacterArr.length; currentCharacterSpot++)
             characterToItsLastOccurrenceSpotInPatStr[patternCharacterArr[currentCharacterSpot]] = currentCharacterSpot;
     }
 
-    // 返回 文本字符串中，模式字符串第一次出现的位置。
-    // 如果没有发生匹配的话，则 返回 文本字符串的长度n
-    public int  search(String txtStr) {
+    // 返回 在文本字符串中，模式字符串 第一次出现的位置。
+    // 如果 没有任何匹配 的话，则 返回 文本字符串的长度n
+    public int search(String txtStr) {
         int patStrLength = patternStr.length();
         int txtStrLength = txtStr.length();
 
         int txtCursorNeedJumpDistance;
-        // #1 使用文本指针 来 在“可能发生匹配的位置”上开始 尝试匹配
-        // 特征：① “文本指针”并不是直接指向“待比较的文本字符”；② 在匹配失败后，文本指针会向后跳动 txtCursorJumpDistance个位置； ③ 跳到新的位置后，会重复同样的过程-对字符进行比较
+        // #1 使用 “文本指针” 来 表示“可能发生匹配的位置”，并 开始尝试匹配
+        // 特征：① “文本指针”并不是直接指向“待比较的文本字符”，而是指向 当前开始尝试匹配的位置；
+        // ② 在匹配失败后，文本指针会 向后跳动 txtCursorJumpDistance个位置；
+        // ③ 跳到 新的位置 后，会重复 同样的过程（对字符进行比较）
         for (int currentTxtCursor = 0; currentTxtCursor <= txtStrLength - patStrLength; currentTxtCursor += txtCursorNeedJumpDistance) { // #2-Ⅰ 更新“当前可能发生字符串匹配的位置”
-            // #2 对于“当前可能发生字符串匹配的位置”（current_txt_cursor）, 执行“逐个字符匹配/比较”的操作
 
-            txtCursorNeedJumpDistance = 0; // 每个 可能的匹配位置，都要重新计算它在失配时的跳转距离
-            for (int backwardsPatCursor = patStrLength - 1; backwardsPatCursor >= 0; backwardsPatCursor--) { // 模式指针从后往前
-                int txtCharacterSpot = currentTxtCursor + backwardsPatCursor; // 文本字符也是从后往前
+            // #2 对于“当前可能发生字符串匹配的位置”（current_txt_cursor）, 执行“逐个字符匹配/比较”的操作
+            txtCursorNeedJumpDistance = 0; // 用于计算 在“当前可能发生匹配的位置” 发生失配时， 文本指针 所需要的跳转距离
+            for (int backwardsPatCursor = patStrLength - 1; backwardsPatCursor >= 0; backwardsPatCursor--) { // “模式指针”从后往前
+                // 计算出 “文本字符”的位置
+                int txtCharacterSpot = currentTxtCursor + backwardsPatCursor;
                 char txtCharacter = txtStr.charAt(txtCharacterSpot);
                 char patCharacter = patternStr.charAt(backwardsPatCursor);
 
-                // #2-Ⅰ 如果文本字符与模式字符之间不匹配,则：
-                if (txtCharacter != patCharacter) {
+                // #2-Ⅰ 如果 文本字符 与 模式字符 之间不相同,说明 在“当前可能发生匹配的位置”上 发生了失配，则：
+                if (characterMismatch(txtCharacter, patCharacter)) {
                     // 计算出 文本指针“应该向后跳转的距离” - 用于更新“当前可能发生字符串匹配的位置”
                     txtCursorNeedJumpDistance = calculateTxtCursorJumpDistance(txtCharacter, backwardsPatCursor);
                     break;
@@ -136,7 +145,7 @@ public class BoyerMoore {
 
             // #2-Ⅱ 如果 “当前可能发生字符串匹配的位置(current_txt_cursor)” 发生了匹配，则：返回该位置(current_txt_cursor)
             // 手段：如果 txtCursorJumpDistance局部变量 保持原始值（等于0），说明 模式字符串中的每一个字符都匹配成功。则 “发生了匹配”
-            if (txtCursorNeedJumpDistance == 0) return currentTxtCursor;    // found
+            if (allCharacterMatches(txtCursorNeedJumpDistance)) return currentTxtCursor;    // found
         }
 
         // #3 如果程序执行到这里，说明 所有“可能发生字符串匹配的位置”都检查完成后，仍旧没有找到成功的匹配。
@@ -144,48 +153,74 @@ public class BoyerMoore {
         return txtStrLength;                       // not found
     }
 
+    private boolean allCharacterMatches(int txtCursorNeedJumpDistance) {
+        return txtCursorNeedJumpDistance == 0;
+    }
+
+    /**
+     * 计算出 当字符发生失配时，文本指针需要 往后跳的距离
+     *
+     * @param mismatchedTxtCharacter 失配的文本字符
+     * @param backwardsPatCursor     失配时 模式指针的位置
+     * @return 文本指针需要 往后跳的距离
+     */
     private int calculateTxtCursorJumpDistance(char mismatchedTxtCharacter, int backwardsPatCursor) {
         // #1 获取到 此文本字符 在模式字符串中最后一次出现的位置
         int txtCharactersLastOccurrenceInPatStr = characterToItsLastOccurrenceSpotInPatStr[mismatchedTxtCharacter];
         // #2 计算出 模式字符串中，“匹配失败的位置”与“会匹配成功的位置”之间的距离
         int distanceBetweenMismatchSpotAndWouldMatchSpot = backwardsPatCursor - txtCharactersLastOccurrenceInPatStr;
-        // #3 这个距离 就是“文本指针应该向后跳转的距离” 🐖 为了防止xxx出现负数，这里使用max()来保证文本指针最少往后移动一个位置
-        return Math.max(1, distanceBetweenMismatchSpotAndWouldMatchSpot);
+        // #3 这个距离 就是“文本指针应该向后跳转的距离”
+        return Math.max(1, // 🐖 为了防止 计算结果 出现负数，这里 使用max() 来 保证文本指针 最少往后移动一个位置
+                distanceBetweenMismatchSpotAndWouldMatchSpot);
     }
 
 
     /**
-     * Returns the index of the first occurrence of the pattern string
-     * in the text string.
+     * 返回 模式字符串 在文本字符串中 第一次出现的索引位置
      *
-     * @param text the text string
-     * @return the index of the first occurrence of the pattern string
-     * in the text string; n if no such match
+     * @param textCharArr 文本字符串
+     * @return 第一次出现的索引位置（如果有匹配存在），如果没有匹配，则：返回 文本字符串的长度N
      */
-    public int search(char[] text) {
-        int m = patternCharacterArr.length;
-        int n = text.length;
-        int skip;
-        for (int i = 0; i <= n - m; i += skip) {
-            skip = 0;
-            for (int j = m - 1; j >= 0; j--) {
-                if (patternCharacterArr[j] != text[i + j]) {
-                    skip = Math.max(1, j - characterToItsLastOccurrenceSpotInPatStr[text[i + j]]);
+    public int search(char[] textCharArr) {
+        int patternLength = patternCharacterArr.length;
+        int textLength = textCharArr.length;
+        int needJumpDistance;
+        for (int currentAttemptingSpot = 0; currentAttemptingSpot <= textLength - patternLength; currentAttemptingSpot += needJumpDistance) {
+
+            needJumpDistance = 0;
+            for (int backwardsPatCursor = patternLength - 1; backwardsPatCursor >= 0; backwardsPatCursor--) {
+                char currentPatCharacter = patternCharacterArr[backwardsPatCursor];
+                char currentTextCharacter = textCharArr[currentAttemptingSpot + backwardsPatCursor];
+
+                if (characterMismatch(currentPatCharacter, currentTextCharacter)) {
+                    int textCharRightmostSpot = characterToItsLastOccurrenceSpotInPatStr[currentTextCharacter];
+                    needJumpDistance =
+                            Math.max(1, // 保证 至少向后移动一个位置
+                                    backwardsPatCursor - textCharRightmostSpot);
                     break;
                 }
             }
-            if (skip == 0) return i;    // found
+
+            if (allCharacterMatches(needJumpDistance)) {
+                return currentAttemptingSpot;    // 在 当前尝试匹配的位置上 得到了一个匹配
+            }
         }
-        return n;                       // not found
+
+        return textLength;                       // not found
+    }
+
+    // 字符 不匹配 的情况
+    private boolean characterMismatch(char currentPatCharacter, char currentTextCharacter) {
+        return currentPatCharacter != currentTextCharacter;
     }
 
 
     /**
-     * Takes a pattern string and an input string as command-line arguments;
-     * searches for the pattern string in the text string; and prints
-     * the first occurrence of the pattern string in the text string.
+     * 接受一个模式字符串 和 一个输入字符串 作为 命令行参数；
+     * 在 文本字符串中 查找 模式字符串；
+     * 打印 模式字符串 在 文本字符串中 第一次出现的位置；
      *
-     * @param args the command-line arguments
+     * @param args 命令行参数
      */
     public static void main(String[] args) {
         String patStr = args[0];
@@ -206,12 +241,12 @@ public class BoyerMoore {
 
         StdOut.print("patternArr: ");
         // 在打印模式字符串之前，先打印 offset个空格 来 实现模式字符串 与 其在文本字符串中的匹配 打印对齐的效果
-        for (int i = 0; i < offset1; i++)
+        for (int currentSpot = 0; currentSpot < offset1; currentSpot++)
             StdOut.print(" ");
         StdOut.println(patStr);
 
         StdOut.print("patternArr: ");
-        for (int i = 0; i < offset2; i++)
+        for (int currentSpot = 0; currentSpot < offset2; currentSpot++)
             StdOut.print(" ");
         StdOut.println(patStr);
     }
