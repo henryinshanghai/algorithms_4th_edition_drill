50386b3ca17d453fbd20ae67a18bc294a8efcca1
HenryInSH <2291972433@qq.com>
2025-10-01 09:10:59 +0800

    --- Rabin-Karp算法（用于找到文本字符串中的模式字符串）：① 计算 模式字符串的hash值 作为其指纹；② 对于文本字符串的当前子串，先 根据前一个子串的hash值 快速计算出其hash值；③ 如果hash值相等，再逐个判断字符是否相等；④ 如果hash值不相等，说明不匹配，则：跳过该位置。⑤ 文本指针 与 待比较文本指针 是不同的指针； ---

--- src/main/java/com/henry/string_05/search_substring_03/Rabin_Karp_03/execution/RabinKarp.java
+++ src/main/java/com/henry/string_05/search_substring_03/Rabin_Karp_03/execution/RabinKarp.java
@@ -50,10 +50,9 @@ public class RabinKarp {
     private long weightOfFirstDigit;         // R^(M-1) % Q ??? 第一个数字的权重??
 
     /**
-     * Preprocesses the pattern string.
-     * 预处理模式字符串
-     * @param pattern                the pattern string 模式字符串
-     * @param characterOptionsAmount the alphabet size 字母表的大小
+     * 预处理 模式字符串
+     * @param pattern                模式字符串
+     * @param characterOptionsAmount 字母表的大小
      */
     public RabinKarp(char[] pattern, int characterOptionsAmount) {
         this.patternStr = String.valueOf(pattern);
@@ -62,18 +61,17 @@ public class RabinKarp {
     }
 
     /**
-     * Preprocesses the pattern string.
-     * 预处理模式字符串
-     * @param patternStr the pattern string
+     * 预处理 模式字符串
+     * @param patternStr 模式字符串
      */
     public RabinKarp(String patternStr) {
         this.patternStr = patternStr;      // 用于保存模式字符串（仅仅对于 Las Vegas适用）
-        characterOptionsAmount = 256; // aka baseSize
+        characterOptionsAmount = 256; // 字母表的大小
         patStrLength = patternStr.length();
         largePrime = longRandomPrime();
 
-        // precompute R^(m-1) % q for use in removing leading digit
-        // #1 预计算出 R^(m-1) % q [首字符(leading digit)对应的权重] - 用于移除首位数字
+        // #1 预计算出 首字符(leading digit)对应的权重：R^(m-1) % Q - 用于 在 计算下一个子字符串的hash值 时，移除首位数字
+        // 例子：256
         weightOfFirstDigit = 1;
         for (int currentRound = 1; currentRound <= patStrLength - 1; currentRound++)
             weightOfFirstDigit = (weightOfFirstDigit * characterOptionsAmount) % largePrime;
@@ -82,8 +80,8 @@ public class RabinKarp {
         patternStrHash = hashOfSubStrTill(patternStr, patStrLength);
     }
 
-    // Compute hash for key[0..m-1].
-    // 计算 key[0..m-1]截取得到的子字符串 的hash值
+    // 计算 key[0..m-1]区间 所截取得到的 子字符串的hash值
+    // 手段：把 字符串 视为 一个R进制的M位数的数字，计算 该数字的数值 即可
     private long hashOfSubStrTill(String passedStr, int rightBoundary) {
         long currentHashValue = 0;
 
@@ -93,17 +91,17 @@ public class RabinKarp {
             currentHashValue = (currentHashValue * characterOptionsAmount + currentCharacter * 1) % largePrime;
         }
 
-        // 返回计算出的hash值
+        // 返回 计算出的hash值
         return currentHashValue;
     }
 
-    // Las Vegas version: does pat[] match txt[i..i-m+1] ?
-    private boolean compareCharactersThenDetermineMatchOrNot(String txtStr, int startingPointToCompare) {
+    // 拉斯维加斯的版本：模式字符串 是否匹配 文本字符串的[i..i-m+1]的子字符串?
+    private boolean characterMatchOnEachSpot(String txtStr, int startingPointToCompare) {
         // 匹配的定义：对于特定的偏移量offset，文本字符串T[offset, offset + patLength]的子字符串 与 patternStr字符串 完全相同
         for (int currentSpot = 0; currentSpot < patStrLength; currentSpot++) {
             // #1 获取 当前位置的模式字符
             char currentPatternCharacter = patternStr.charAt(currentSpot);
-            // #2 获取 对应位置(起点位置 + 当前比较位置)上的文本字符
+            // #2 获取 对应位置(文本指针起点位置 + 当前比较位置)上的文本字符
             char currentTxtCharacter = txtStr.charAt(startingPointToCompare + currentSpot);
 
             if (currentPatternCharacter != currentTxtCharacter)
@@ -117,54 +115,55 @@ public class RabinKarp {
     //    return true;
     //}
 
-    // 返回 模式字符串在文本字符串中第一次出现的位置
-    // 如果文本字符串中不存在这样的match，则返回 n
+    // 返回 模式字符串 在文本字符串中 第一次出现的位置
+    // 如果 文本字符串中 不存在 这样的match，则 返回 n
     public int search(String txtStr) {
         int txtStrLength = txtStr.length();
         if (txtStrLength < patStrLength) return txtStrLength;
 
-        // #1-Ⅰ 从文本字符串中截取 [0..patStrLength]的子字符串clip，并计算出 其hash值
+        // #1-Ⅰ 从 文本字符串 中 截取出 [0..patStrLength]的子字符串clip，并 计算出 其hash值
         long txtSubStrHash = hashOfSubStrTill(txtStr, patStrLength);
 
         // #1-Ⅱ 检查 位置0是不是一个 能够发生匹配的位置(matched_spot) 如果匹配，则：返回位置0
-        if (findAMatchInSpot0(txtStr, txtSubStrHash))
+        if (findAMatchOnSpot0(txtStr, txtSubStrHash))
             return 0;
 
-        // #2 对于 文本字符串中从patStrLength开始的每一个位置（可能发生匹配的位置）...
-        for (int currentTxtCursor = patStrLength; currentTxtCursor < txtStrLength; currentTxtCursor++) {
+        // #2 对于 文本字符串中 从patStrLength开始的 每一个位置（aka 所有 可能发生匹配的 位置）...
+        // 🐖 奇奇怪怪的实现：突然使用 clipEndSpot 来 截取 文本子字符串了
+        for (int currentClipEndSpot = patStrLength; currentClipEndSpot < txtStrLength; currentClipEndSpot++) {
             // #2-Ⅰ 为 以“当前文本指针所指向的位置”为结束位置的 子字符串，计算其hash值
-            txtSubStrHash = calculateHashForNextSubStr(txtSubStrHash, txtStr, currentTxtCursor);
+            txtSubStrHash = calculateHashForNextSubStr(currentClipEndSpot, txtStr, txtSubStrHash);
 
             // #2-Ⅱ 判断 “当前文本指针”所对应的“当前尝试匹配位置” 是否 产生了一个匹配
             // 计算出 当前尝试匹配位置
-            int currentTriedMatchSpot = (currentTxtCursor - patStrLength) + 1;
+            int currentTriedMatchSpot = (currentClipEndSpot - patStrLength) + 1;
             // 如果 发生了匹配，则：返回 当前尝试匹配的位置
             if (findAMatchOn(txtStr, txtSubStrHash, currentTriedMatchSpot))
                 return currentTriedMatchSpot;
         }
 
-        // #3 如果代码执行到此处，说明 所有“可能发生匹配的位置”，都没有发生匹配。则：
-        // 返回文本字符串的长度 来 表示匹配失败
+        // #3 如果代码 执行到了 此处，说明 所有“可能发生匹配的位置”都 没有发生匹配。则：
+        // 返回 文本字符串的长度 来 表示匹配失败
         return txtStrLength;
     }
 
-    // 移除leading数码，添加trailing数码，从而 得到新的文本子字符串的hash值
-    // 示例：从123456中截取长度为5的clip   初级计算公式：(12345 - 1 * 10^4) * 10 + 6 = 23456
-    // 经验：有些上下文信息（参数的具体上下文）应该由真实的入参提供，这样方法的形参中就可以不包含这些信息，由此来简化方法中的变量名
-    // implement_SOP_05:（🐖 在txtHash的计算中，额外加上了一个Q 来 保证所有的数都为正，这样 取余操作 才能够得到预期的结果）
-    private long calculateHashForNextSubStr(long currentSubStrHash, String txtStr, int clipEndSpot) {
+    // 作用：移除 leading数码，添加 trailing数码，从而 得到 新的文本子字符串的hash值
+    // 示例：从123456中 截取 长度为5的clip   初级计算公式：(12345 - 1 * 10^4) * 10 + 6 = 23456
+    // 经验：有些上下文信息（参数的具体上下文）应该 由真实的入参提供，这样方法的形参中 就可以 不包含这些信息，由此 来 简化方法中的变量名
+    // implement_SOP_05:（🐖 在 txtHash的计算 中，额外加上了一个Q 来 保证所有的数 都为正，这样 取余操作 才能够得到 预期的结果）
+    private long calculateHashForNextSubStr(int clipEndSpot, String txtStr, long currentSubStrHash) {
         // #0 计算出 当前首字符的真实值
         int clipStartSpot = clipEndSpot - patStrLength;
         char startCharacter = txtStr.charAt(clipStartSpot);
         long startCharactersTrueValue = (startCharacter * weightOfFirstDigit) % largePrime; // 10000
 
-        // #1 计算出 “移除首字符的真实值”之后的剩余差值；
+        // #1 计算出 “移除首字符的真实值”之后的 剩余差值；
         // 手段：计算公式 = 当前哈希结果值 - 首位数字的真实值
         // 🐖 这里 +大素数、%大素数的操作 是为了不溢出吗???
         currentSubStrHash = (currentSubStrHash + largePrime - startCharactersTrueValue) % largePrime; // 2345
 
-        // #2 基于#1中的差值，计算出新的字符串的哈希结果；
-        // 手段：计算公式 = 差值 * 进制基数/baseSize + 下一个字符的数值
+        // #2 基于 #1中的差值，计算出 新的字符串的哈希结果；
+        // 手段：计算公式 = 差值 * 进制基数R/baseSize + 下一个字符的数值 * 1
         // 🐖 这里%大素数的操作 是为了哈希结果能够落在散列表中??
         char endCharacter = txtStr.charAt(clipEndSpot);
         currentSubStrHash = (currentSubStrHash * characterOptionsAmount + endCharacter * 1) % largePrime; // 2345*10 + 6*1
@@ -175,18 +174,18 @@ public class RabinKarp {
     private boolean findAMatchOn(String txtStr, long subTxtHash, int triedMatchSpot) {
         // #1 当前clip的hash值 与 模式字符串的hash值是否相等
         // #2 逐个字符比较 判断是否匹配
-        return (patternStrHash == subTxtHash) && compareCharactersThenDetermineMatchOrNot(txtStr, triedMatchSpot);
+        return equalsToPatStrHash (subTxtHash) && characterMatchOnEachSpot(txtStr, triedMatchSpot);
     }
 
-    private boolean findAMatchInSpot0(String passedTxtStr, long txtSubStrHash) {
-        // #1 文本字符串的子字符串的hash值 是不是 与模式字符串的hash值 相同；并且
+    private boolean findAMatchOnSpot0(String passedTxtStr, long txtSubStrHash) {
+        // #1 文本字符串的 子字符串的hash值 是不是 与 模式字符串的hash值 相同；并且
         // #2 检查 子字符串 与 模式字符串 是不是 在每个字符都是匹配的
         return equalsToPatStrHash(txtSubStrHash) && everyCharacterMatchWithPatStr(passedTxtStr);
     }
 
     private boolean everyCharacterMatchWithPatStr(String passedTxtStr) {
         int offsetFromSpot0InTxtStr = 0;
-        return compareCharactersThenDetermineMatchOrNot(passedTxtStr, offsetFromSpot0InTxtStr);
+        return characterMatchOnEachSpot(passedTxtStr, offsetFromSpot0InTxtStr);
     }
 
     private boolean equalsToPatStrHash(long txtHash) {
@@ -201,11 +200,10 @@ public class RabinKarp {
     }
 
     /**
-     * Takes a pattern string and an input string as command-line arguments;
-     * searches for the pattern string in the text string; and prints
-     * the first occurrence of the pattern string in the text string.
-     *
-     * @param args the command-line arguments
+     * 接受一个 模式字符串 以及 一个输入字符串 作为 命令行参数；
+     * 在 文本字符串 中 搜索模式字符串；
+     * 打印出 模式字符串 在文本字符串中 第一次出现的位置
+     * @param args 命令行参数
      */
     public static void main(String[] args) {
         String patternStr = args[0];
@@ -213,13 +211,13 @@ public class RabinKarp {
 
         // #1 预计算出 pat_str的hash值 与 clip首字符的权重
         RabinKarp searcher = new RabinKarp(patternStr);
-        // 手段：search()方法接收文本字符串 来 得到匹配所需的偏移量
+        // #2 获取到 发生匹配的位置
+        // 手段：search()方法 接收 文本字符串 来 得到 发生匹配的位置 (aka 匹配所需的偏移量)
         int offset = searcher.search(txtStr);
 
-        // print results
+        /* #3 打印匹配结果 */
         StdOut.println("text:    " + txtStr);
 
-        // from brute force search method 1
         StdOut.print("pattern: ");
         for (int currentSpot = 0; currentSpot < offset; currentSpot++)
             StdOut.print(" ");
