a2a618aff5bb2576a3cc201acbb45fd3b093ab1e
HenryInSH <2291972433@qq.com>
2025-10-01 09:24:53 +0800

    --- 正则表达式（判断一个指定的正则表达式 能否识别 指定的文本字符串）：① 根据 正则表达式中的各种操作(选择操作、闭包操作、括号)的规则 来 得到正则表达式字符串的NFA；② 使用文本字符串 驱动 该NFA，查看能够到达接收状态；③ NFA中，经由 ε转换(不消耗文本字符) 所能到达的状态集合 εTransferReachedStates；④ NFA中，经由 匹配转换(消耗文本字符) 所能到达的状态集合 matchTransferReachedStates； ⑤ 查看 接受状态 是否存在于 最终的 εTransferReachedStates中。 ---

--- src/main/java/com/henry/string_05/regex_04/execution/NFA.java
+++ src/main/java/com/henry/string_05/regex_04/execution/NFA.java
@@ -59,53 +59,55 @@ import edu.princeton.cs.algs4.StdOut;
  * @author Robert Sedgewick
  * @author Kevin Wayne
  */
-// 验证：可以使用 正则表达式字符串的NFA 来 判断指定的文本字符串中是否存在有 与其相匹配的子字符串
+// 验证：可以使用 正则表达式字符串的NFA 来 判断 指定的文本字符串中 是否存在有 与其相匹配的子字符串
 public class NFA {
 
-    private Digraph epsilonTransitionDigraph;     // digraph of epsilon transitions
-    private String regexStr;     // regular expression
-    private final int characterAmountInRegexStr;       // 正则表达式的字符长度 用作 正则表达式的NFA中，最后预留的”接受状态“
+    private Digraph epsilonTransitionDigraph;     // epsilon转换的有向图
+    private String regexStr;     // 正则表达式
+    private final int characterAmountInRegexStr;       // 正则表达式的字符数量 用作 正则表达式的NFA中，最后预留的”接受状态“
 
     /**
-     * 根据给定的正则表达式字符串（模式字符串） 来 构造其所对应的NFA的∈-转换有向图
-     * 关键：对需要添加ε转换的场景 进行分类条论；
-     * ε转换的分类：#1 由当前状态转换为下一个状态； #2 用于支持闭包操作/重复操作； #3 用于支持选择/或操作
+     * 根据 给定的正则表达式字符串（模式字符串） 来 构造 其所对应的NFA的 ∈-转换有向图
+     * 关键：对 需要添加ε转换的场景 进行 分类讨论；
+     * ε转换的分类：#1 由 当前状态 转换为 下一个状态； #2 用于支持 闭包操作/重复操作； #3 用于支持 选择/或操作
      * <p>
-     * 🐖 正则表达式的NFA中，结点中元素是“模式字符”，结点的状态是“模式字符在正则表达式字符串中的位置”
-     * 特征：某一状态的结点，可能会向多个其他状态发生转移。
+     * 🐖 正则表达式的NFA中，结点中的元素 是 “模式字符”，结点的状态 是 “模式字符 在正则表达式字符串中的 位置”
+     * 特征：某一状态的结点，可能会 向 多个其他状态 发生转移。
      * <p>
-     * 状态之间发生转移的原因是：当前模式字符的性质 - 对于不同类型的模式字符，它会有自己的状态转换规则👇
-     * #1 如果模式字符是一个 字母字符，则：它会通过“匹配转换” 来 转换到下一个状态/字符；    特征：匹配转换会消耗掉一个 文本字符串中的字符
-     * #2 如果模式字符是一个 “非字母字符”，则：它会通过“ε转换” 来 转换到下一个状态/字符；  特征：ε转换 不会消耗 文本字符串中的字符，也就是说 模式字符与文本字符没有匹配时，仍旧会进行状态转移
+     * 状态之间 发生转移的 原因是：当前模式字符的性质 - 对于 不同类型的模式字符，它会有 自己的 状态转换规则👇
+     * #1 如果 模式字符 是一个 字母字符，则：它 会通过 “匹配转换” 来 转换到 下一个状态/字符；    特征：匹配转换 会消耗掉一个 文本字符串中的字符
+     * #2 如果 模式字符 是一个 “非字母字符”，则：它会通过 “ε转换” 来 转换到 下一个状态/字符；  特征：ε转换 不会消耗 文本字符串中的字符，也就是说 模式字符 与 文本字符 没有匹配时，仍旧会进行 状态转移
      */
     public NFA(String regexStr) {
-        // 准备一个栈对象 用于记录下 open_character(用作ε转换的起点)的位置
+        System.out.println("== 当前模式字符串为：" + regexStr + " ==");
+        // 准备一个栈对象 用于记录下 open_character(用作 ε转换的起点)的位置
         Stack<Integer> openCharactersSpotStack = new Stack<Integer>();
         this.regexStr = regexStr;
         characterAmountInRegexStr = regexStr.length(); // stateAmountInRegStr
-        // 准备一个有向图 用于描述 正则表达式的NFA中的ε转换
+        // 准备一个有向图 用于描述 正则表达式的NFA中的 ε转换
         epsilonTransitionDigraph = new Digraph(characterAmountInRegexStr + 1);
 
         // 对于模式字符串中的 从0开始的当前位置/状态...
         for (int currentRegexCharacterSpot = 0; currentRegexCharacterSpot < characterAmountInRegexStr; currentRegexCharacterSpot++) {
-            // 声明 ”左括号位置“变量，用于： #1 记录“当前左括号字符”的位置/状态； 或者 #2 模式字符的当前位置/状态[初始值]
-            int leftParenthesisSpotCursor = currentRegexCharacterSpot;
+            // 声明 ”多用途“变量，用于： ① 记录“当前左括号字符”的位置/状态； 或者 ② 模式字符的当前位置/状态[初始值]
+            int leftParenthesisSpot = currentRegexCharacterSpot;
 
-            /* 对“当前位置上字符/模式字符”进行分类讨论👇 */
-            // Ⅰ 如果当前位置上的字符是 选择操作相关的字符 ( | ) ① “启动字符”（左括号字符、或字符）；② “结束字符”（右括号字符） 的话,
-            // 则：在遇到”结束字符“时，向NFA中添加 所需的ε转换
-            // 副作用：把 左括号(字符的位置 绑定到 此变量
-            leftParenthesisSpotCursor = whenRegexCharacterRelatedToChoose(regexStr, currentRegexCharacterSpot, openCharactersSpotStack, leftParenthesisSpotCursor);
+            /* 对 当前位置上字符/模式字符 进行分类讨论👇 */
+            // Ⅰ 如果 当前位置上的字符 是 与 选择操作的场景 相关，则：在 需要 时，向NFA中 添加 选择操作 所需的ε转换
+            // 返回值：左括号指针的位置
+            leftParenthesisSpot = isChooseOperationScenarios(regexStr,
+                    currentRegexCharacterSpot,
+                    openCharactersSpotStack,
+                    leftParenthesisSpot);
 
-            // Ⅱ 如果“当前位置上的字符”的后面 紧跟着“闭包操作符”, 则：向NFA中添加对应的ε转换 来 支持闭包/重复操作
-            // 用法：#1 X* #2 (X)*
-            // 🐖 这里只需要使用 leftParenthesisSpotCursor这个变量就能表示两种情况 - 因为这个变量的值 要么是 X, 要么是 (
-            if (isLegitState(currentRegexCharacterSpot) && nextRegexCharacterIsAsterisk(regexStr, currentRegexCharacterSpot)) {
-                supportClosureOperation(leftParenthesisSpotCursor, currentRegexCharacterSpot);
+            // Ⅱ 如果 当前位置上的字符 与 闭包操作的场景 相关, 则：向NFA中添加 闭包操作 所需要的 ε转换
+            // 🐖 这里 只需要使用 左括号指针 就能表示 两种情况 - 因为 左括号指针的值 要么是 X, 要么是 (
+            if (isClosureOperationScenarios(regexStr, currentRegexCharacterSpot)) {
+                supportClosureOperation(leftParenthesisSpot, currentRegexCharacterSpot);
             }
 
-            // Ⅲ 如果“当前位置上的字符”是 不是|的非字母字符，说明它无法产生一个匹配转换，则：
-            // 需要为它添加一个ε转换
+            // Ⅲ 如果 “当前位置上的字符”是一个 非字母字符，说明 它无法产生 一个 ”匹配转换“，则：
+            /* 需要 为它添加 一个ε转换 */
             // 手段：向NFA中添加 转换到下一个状态/位置的ε转换 来 使NFA继续下去
             if (isNonLetterCharacter(regexStr, currentRegexCharacterSpot))
                 advanceNFAFrom(currentRegexCharacterSpot);
@@ -115,55 +117,80 @@ public class NFA {
             throw new IllegalArgumentException("Invalid regular expression");
     }
 
-    private int whenRegexCharacterRelatedToChoose(String regexStr, int characterSpot, Stack<Integer> departCharactersSpotStack, int leftParenthesisSpotCursor) {
-        if (isDepartCharacterOn(regexStr, characterSpot)) // 如果当前位置上的模式字符 是 启动字符（左括号字符、或字符），则：
-            // 把 当前模式字符的位置 作为启动字符的位置 记录到 一个栈结构中
+
+    private boolean isClosureOperationScenarios(String regexStr, int currentRegexCharacterSpot) {
+        // 手段：查看 “当前位置上的字符”的后面 是不是 紧跟着 “闭包操作符”
+        return isLegitState(currentRegexCharacterSpot)
+                && nextRegexCharacterIsAsterisk(regexStr, currentRegexCharacterSpot);
+    }
+
+    /**
+     * 作用：
+     * ① 如果 当前模式字符 属于选择操作场景中的字符，则 在正确的时机 为NFA添加 其所需要的ε转换
+     * ② 返回 左括号指针
+     *
+     * @param regexStr                  模式字符串
+     * @param characterSpot             当前字符的位置
+     * @param departCharactersSpotStack 出发字符位置的栈集合
+     * @param leftParenthesisSpot       用于指示 左括号位置 的变量
+     * @return
+     */
+    private int isChooseOperationScenarios(String regexStr,
+                                           int characterSpot,
+                                           Stack<Integer> departCharactersSpotStack,
+                                           int leftParenthesisSpot) {
+        // 如果 当前位置上的模式字符 是 出发字符（左括号字符、或字符），则：
+        if (isDepartCharacterOn(regexStr, characterSpot))
+            // 把 该位置 记录到 一个栈结构中
             departCharactersSpotStack.push(characterSpot);
-        else if (isTerminalCharacterOn(regexStr, characterSpot)) { // 如果当前位置上的模式字符 是 “结束字符”（右括号字符）,则：
-            // 把 当前模式字符的位置 作为 |操作符的位置
+        else if (isTerminalCharacterOn(regexStr, characterSpot)) { // 如果 当前位置上的模式字符 是 “终点字符)”,则：
+            // 把 该位置 作为 rightParenthesisSpot变量的值 - 用于支持 选择操作所需的ε转换
             int rightParenthesisSpot = characterSpot;
 
-            // 弹出以获取 栈顶当前所记录的“启动字符的位置”
+            // 弹出以获取 栈顶当前所记录的 “启动字符的位置”
             int departCharacterSpot = departCharactersSpotStack.pop();
-            // 并进一步获取到”启动字符“ - 可能是 左括号字符，也可能是 或字符
+            // 并进一步获取到 该位置上的”启动字符“ - 可能是 (字符，也可能是 |字符
             char departCharacter = regexStr.charAt(departCharacterSpot);
 
-            /* 对“此启动字符”进行分类讨论👇 */
-            if (departCharacter == '|') { // 如果“此启动字符”是“选择操作符”，说明需要向NFA中添加分支来支持选择的操作
-                // 则：向NFA中添加对应的ε转换 来 支持选择操作
+            /* 对 “此启动字符” 进行分类讨论👇 */
+            if (departCharacter == '|') { // 如果 “此出发字符” 是 “|操作符”，说明 当前右括号 是一个 选择操作中的右括号，则...
+                /* 向NFA中 添加 对应的ε转换 来 支持选择操作 */
 
-                // 把第一次弹出的”启动字符的位置“ 作为 |操作符的位置
+                // 把 第一次弹出的”启动字符的位置“ 作为 |操作符的位置
                 int orCharacterSpot = departCharacterSpot;
-                // 再次弹出栈元素 来 获取 栈顶当前所记录的“启动字符的位置” - 断言：按照合法的正则表达式的规则约束，这会是左括号(字符；
-                leftParenthesisSpotCursor = departCharactersSpotStack.pop();
+                // 再次弹出栈元素 来 作为 (操作符的位置 - 断言：按照 合法的正则表达式 的规则约束，这会是 左括号(字符；
+                leftParenthesisSpot = departCharactersSpotStack.pop();
 
-                // 为”选择操作符“ 添加 ε转换 - (到B的首字符的ε转换 以及 |到右括号的ε转换
-                supportChooseOperation(leftParenthesisSpotCursor, orCharacterSpot, rightParenthesisSpot);
-            } else if (departCharacter == '(') { // 如果“此启动字符”是“左括号(字符”，则
-                // 把它的位置 作为 leftParenthesisSpot变量的值 - 用于支持 重复操作符*的ε转换
-                leftParenthesisSpotCursor = departCharacterSpot;
-            } else
+                // 为 ”选择操作符“ 添加 其对应的ε转换 - ① ( 到 B的首字符的 ε转换 以及 ② | 到 右括号的 ε转换
+                supportChooseOperation(leftParenthesisSpot, orCharacterSpot, rightParenthesisSpot);
+            } else if (departCharacter == '(') { // 如果 “此出发字符” 是 (字符，说明 当前右括号 是一个 与左括号相对应的右括号，则：
+                /* 记录 此(字符 的位置 - 作用: 用于 为 闭包操作符 添加 需要的ε转换 */
+                leftParenthesisSpot = departCharacterSpot;
+            } else // 出发字符 只允许是(或者|，如果 不是两者之一，说明 代码出现了错误，直接 assert false 来 抛出错误，终止执行
                 assert false;
         }
 
-        // 这是本方法的一个副作用：为此变量 绑定了 左括号字符的位置
-        return leftParenthesisSpotCursor;
+        // 返回 左括号指针 所指向的位置
+        return leftParenthesisSpot;
     }
 
-    // 推进NFA到下一个状态
+    // 推进 NFA 到 下一个状态
     private void advanceNFAFrom(int currentSpot) {
+        System.out.println("~~ 为 NFA中的非文本字符 添加 从 当前位置" + currentSpot + " 到 下一个位置" + (currentSpot + 1) + "的 添加ε转换 ~~");
         // 向NFA中添加 “从当前状态 -> 当前状态的下一个状态”的epsilon转换👇
-        // 手段：在epsilon有向图中，添加边
+        // 手段：在 epsilon有向图 中，添加 边
         epsilonTransitionDigraph.addEdge(currentSpot, currentSpot + 1);
     }
 
-    // “闭包操作符” 在NFA中所能够产生的epsilon转换👇：
-    // #1 如果出现在单个字符之后，则：在此字符 与 *字符之间，添加两条相互指向的epsilon转换；
-    // #2 如果出现在 右括号字符之后，则：在当前左括号字符 与 此字符之间，添加两条相互指向的epsilon转换；
+    // “闭包操作符” 在NFA中 所能够产生的 epsilon转换👇：
+    // 闭包操作符的两种使用方式（X 作为 当前字符）：① X* ② (X)*
+    // ① 如果 闭包操作符* 出现在 单个字符 之后，则：在 该字符 与 *字符 之间，添加 两条相互指向的epsilon转换；
+    // ② 如果 闭包操作符* 出现在 右括号字符 之后，则：在 当前左括号字符 与 该字符 之间，添加 两条相互指向的epsilon转换；
     private void supportClosureOperation(int leftParenthesisSpotCursor, int currentRegexCharacterSpot) {
-        // 在NFA中添加 上述的一对epsilon转换（两种类型二选一）👇
-        // 手段：在epsilon有向图中，添加边
+        // 在NFA中 添加 上述的 一对epsilon转换（两种类型 二选一）👇
+        // 手段：在 epsilon有向图 中，添加边
         int asteriskSpot = currentRegexCharacterSpot + 1;
+        System.out.println("++ 为 闭包操作 添加ε转换（左括号位置：" + leftParenthesisSpotCursor + ", 星号位置：" + asteriskSpot + "） ++");
         epsilonTransitionDigraph.addEdge(leftParenthesisSpotCursor, asteriskSpot);
         epsilonTransitionDigraph.addEdge(asteriskSpot, leftParenthesisSpotCursor);
     }
@@ -172,6 +199,8 @@ public class NFA {
     // #1 从当前左括号字符->或字符的下一个字符 的epsilon转换；
     // #2 从或字符本身->当前右括号字符 的epsilon转换；
     private void supportChooseOperation(int leftParenthesisSpot, int orCharacterSpot, int rightParenthesisSpot) {
+        System.out.println("-- 为 选择操作 添加 ε转换（左括号位置：" + leftParenthesisSpot + ", 或操作符位置：" + orCharacterSpot + ", 右括号位置：" + rightParenthesisSpot + "） --");
+
         // 把位置作为状态，在NFA中添加 左括号字符->或字符的下一个字符 的epsilon转移👇
         int leftParenthesisState = leftParenthesisSpot;
         int firstCharacterStateInB = orCharacterSpot + 1;
@@ -207,42 +236,60 @@ public class NFA {
     }
 
     /**
-     * 如果文本被正则表达式匹配的话，则返回true，否则返回false
+     * 如果 文本字符串 被 正则表达式 所匹配的话，则：返回true，否则 返回false
      */
     public boolean recognizes(String txtStr) {
-        // #1 获取到 在正则表达式字符串的NFA中，由状态0作为起点，经ε转换所能到达的状态集合 ”ε转换所到达状态的集合“
-        // 这本质上是 有向图中的”单点可达性问题“
+        // #1 获取到 在 正则表达式字符串的NFA 中，由 状态0 作为起点，经ε转换 所能到达的状态集合 ”ε转换所到达状态的集合“
+        // 这 本质上是 有向图中的”单点可达性问题“
         Bag<Integer> εTransferReachedStates = getReachedStatesViaεTransferFromSpot0();
+        System.out.println("@@ NFA中，从位置0/状态0 经ε转换 可达的 所有状态为：{" + printItems(εTransferReachedStates) + "} @@");
 
-        // 对于当前文本字符的位置...
+        // 对于 当前文本字符的位置...
         for (int currentTxtCharacterSpot = 0; currentTxtCharacterSpot < txtStr.length(); currentTxtCharacterSpot++) {
-            // 获取此位置上的文本字符
+            // 获取 该位置上的文本字符
             char txtCurrentCharacter = txtStr.charAt(currentTxtCharacterSpot);
 
             dealWithBreachOf(txtCurrentCharacter);
 
-            // #2 获取到NFA中，由“当前可达的所有状态集合”中的各个状态，经过 与当前文本字符的匹配（匹配转换） 所能到达的状态集合
+            // #2 获取到 NFA中，由 “当前可达的所有状态集合”中的各个状态，经过 匹配转换（与当前文本字符的匹配） 所能到达的状态集合
             Bag<Integer> matchTransferReachedStates = getReachedStatesViaMatchTransferFrom(εTransferReachedStates, txtCurrentCharacter);
-            // 如果当前文本字符不存在匹配转换，说明 在“当前文本位置”无法得到一个匹配, 则：继续在“下一个位置"尝试匹配
+            System.out.println("%% NFA中，从 当前εTransferReachedStates {" + printItems(εTransferReachedStates) + "} 中的所有状态，经由 与第" + currentTxtCharacterSpot + "个位置上的当前文本字符" + txtCurrentCharacter + "的匹配转换 可达的 所有状态为：{" + printItems(matchTransferReachedStates) + "} %%");
+
+            // 如果 当前文本字符 不存在 匹配转换，说明 在“当前文本位置” 无法得到 一个匹配, 则：继续在“下一个位置"尝试匹配
             if (matchTransferReachedStates.isEmpty()) continue;
 
             // #3 更新 ”ε转换所到达状态的集合“
-            // 获取到NFA中，以”匹配后到达的状态集合“中的各个状态作为起点，经ε转换(不消耗文本字符)所能到达的状态集合；
-            // 本质上是有向图中的“多点可达性问题”
-            εTransferReachedStates = renewETransferReachedStatesVia(matchTransferReachedStates);
+            // 获取到 NFA中，以 ”匹配后到达的状态集合“中的 各个状态 作为起点，经 ε转换(不消耗文本字符) 所能到达的 状态集合；
+            // 🐖 本质上是 有向图中的“多点可达性问题”
+            εTransferReachedStates = reachedStatesViaεTransferFrom(matchTransferReachedStates);
+            System.out.println("^^ NFA中，从 当前matchTransferReachedStates {" + printItems(matchTransferReachedStates) + "}中的所有状态，经由 ε转换 可达的 所有状态为：{" + printItems(εTransferReachedStates) + "} ^^");
 
-            // 如果”可达状态“的集合为空，说明NFA的运行中断，不可能运行到“接受状态”, 则：可以提前return false
+            // 如果 ”经由ε转换可达的状态“的集合 为空，说明 NFA的运行中断，不可能 运行到“接受状态”, 则：可以提前return false
             if (εTransferReachedStates.size() == 0) return false;
         }
 
-        // #4 检查 ”可接受状态（stateAmountInRegStr）“ 是否被包含在 ”当前由epsilon转换所到达的状态集合“中
-        // 🐖 NFA中最后的接受状态，一定是经由ε转换得到的 因为它是一个额外添加的状态
-        if (acceptedStateIncludeIn(εTransferReachedStates)) return true;
+        System.out.println("&& for循环结束后，当前 由ε转换可达的 所有状态为：{" + printItems(εTransferReachedStates) + "} &&");
+
+        // #4 检查 ”可接受状态（stateAmountInRegStr）“ 是否被包含在 ”当前由epsilon转换所到达的状态集合“ 中
+        // 🐖 NFA中 最后的接受状态，一定是 经由ε转换得到的 因为它是一个 额外添加的状态
+        if (acceptedStateIncludeIn(εTransferReachedStates)) {
+            System.out.println("** 最终的可达状态集合{" + printItems(εTransferReachedStates) + "}中，包含有结束状态" + characterAmountInRegexStr + "。正则表达式" + regexStr + "成功识别了 文本字符串" + txtStr + "**");
+            return true;
+        }
 
         return false;
     }
 
-    // 🐖 文本字符不允许是 正则表达式的元字符
+    private String printItems(Bag<Integer> items) {
+        StringBuilder itemsSB = new StringBuilder();
+        for (Integer currentItem : items) {
+            itemsSB.append(currentItem + " ");
+        }
+
+        return itemsSB.toString().substring(0, itemsSB.length() - 1);
+    }
+
+    // 🐖 文本字符 不允许是 正则表达式的元字符
     private void dealWithBreachOf(char txtCurrentCharacter) {
         if (isRegexMetaCharacter(txtCurrentCharacter))
             throw new IllegalArgumentException("text contains the metacharacter '" + txtCurrentCharacter + "'");
@@ -257,20 +304,23 @@ public class NFA {
         return false;
     }
 
-    // 对于 NFA中“由匹配转换所到达的”状态集合（matchTransferReachedStates）中的每一个状态，获取其 “在NFA中通过epsilon转换所能够到达的状态”，并把得到的状态添加到”可达顶点“的集合中
-    private Bag<Integer> renewETransferReachedStatesVia(Bag<Integer> startStates) {
+    // 对于 NFA中“由匹配转换所到达的”状态集合（matchTransferReachedStates）中的每一个状态，
+    // ① 获取其 “在NFA中 通过epsilon转换 所能够到达的状态”，并 ② 把 得到的状态 添加到”可达顶点“的集合中
+    private Bag<Integer> reachedStatesViaεTransferFrom(Bag<Integer> startStates) {
         Bag<Integer> reachableStates = new Bag<Integer>();
 
-        // 获取到 在NFA对应的有向图中，以指定顶点集合作为”起点集合“，所能够到达的所有顶点
-        // aka 在NFA中通过epsilon转换 所能够到达的状态
-        // 手段: #1 先标记有向图中的结点；
+        // 获取到 在 NFA对应的有向图 中，以 指定顶点集合 作为”起点集合“，所能够到达的 所有顶点
+        // aka 在NFA中 通过epsilon转换 所能够到达的状态
+        // #1 先标记有向图中的结点；
         AccessibleVertexesInDigraph markedDigraph = new AccessibleVertexesInDigraph(epsilonTransitionDigraph, startStates);
 
+        // #2 再收集 所有“被标记的节点”
         for (int currentVertex = 0; currentVertex < epsilonTransitionDigraph.getVertexAmount(); currentVertex++)
-            // #2 如果当前顶点 由”matchTransferReachedStates“中的任意起点可达，则：
-            // 把它添加到 “可达顶点”的集合中
-            if (markedDigraph.isAccessibleFromStartVertex(currentVertex))
+            // 如果 当前顶点 由 ”matchTransferReachedStates“中的任意起点 可达，则：
+            if (markedDigraph.isAccessibleFromStartVertex(currentVertex)) {
+                // 把 它 添加到 “可达顶点”的集合中
                 reachableStates.add(currentVertex);
+            }
 
         return reachableStates;
     }
@@ -279,15 +329,15 @@ public class NFA {
         Bag<Integer> matchTransferReachedStates = new Bag<Integer>();
         // 检查 当前可达状态集合中，是否存在有 能够与当前文本字符 相匹配的状态
         for (int currentStartState : startStates) {
-            // 如果当前状态 已经是 “接受状态”，说明 在文本字符串中已经找到了一个 正则表达式模式字符串的匹配，
-            // 则：不再继续处理集合中的其他状态
+            // 如果 当前状态 已经是 “接受状态”，说明 在文本字符串中 已经找到了一个 正则表达式模式字符串的匹配，
+            // 则：不再继续处理 集合中的其他状态
             if (isAcceptedState(currentStartState)) continue;
 
             // 获取到 当前状态所对应的模式字符
             char currentRegexCharacter = regexStr.charAt(currentStartState);
             // 如果 当前状态上的模式字符 与 当前文本字符 相匹配，则：
             if (isMatchBetween(currentTxtCharacter, currentRegexCharacter))
-                // 把 此匹配转换所到达的状态(当前状态+1) 添加到 “匹配所达的状态集合”中
+                // 把 此匹配转换 所到达的状态(当前状态+1) 添加到 “匹配所达的状态集合”中
                 matchTransferReachedStates.add(currentStartState + 1);
         }
 
@@ -298,15 +348,16 @@ public class NFA {
         return currentState == characterAmountInRegexStr;
     }
 
-    // 可达性问题 - 有向图中，由指定顶点（顶点0）可达的所有其他顶点（包含起始顶点本身）
+    // 可达性问题 - 有向图中，由 指定顶点（顶点0）可达的 所有其他顶点（包含 起始顶点 本身）
     private Bag<Integer> getReachedStatesViaεTransferFromSpot0() {
         Bag<Integer> reachableStates = new Bag<Integer>();
 
-        // 标记图中“从结点0可达的所有其他结点”
+        // 先标记 图中“从结点0可达的 所有其他结点”
         AccessibleVertexesInDigraph markedDigraph = new AccessibleVertexesInDigraph(epsilonTransitionDigraph, 0);
 
+        // 再收集 所有被标记了的节点
         for (int currentVertex = 0; currentVertex < epsilonTransitionDigraph.getVertexAmount(); currentVertex++)
-            // 如果当前结点 “由起始结点可达”，则：把它添加到 reachableStates集合中
+            // 如果 当前结点 “由起始结点0可达”，则：把 它 添加到 reachableStates集合 中
             if (markedDigraph.isAccessibleFromStartVertex(currentVertex))
                 reachableStates.add(currentVertex);
 
@@ -327,18 +378,18 @@ public class NFA {
      * @param args the command-line arguments
      */
     public static void main(String[] args) {
-        // 获取正则表达式模式字符串
+        // 获取到 正则表达式模式字符串
         String originalRegexStr = args[0];
         String wrappedRegexStr = "(" + originalRegexStr + ")";
-        // 获取文本字符串
+        // 获取到 文本字符串
         String txtStr = args[1];
 
-        // #1 构造出 正则表达式模式字符串的NFA(由ε转换所构成的有向图)
+        // #1 构造出 正则表达式模式字符串的NFA(aka 由 ε转换 所构成的 有向图)
         NFA regexConstructedNFA = new NFA(wrappedRegexStr);
-        // #2 使用 其NFA 来 判断文本字符串中是否存在有 与正则表达式相匹配的子字符串
+        // #2 使用 其NFA 来 判断 在文本字符串中 是否存在有 与正则表达式相匹配的 子字符串
         boolean matchResult = doesExistMatchIn(txtStr, regexConstructedNFA);
 
-        // 🐖 NFA只能提供一个boolean值的答案 - yes | no
+        // 🐖 NFA 只能提供一个 boolean值的答案 - yes | no
         StdOut.println(matchResult);
     }
 
