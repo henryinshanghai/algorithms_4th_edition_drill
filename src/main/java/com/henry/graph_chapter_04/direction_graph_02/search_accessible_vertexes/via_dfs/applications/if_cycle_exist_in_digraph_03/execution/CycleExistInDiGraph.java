7928c102ba89b9b7f40fc460fd512e7709f94715
HenryInSH <2291972433@qq.com>
2025-11-23 22:53:08 +0800

    --- 有向图中的是否存在有环：手段：当前顶点的 已被标记的邻居顶点 是否存在于递归调用栈中。  原理：#1 递归调用栈中 记录着 路径中的各个顶点； #2 当某个顶点的邻居 是 其祖先节点时，意味着 出现了环。 ---

--- src/main/java/com/henry/graph_chapter_04/direction_graph_02/search_accessible_vertexes/via_dfs/applications/if_cycle_exist_in_digraph_03/execution/CycleExistInDiGraph.java
+++ src/main/java/com/henry/graph_chapter_04/direction_graph_02/search_accessible_vertexes/via_dfs/applications/if_cycle_exist_in_digraph_03/execution/CycleExistInDiGraph.java
@@ -1,4 +1,4 @@
-package com.henry.graph_chapter_04.direction_graph_02.search_accessible_vertexes.via_dfs.applications.if_cycle_exist_in_digraph_03;
+package com.henry.graph_chapter_04.direction_graph_02.search_accessible_vertexes.via_dfs.applications.if_cycle_exist_in_digraph_03.execution;
 
 /******************************************************************************
  *  Compilation:  javac DirectedCycle.java
@@ -54,60 +54,90 @@ import edu.princeton.cs.algs4.Topological;
 // 手段：使用 名为 terminalVertexToDepartVertex的数组，指定 正确的 backwardsVertexCursor 与 startVertex 就能够 使用for循环 来 把所有结点收集到栈集合中
 public class CycleExistInDiGraph {
     private final boolean[] vertexToIsMarked;        // 作为DFS算法的基础操作 用于标记顶点是否已经被标记
-    private final int[] terminalVertexToDepartVertex;            // 用于记录单条边中 结束顶点->出发顶点的映射关系 可以用来反溯出整个路径
+    private final int[] terminalVertexToDepartVertex;    // 用于记录单条边中 结束顶点->出发顶点的映射关系 可以用来反溯出整个路径
 
-    private final boolean[] vertexToIsInRecursionStack;       // 用于记录 当前路径中的结点 是否存在于 当前DFS的递归调用栈中
+    private final boolean[] vertexToIsInRecursionStack;  // 用于记录 当前路径中的结点 是否存在于 当前DFS的递归调用栈中
     private Stack<Integer> vertexesInCycleViaStack;    // 用于 临时记录 出现的 环中的所有顶点，只有在 找到环 的时候 才会使用它
 
     /**
-     * Determines whether the digraph {@code G} has a directed cycle and, if so,
-     * finds such a cycle.
-     * 确定有向图G中 是否存在 有有向环，如果存在的话，找到这个环
+     * 构造器方法
+     * 一般作用：用于 创建 当前类的实例对象
+     * 具体的作用：逐一 以 图中所有的节点 作为起始节点，在有向图中 执行DFS。
+     * 应用：确定 在有向图G中 是否存在有 有向环，如果存在的话，找到 这个环
      * 🐖 在构造方法中执行任务
+     *
      * @param digraph the digraph 待检查的有向图
      */
     public CycleExistInDiGraph(Digraph digraph) {
+        // 成员变量的初始化
         vertexToIsMarked = new boolean[digraph.getVertexAmount()];
         vertexToIsInRecursionStack = new boolean[digraph.getVertexAmount()];
         terminalVertexToDepartVertex = new int[digraph.getVertexAmount()];
 
         // 对于当前顶点...
         for (int currentVertex = 0; currentVertex < digraph.getVertexAmount(); currentVertex++)
-            // 如果它还没有被标记 && 当前还没有找到环...
-            if (isNotMarked(currentVertex) && notFindCycleYet())
-                // 则：对它进行标记，并把它记录进当前路径中...
+            // 如果 该顶点 还没有被标记 && 当前 还没有找到环...
+            if (isNotMarked(currentVertex) && notFindCycleYet()) {
+                // 则：以它为起始顶点，在有向图中进行DFS
+                System.out.println("~~~ 1 当前节点" + currentVertex + " 还没有被标记，且 图中当前仍没有检测到环，则：以该节点" + currentVertex + "为起点 执行DFS ~~~");
                 markVertexAndRecordVertexInCurrentPathViaDFS(digraph, currentVertex);
+                System.out.println("~~~ 2 图中 以节点" + currentVertex + "作为起始节点 所进行的DFS结束，所有 由" + currentVertex + "可达的顶点 都已经被标记。 ~~~");
+            }
     }
 
     private boolean notFindCycleYet() {
         return vertexesInCycleViaStack == null;
     }
 
-    // 🐖  如果有向图中存在有环，则：vertexesInCycle 会不为空
+    // 🐖  如果 有向图中 存在有 环，则：vertexesInCycle 会不为空
+
+    /**
+     * 在 指定的有向图 中，以 指定的顶点 作为 起始顶点 进行DFS；
+     * 作用：标记 图中 所有 由起始顶点可达的 所有顶点
+     * 应用：检测出 图中存在的有向环 并 收集有向环中的所有顶点
+     *
+     * @param digraph       指定的有向图
+     * @param currentVertex 作为起始顶点的指定顶点
+     */
     private void markVertexAndRecordVertexInCurrentPathViaDFS(Digraph digraph, int currentVertex) {
-        // #1 为成员变量中的当前结点 绑定 boolean值
+        // #1 为 相关成员变量中的 当前结点 按需绑定 正确的boolean值
         flag(currentVertex);
 
         // #2 对于 当前结点 所有的邻居节点，记录路径中的边 && 验证是否找到了环
         for (int currentAdjacentVertex : digraph.adjacentVertexesOf(currentVertex)) {
-            if (findACycle()) { // 〇 如果发现了环，则：short circuit(短路/提前返回)
+            System.out.println("@@@ 对于 当前顶点" + currentVertex + "的 当前邻居顶点" + currentAdjacentVertex + " @@@");
+            // 〇 如果发现了环，说明 不再需要后继步骤，
+            if (findACycle()) {
+                System.out.println("### 检测到了 有向环的存在，直接返回到 上一级调用。 ###");
+                // 则：short circuit(短路/提前返回)
                 return;
-            } else if (isNotMarked(currentAdjacentVertex)) { // Ⅰ 如果 “当前邻居结点” 是 未被标记的结点，则：继续 递归地 处理它
-                // 在 搜索过程 中，记录下 搜索路径上 当前边的 结束顶点->出发顶点的映射关系  用于回溯出“路径本身”
+            } else if (isNotMarked(currentAdjacentVertex)) { // Ⅰ 如果 “当前邻居结点” 是 未被标记的结点，说明 它还没有被访问到，
+                System.out.println("$$$ 当前顶点" + currentVertex + "的 邻居顶点" + currentAdjacentVertex + " 还没有被标记，则：" +
+                        "① 记录下 搜索路径中的当前边" + currentVertex + "->" + currentAdjacentVertex +
+                        "; ② 以" + currentAdjacentVertex + "作为起始顶点，递归地进行DFS $$$");
+                /* 则：继续 递归地 处理它 */
+                // ① 在 搜索过程 中，记录下 搜索路径上 当前边的 结束顶点->出发顶点的映射关系  用于回溯出“路径本身”
                 terminalVertexToDepartVertex[currentAdjacentVertex] = currentVertex;
-                // 递归处理 此结点
+                // ② 递归处理 此结点
                 markVertexAndRecordVertexInCurrentPathViaDFS(digraph, currentAdjacentVertex);
-            } else if (inRecursionStack(currentAdjacentVertex)) { // Ⅲ 如果在 搜索过程 中，当前邻居结点 ① 已经被标记； ② 且仍旧在递归调用栈中，说明 图中出现了环
+                System.out.println("$$$ 以" + currentAdjacentVertex + "作为 起始顶点的DFS 已经结束，节点" + currentAdjacentVertex + "出栈！ $$$");
+            } else if (inRecursionStack(currentAdjacentVertex)) { // Ⅱ 如果在 搜索过程 中，当前邻居结点 ① 已经被标记； ② 且仍旧在递归调用栈中，说明 图中出现了环
+                System.out.println("%%% 1 当前邻居节点" + currentAdjacentVertex + "已经被标记，并且 存在于 递归调用栈中，说明 找到了一个有向环，则：收集有向环中的所有顶点 %%%");
                 // 则：从 当前结点 开始，沿着路径，一直回溯到 它当前的邻居结点 来 得到环中所有的结点
                 collectVertexesInCycle(currentVertex, currentAdjacentVertex);
+                System.out.println("%%% 2 有向环中的所有顶点 都已经收集完成 %%%");
                 assert verifyIfCycleReallyExist();
             }
         }
 
-        // #3 正确地维护 节点是否存在于递归调用栈 ① 在递归调用结束后，需要把 当前结点 设置为 “不存在于递归调用栈”
+        // #3 正确地维护 节点 是否存在于 递归调用栈中 - 在 递归调用结束 后，需要把 当前结点 设置为 “不存在于递归调用栈”
         vertexToIsInRecursionStack[currentVertex] = false;
+        System.out.println("^^^ 在当前顶点" + currentVertex + "的 所有邻居顶点处理完成后，把其在xxx数组中所对应的元素更新为false" + " ^^^");
+        System.out.println();
     }
 
+    // 判断 指定的顶点 是否存在于 递归调用栈中
+    // 手段：检查 该节点 在手动维护的栈中的值 是否为true
     private boolean inRecursionStack(int currentAdjacentVertex) {
         return vertexToIsInRecursionStack[currentAdjacentVertex];
     }
@@ -115,55 +145,74 @@ public class CycleExistInDiGraph {
     private void flag(int currentVertex) {
         // #1 把 当前结点 设置为 “已标记”
         vertexToIsMarked[currentVertex] = true;
-        // #2 正确地维护 节点是否存在于递归调用栈 ① 在递归调用伊始，把 当前结点 设置为 “存在于递归调用栈”
+        // #2 正确地维护 节点 是否存在于 递归调用栈 ① 在 递归调用 伊始，把 当前结点 设置为 “存在于递归调用栈”
         vertexToIsInRecursionStack[currentVertex] = true;
+        System.out.println("!!! 底层数组中，当前顶点" + currentVertex + "所对应的元素 都已经 按需标记 !!!");
     }
 
-    // 从 terminalVertexToDepartVertex[] 中，获取到 环路径中的所有顶点
-    // endVertex - 路径中的最后一个顶点(终止顶点) startVertex - 路径中的第一个顶点(起始顶点)
+    /**
+     * 从 terminalVertexToDepartVertex[] 中，获取到 有向环中的所有顶点
+     *
+     * @param endVertex   路径中的最后一个顶点(终止顶点)
+     * @param startVertex 路径中的第一个顶点(起始顶点)
+     */
     private void collectVertexesInCycle(int endVertex, int startVertex) {
+        System.out.println();
         // 准备一个节点容器（局部变量）- 这里使用栈
         vertexesInCycleViaStack = new Stack<Integer>();
 
         // #1 向stack中添加 由数组所记录下的结点
+        System.out.println("&&& 1 向预备的栈中，逐个添加 有向环中的顶点 &&&");
         for (int backwardsVertexCursor = endVertex; backwardsVertexCursor != startVertex; backwardsVertexCursor = terminalVertexToDepartVertex[backwardsVertexCursor]) {
+            System.out.println("&&& 当前添加的顶点为：" + backwardsVertexCursor + " &&&");
             vertexesInCycleViaStack.push(backwardsVertexCursor);
         }
 
         // #2 手动添加 环中的“起始结点”（因为for循环中不会添加它）
         vertexesInCycleViaStack.push(startVertex);
+        System.out.println("&&& 2 手动添加 有向环的起始顶点" + startVertex + " &&&");
 
         // #3 手动添加“当前结点”，从而得到 字符形式上/物理意义上的环
         vertexesInCycleViaStack.push(endVertex);
+        System.out.println("&&& 3 手动添加 有向环的终止顶点" + endVertex + "&&&");
+        System.out.println();
     }
 
     private boolean isNotMarked(int currentAdjacentVertex) {
         return !vertexToIsMarked[currentAdjacentVertex];
     }
 
-    // key API*1:有向图中 是否含有一个 有向环?
+    /**
+     * 有向图中 是否含有一个 有向环?
+     *
+     * @return 如果存在，则 返回true；否则 返回false
+     */
     public boolean findACycle() {
-        // 手段：检查用于存储环中顶点的栈 是否为空
+        // 手段：检查 用于存储环中顶点的栈 是否为空
         return vertexesInCycleViaStack != null;
     }
 
-    // key API*2:获取到 有向图中的 有向环(以 有向环中所有顶点的 可迭代集合的方式)
-    // 如果不存在环，则：返回null
+    /**
+     * 获取到 有向图中的 有向环(以 有向环中所有顶点的 可迭代集合的方式)
+     *
+     * @return 如果存在有环，则 返回环中所有节点的可迭代集合；否则 返回null
+     */
     public Iterable<Integer> getVertexesInCycle() {
         // 手段：算法中，有向环中的顶点 会被顺序添加到 栈中👇
         return vertexesInCycleViaStack;
     }
 
-
-    // 确保 算法的正确性：如果 算法 报告发现了 环，使用 此方法 验证算法的结论
+    // 确保 算法的正确性：如果 算法 报告发现了 环，使用 此方法 验证算法的结论 是否正确
     private boolean verifyIfCycleReallyExist() {
         if (findACycle()) {
-            // 验证环 是否真的存在    手段：定指针 + 动指针
+            // 验证 环 是否真的存在    手段：定指针 + 动指针
             int anchorCursor = -1, dynamicCursor = -1;
             for (int currentVertex : getVertexesInCycle()) {
                 // anchorCursor 会一直指在 环的第一个结点上(aka 起始顶点)
                 // 原理：只有第一次循环时，条件成立 anchorCursor会被赋值；其他条件下，不会执行此语句
-                if (anchorCursor == -1) anchorCursor = currentVertex;
+                if (anchorCursor == -1) {
+                    anchorCursor = currentVertex;
+                }
 
                 // dynamicCursor 会 沿着 环上的结点 一直向后移动 - 最终指在 栈的最后一个结点上(aka 起始顶点)
                 dynamicCursor = currentVertex;
@@ -180,9 +229,9 @@ public class CycleExistInDiGraph {
     }
 
     /**
-     * Unit tests the {@code DirectedCycle} data type.
+     * 当前数据类型的单元测试
      *
-     * @param args the command-line arguments
+     * @param args 命令行参数
      */
     public static void main(String[] args) {
         In in = new In(args[0]);
